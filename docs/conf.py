321a0c42e09895eadc6e958d2621f72b8b6138b4
Eric Ma <ericmajinglong@gmail.com>
2018-03-09 08:09:35 -0500

    Updated docs.

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinxcontrib.fulltoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.

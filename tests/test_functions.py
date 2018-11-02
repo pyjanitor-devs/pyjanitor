01e38619f56f20e0dbebbb8b603f4d62478b200b
Sam Zuckerman <sam.p.zuckerman@gmail.com>
2018-11-02 12:58:01 -0400

    Run tests through black

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -545,8 +545,8 @@ def test_row_to_names(dataframe):
     assert df.columns[0] == 3
     assert df.columns[1] == 3
     assert df.columns[2] == 3
-    assert df.columns[3] == 'lion'
-    assert df.columns[4] == 'Basel'
+    assert df.columns[3] == "lion"
+    assert df.columns[4] == "Basel"
 
 
 def test_row_to_names_delete_this_row(dataframe):
@@ -554,8 +554,8 @@ def test_row_to_names_delete_this_row(dataframe):
     assert df.iloc[2, 0] == 1
     assert df.iloc[2, 1] == 1
     assert df.iloc[2, 2] == 1
-    assert df.iloc[2, 3] == 'rabbit'
-    assert df.iloc[2, 4] == 'Cambridge'
+    assert df.iloc[2, 3] == "rabbit"
+    assert df.iloc[2, 4] == "Cambridge"
 
 
 def test_row_to_names_delete_above(dataframe):
@@ -563,5 +563,5 @@ def test_row_to_names_delete_above(dataframe):
     assert df.iloc[0, 0] == 3
     assert df.iloc[0, 1] == 3
     assert df.iloc[0, 2] == 3
-    assert df.iloc[0, 3] == 'lion'
-    assert df.iloc[0, 4] == 'Basel'
+    assert df.iloc[0, 3] == "lion"
+    assert df.iloc[0, 4] == "Basel"

073be87842f6fa556164905d6d26dfd1640660f3
Eric Ma <ericmajinglong@gmail.com>
2018-12-15 19:01:36 -0500

    pycodestyle formatting

--- janitor/utils.py
+++ janitor/utils.py
@@ -1,6 +1,7 @@
 def import_message(submodule, package, installation):
     print(
-        f"To use the janitor submodule {submodule}, you need to install {package}."
+        f"To use the janitor submodule {submodule}, you need to install \
+        {package}."
     )
     print()
     print(f"To do so, use the following command:")

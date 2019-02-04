1aa90ac6986ba9b619754271a644806499d1fd3e
Eric Ma <ericmajinglong@gmail.com>
2019-02-03 20:47:49 -0500

    Added documentation for the test

--- tests/functions/test_filter_column_isin.py
+++ tests/functions/test_filter_column_isin.py
@@ -11,6 +11,17 @@ from janitor.testing_utils.strategies import (
 @pytest.mark.functions
 @given(df=categoricaldf_strategy(), iterable=names_strategy())
 def test_filter_column_isin(df, iterable):
+    """
+    `filter_column_isin` should return the property that the column of
+    interest's set of values should be a subset of the iterable provided.
+    This encompasses a few scenarios:
+
+    - Each element in iterable is present in the column.
+    - No elements of iterable are present in the column.
+    - A subset of elements in iterable are present in the column.
+
+    All 3 cases can be caught by using subsets.
+    """
     assume(len(iterable) >= 1)
     df = df.filter_column_isin("names", iterable)
     assert set(df["names"]).issubset(iterable)

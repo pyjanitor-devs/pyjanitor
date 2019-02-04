d8b1791571dddd0471cf619f88962450af964f5f
Eric Ma <ericmajinglong@gmail.com>
2019-02-03 19:23:03 -0500

    added some bounds to hypothesis-based test

--- tests/functions/test_add_columns.py
+++ tests/functions/test_add_columns.py
@@ -7,24 +7,29 @@ from hypothesis import given, strategies as st, assume
 from hypothesis.extra.numpy import arrays
 
 
+@pytest.mark.test
 @pytest.mark.functions
 @given(
     df=df_strategy(),
+    x_vals=st.floats(),
+    n_yvals=st.integers(min_value=0, max_value=100)
 )
-def test_add_columns(df):
+def test_add_columns(df, x_vals, n_yvals):
     """
     Test for adding multiple columns at the same time.
     """
-    x_vals = 42
-    y_vals = np.linspace(0, 42, len(df))
-    # assume(len(y_vals) == len(df))
+    y_vals = np.linspace(0, 42, n_yvals)
 
-    df = df.add_columns(x=x_vals, y=y_vals)
+    if n_yvals != len(df) or n_yvals == 0:
+        with pytest.raises(ValueError):
+            df = df.add_columns(x=x_vals, y=y_vals)
 
-    series = pd.Series([x_vals] * len(df))
-    series.name = "x"
-    pd.testing.assert_series_equal(df["x"], series)
+    else:
+        df = df.add_columns(x=x_vals, y=y_vals)
+        series = pd.Series([x_vals] * len(df))
+        series.name = "x"
+        pd.testing.assert_series_equal(df["x"], series)
 
-    series = pd.Series(y_vals)
-    series.name = "y"
-    pd.testing.assert_series_equal(df["y"], series)
+        series = pd.Series(y_vals)
+        series.name = "y"
+        pd.testing.assert_series_equal(df["y"], series)

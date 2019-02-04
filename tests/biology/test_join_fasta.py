b3c8aee37e138768cf23f10c036078ceff65ac0d
Eric Ma <ericmajinglong@gmail.com>
2019-02-03 19:06:06 -0500

    made test more explicit with kwarg names

--- tests/biology/test_join_fasta.py
+++ tests/biology/test_join_fasta.py
@@ -7,7 +7,9 @@ from janitor.testing_utils.fixtures import TEST_DATA_DIR, biodf
 @pytest.mark.biology
 def test_join_fasta(biodf):
     df = biodf.join_fasta(
-        TEST_DATA_DIR / "sequences.fasta", "sequence_accession", "sequence"
+        filename=TEST_DATA_DIR / "sequences.fasta",
+        id_col="sequence_accession",
+        col_name="sequence"
     )
 
     assert "sequence" in df.columns

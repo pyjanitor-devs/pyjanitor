ae8a46cca18b8082261d8a8baea91f458a95c3f7
barryza1 <zach.barry@novartis.com>
2019-02-22 14:42:18 -0500

    little addition to test to ensure that the rest of the DF columns of the dropnotnull()-returned dataframe are correct

--- tests/functions/test_dropnotnull.py
+++ tests/functions/test_dropnotnull.py
@@ -4,6 +4,9 @@ import pytest
 
 @pytest.mark.functions
 def test_dropnotnull(missingdata_df):
-    df = missingdata_df.clean_names().dropnotnull("bell_chart")
+    df = missingdata_df.clean_names()
+    df_drop = df.dropnotnull("bell_chart")
 
-    assert pd.isnull(df["bell_chart"]).all()
+    assert pd.isnull(df_drop["bell_chart"]).all()
+
+    pd.testing.assert_frame_equal(df.loc[df_drop.index], df_drop)

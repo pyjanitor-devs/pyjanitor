4c057d82403be2a42a5ea6365a8e1195acba411b
Eric Ma <ericmajinglong@gmail.com>
2019-04-14 18:01:46 -0400

    update docstring for currency API request

--- tests/finance/test_convert_currency.py
+++ tests/finance/test_convert_currency.py
@@ -7,6 +7,17 @@ import janitor.finance
 @pytest.mark.finance
 @pytest.mark.xfail
 def test_make_currency_api_request():
+    """
+    Test for currency API request.
+
+    This test exists because we rely solely on the service by
+    exchangeratesapi. That said, we also mark it as expected to fail because
+    it sometimes pings the exchange rates API a too frequently and causes
+    tests to fail.
+
+    For an example of how this test fails, see:
+    https://github.com/ericmjl/pyjanitor/issues/147
+    """
     r = requests.get("https://api.exchangeratesapi.io")
     assert r.status_code == 200
 

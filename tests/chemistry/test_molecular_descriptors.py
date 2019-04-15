9a9d2ef31d2f97f65591fc731602f067c4d3c20b
bhallaY <bhalla.yash98@gmail.com>
2019-04-15 13:52:56 -0400

    Fixed style errors

--- tests/chemistry/test_molecular_descriptors.py
+++ tests/chemistry/test_molecular_descriptors.py
@@ -1,10 +1,7 @@
-import pandas as pd
 import pytest
-import janitor.chemistry
-    
+
+
 @pytest.mark.chemistry
 def test_molecular_descriptors(chemdf):
-    mol_desc = chemdf.smiles2mol("smiles", "mol").molecular_descriptors(
-            "mol"
-    )
+    mol_desc = chemdf.smiles2mol("smiles", "mol").molecular_descriptors("mol")
     assert mol_desc.shape == (10, 39)

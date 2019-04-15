4781387ecc0afec6e3effa453b5db4c94004c8a3
bhallaY <bhalla.yash98@gmail.com>
2019-04-15 11:32:43 -0400

    Added test cases for maccs_keys_fingerprint Chemistry function

---
+++ tests/chemistry/test_maccs_keys_fingerprint.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import pytest
+
+import janitor.chemistry
+
+@pytest.mark.chemistry
+def test_maccs_keys_fingerprint(chemdf):
+    maccs_keys = chemdf.smiles2mol("smiles", "mol").maccs_keys_fingerprint(
+        "mol"
+    )
+    assert maccs_keys.shape == (10, 167)
+    assert set(maccs_keys.values.flatten().tolist()) == set([0, 1])
+

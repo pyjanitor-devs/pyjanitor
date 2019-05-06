eb125584743483034e3d9a293323db7174298e5e
m.mdini <m.mdini@astellia.com>
2019-05-06 15:05:44 -0400

    change type -- unit tests

--- tests/functions/test_change_type.py
+++ tests/functions/test_change_type.py
@@ -1,7 +1,38 @@
-import pytest
+import pytest 
+import pandas as pd
+import numpy as np 
 
 
 @pytest.mark.functions
 def test_change_type(dataframe):
     df = dataframe.change_type(column="a", dtype=float)
     assert df["a"].dtype == float
+    
+    
+
+@pytest.mark.functions
+def test_change_type_Keep_values():
+    df=pd.DataFrame(['a',1,True], columns=['col1'])
+    df = df.change_type(column="col1", dtype=float, ignore_exception="Keep_values")
+    assert df.equals(pd.DataFrame(['a',1,True], columns=['col1']))
+    
+    
+@pytest.mark.functions
+def test_change_type_Fill_NaN():
+    df=pd.DataFrame(['a',1,True], columns=['col1'])
+    df = df.change_type(column="col1", dtype=float, ignore_exception="FillNaN")
+    assert np.isnan(df.col1[0])
+
+    
+@pytest.mark.functions
+def test_change_type_unknown_option():
+    df=pd.DataFrame(['a',1,True], columns=['col1'])
+    with pytest.raises(Exception):
+        df = df.change_type(column="col1", dtype=float, ignore_exception="blabla")
+        
+        
+@pytest.mark.functions
+def test_change_type_raise_exception():
+    df=pd.DataFrame(['a',1,True], columns=['col1'])
+    with pytest.raises(Exception):
+        df = df.change_type(column="col1", dtype=float, ignore_exception=False)

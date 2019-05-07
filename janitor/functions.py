06686df07d9582ae43f1b2fb3213969c2815e211
danieldsouza <ddsouza@umich.edu>
2019-05-07 16:03:19 -0400

    Added Deprecation message + ran black

--- janitor/functions.py
+++ janitor/functions.py
@@ -2117,8 +2117,10 @@ def reset_index_inplace(df: pd.DataFrame, *args, **kwargs):
     """
 
     # Deprecation Warning
-    warnings.warn("reset_index_inplace will be deprecated in the "
-                  "upcoming 0.18 release. Use .reset_index() instead")
+    warnings.warn(
+        "reset_index_inplace will be deprecated in the "
+        "upcoming 0.18 release. Use .reset_index() instead"
+    )
 
     kwargs.update(inplace=True)
 

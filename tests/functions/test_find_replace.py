ff0bb595eb36f3aac9a3261fff065ca28b06aca4
m.mdini <m.mdini@astellia.com>
2019-05-07 13:50:34 -0400

    find_replace fix unit test

--- tests/functions/test_find_replace.py
+++ tests/functions/test_find_replace.py
@@ -6,8 +6,13 @@ import pytest
 @pytest.mark.test
 @pytest.mark.functions
 def test_find_replace():
-    df = pd.DataFrame(
-        {"a": [1, np.nan, 3], "b": [2, 3, 1], "c": [2, np.nan, 9]}
-    ).find_replace("a", {1: 2, 3: 4, np.nan: 5})
+    df= pd.DataFrame({"a": [1, np.nan, 3], "b": [2, 3, np.nan], "c": [2, np.nan, 9]})
+    df1= df.find_replace(["a","c"], {1: 2, 3: 4, np.nan: 5})
+    df2= pd.DataFrame(
+        {"a": [2, 5, 4], "b": [2, 3, np.nan], "c": [2, 5, 9]})
+    assert (df1[["a","c"]]==df2[["a","c"]]).all().all()
+    
+    
+    
+    
 
-    assert not all(pd.isnull(df["a"]))

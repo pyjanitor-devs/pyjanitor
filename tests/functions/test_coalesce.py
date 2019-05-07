01230aa0b8d5e6a0c15ea07379ae4391b369f386
Dal Williams <jwilli18@go.olemiss.edu>
2019-05-06 22:42:22 -0400

    Added test for case in which new_column_name is not given

--- tests/functions/test_coalesce.py
+++ tests/functions/test_coalesce.py
@@ -7,6 +7,13 @@ import pytest
 def test_coalesce():
     df = pd.DataFrame(
         {"a": [1, np.nan, 3], "b": [2, 3, 1], "c": [2, np.nan, 9]}
-    ).coalesce(["a", "b", "c"], "a")
+    ).coalesce(["a", "b", "c"], "d")
     assert df.shape == (3, 1)
     assert pd.isnull(df).sum().sum() == 0
+
+    df = pd.DataFrame(
+        {"a": [1, np.nan, 3], "b": [2, 3, 1], "c": [2, np.nan, 9]}
+    ).coalesce(["a", "b", "c"])
+    assert df.shape == (3, 1)
+    assert pd.isnull(df).sum().sum() == 0
+

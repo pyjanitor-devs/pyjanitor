f8cb86eedac9a28485217b282abee7f94a115dee
Lucas Kushner <lphk92@gmail.com>
2019-05-07 14:14:37 -0400

    Formatting conf.py from docs

--- docs/conf.py
+++ docs/conf.py
@@ -99,9 +99,7 @@ html_theme = "alabaster"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    "logo": "logo_title.svg",
-}
+html_theme_options = {"logo": "logo_title.svg"}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -117,12 +115,7 @@ html_static_path = ["_static"]
 # 'searchbox.html']``.
 #
 html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',
-        'searchbox.html',
-    ]
+    "**": ["about.html", "navigation.html", "relations.html", "searchbox.html"]
 }
 
 

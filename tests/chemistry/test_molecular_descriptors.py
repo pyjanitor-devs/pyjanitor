e147a5fbe4084bf3f6f854fbb11060a37d4228ac
Hector <hectorenrique.munoz@gmail.com>
2019-05-06 17:26:52 -0700

    Cleanup code formatting issues raised by black and pycodestyle

--- tests/chemistry/test_molecular_descriptors.py
+++ tests/chemistry/test_molecular_descriptors.py
@@ -4,7 +4,10 @@ import pytest
 import janitor.chemistry  # noqa: disable=unused-import
 
 
-@pytest.mark.skipif(importlib.util.find_spec('rdkit') is None, reason="rdkit tests only required for CI")
+@pytest.mark.skipif(
+    importlib.util.find_spec("rdkit") is None,
+    reason="rdkit tests only required for CI",
+)
 @pytest.mark.chemistry
 def test_molecular_descriptors(chemdf):
     mol_desc = chemdf.smiles2mol("smiles", "mol").molecular_descriptors("mol")

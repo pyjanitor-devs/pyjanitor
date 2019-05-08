a91cce468ed268746a00d01097d9018e4f3ad021
deterministic-forest <jwilli18@go.olemiss.edu>
2019-05-08 16:01:13 -0500

    Removed all document strings > 80 chars, split coalesce test into multiple

--- tests/functions/test_coalesce.py
+++ tests/functions/test_coalesce.py
@@ -4,16 +4,21 @@ import pytest
 
 
 @pytest.mark.functions
-def test_coalesce():
+def test_coalesce_with_title():
     df = pd.DataFrame(
         {"a": [1, np.nan, 3], "b": [2, 3, 1], "c": [2, np.nan, 9]}
     ).coalesce(["a", "b", "c"], "d")
+    assert "a" not in df.columns
     assert df.shape == (3, 1)
     assert pd.isnull(df).sum().sum() == 0
 
+
+@pytest.mark.functions
+def test_coalesce_without_title():
     df = pd.DataFrame(
         {"a": [1, np.nan, 3], "b": [2, 3, 1], "c": [2, np.nan, 9]}
     ).coalesce(["a", "b", "c"])
+    assert "a" in df.columns
+    assert "b" not in df.columns
     assert df.shape == (3, 1)
     assert pd.isnull(df).sum().sum() == 0
-

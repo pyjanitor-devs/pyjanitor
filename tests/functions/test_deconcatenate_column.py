5d03f083d1bc481b3458a8a31a68d62b0304738e
Hector <hectorenrique.munoz@gmail.com>
2019-05-09 07:52:04 -0700

    Update naming conventions in concatenate_columns() and deconcatenate_column()

--- tests/functions/test_deconcatenate_column.py
+++ tests/functions/test_deconcatenate_column.py
@@ -4,10 +4,12 @@ import pytest
 @pytest.mark.functions
 def test_deconcatenate_column(dataframe):
     df = dataframe.concatenate_columns(
-        columns=["a", "decorated-elephant"], sep="-", new_column_name="index"
+        column_names=["a", "decorated-elephant"],
+        sep="-",
+        new_column_name="index",
     )
     df = df.deconcatenate_column(
-        column="index", new_column_names=["A", "B"], sep="-"
+        column_name="index", new_column_names=["A", "B"], sep="-"
     )
     assert "A" in df.columns
     assert "B" in df.columns

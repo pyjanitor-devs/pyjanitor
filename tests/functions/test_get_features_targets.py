fb1fc2dad90bc88d9ffe84a14e697498b89676bc
Hector <hectorenrique.munoz@gmail.com>
2019-05-08 20:09:54 -0700

    Update naming conventions in rename_column(), get_features_targets()

--- tests/functions/test_get_features_targets.py
+++ tests/functions/test_get_features_targets.py
@@ -7,7 +7,9 @@ from janitor.testing_utils.strategies import df_strategy
 @pytest.mark.functions
 @given(df=df_strategy())
 def test_get_features_targets(df):
-    X, y = df.clean_names().get_features_targets(target_columns="bell_chart")
+    X, y = df.clean_names().get_features_targets(
+        target_column_names="bell_chart"
+    )
     assert X.shape[1] == 4
     assert len(y.shape) == 1
 
@@ -16,7 +18,8 @@ def test_get_features_targets(df):
 @given(df=df_strategy())
 def test_get_features_targets_multi_features(df):
     X, y = df.clean_names().get_features_targets(
-        feature_columns=["animals@#$%^", "cities"], target_columns="bell_chart"
+        feature_column_names=["animals@#$%^", "cities"],
+        target_column_names="bell_chart",
     )
     assert X.shape[1] == 2
     assert len(y.shape) == 1
@@ -26,7 +29,7 @@ def test_get_features_targets_multi_features(df):
 @given(df=df_strategy())
 def test_get_features_target_multi_columns(df):
     X, y = df.clean_names().get_features_targets(
-        target_columns=["a", "bell_chart"]
+        target_column_names=["a", "bell_chart"]
     )
     assert X.shape[1] == 3
     assert y.shape[1] == 2

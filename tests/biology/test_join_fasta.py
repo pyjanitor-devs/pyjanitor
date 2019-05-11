d77be7f04530e1ce550155d40d6fa335afd8111c
Zachary Barry <zbarry@protonmail.com>
2019-05-11 18:52:15 -0400

    [TST] add skipif for biology (#338)

--- tests/biology/test_join_fasta.py
+++ tests/biology/test_join_fasta.py
@@ -1,10 +1,15 @@
 import os
 
 import pytest
+import importlib
 
 import janitor.biology
 
 
+@pytest.mark.skipif(
+    importlib.util.find_spec("Bio") is None,
+    reason="Biology tests relying on Biopython only required for CI",
+)
 @pytest.mark.biology
 def test_join_fasta(biodf):
     df = biodf.join_fasta(

0086f6daf1a9348d1d479a2dd4ff29b221c4662d
Eric Ma <ericmajinglong@gmail.com>
2019-05-20 15:21:50 -0400

    Bump version: 0.16.6 → 0.17.0

--- janitor/__init__.py
+++ janitor/__init__.py
@@ -3,4 +3,4 @@ from .functions import *  # noqa: F403, F401
 # from .dataframe import JanitorDataFrame as DataFrame  # noqa: F401
 # from .dataframe import JanitorSeries as Series  # noqa: F401
 
-__version__ = "0.16.6"
+__version__ = "0.17.0"

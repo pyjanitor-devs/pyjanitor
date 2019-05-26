df1aa79ca944d799ae6547be2ea58fc758ab4310
Hector <HectorM14@users.noreply.github.com>
2019-05-26 14:53:32 -0700

    [ENH] Update `read_csvs()` naming, argument and testing (#375)

--- janitor/io.py
+++ janitor/io.py
@@ -1,34 +1,49 @@
 import os
 from glob import glob
+from typing import Iterable, Union
 
 import pandas as pd
 
+from .utils import deprecated_alias
 
-def read_csvs(filespath: str, seperate_df: bool = False, **kwargs):
+
+@deprecated_alias(seperate_df="separate_df", filespath="files_path")
+def read_csvs(
+    files_path: Union[str, Iterable[str]], separate_df: bool = False, **kwargs
+) -> Union[pd.DataFrame, dict]:
     """
-    :param filespath: The filepath pattern matching the CSVs files.
+    :param files_path: The filepath pattern matching the CSVs files.
         Accepts regular expressions, with or without csv extension.
-    :param seperate_df: If False (default) returns a single Dataframe
+        Also accepts iterable of file paths.
+    :param separate_df: If False (default) returns a single Dataframe
         with the concatenation of the csv files.
-        If True, returns a dictionary of seperate dataframes
+        If True, returns a dictionary of separate dataframes
         for each CSV file.
     :param kwargs: Keyword arguments to pass into the
         original pandas `read_csv`.
     """
     # Sanitize input
-    assert filespath is not None
-    assert len(filespath) != 0
+    assert files_path is not None
+    assert len(files_path) != 0
 
     # Read the csv files
-    dfs = {
-        os.path.basename(f): pd.read_csv(f, **kwargs) for f in glob(filespath)
-    }
+    # String to file/folder or file pattern provided
+    if isinstance(files_path, str):
+        dfs = {
+            os.path.basename(f): pd.read_csv(f, **kwargs)
+            for f in glob(files_path)
+        }
+    # Iterable of file paths provided
+    else:
+        dfs = {
+            os.path.basename(f): pd.read_csv(f, **kwargs) for f in files_path
+        }
     # Check if dataframes have been read
     if len(dfs) == 0:
-        raise ValueError("No CSV files to read with the given filespath")
+        raise ValueError("No CSV files to read with the given `files_path`")
     # Concatenate the dataframes if requested (default)
     col_names = list(dfs.values())[0].columns
-    if not seperate_df:
+    if not separate_df:
         # If columns do not match raise an error
         for df in dfs.values():
             if not all(df.columns == col_names):
--- tests/io/test_read_csvs.py
+++ tests/io/test_read_csvs.py
@@ -12,13 +12,12 @@ CSV_FILE_PATH = "my_test_csv_for_read_csvs_{}.csv"
 def create_csv_file(number_of_files, col_names=None):
     for i in range(number_of_files):
         filename = CSV_FILE_PATH.format(i)
-        df = pd.DataFrame([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+        df = pd.DataFrame([[1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3]])
         df.to_csv(filename, index=False)
 
 
 def remove_csv_files():
-    # Get a list of all the file paths that ends with .txt
-    # from in specified directory
+    # Get a list of all the file paths matching pattern in specified directory
     fileList = glob.glob(CSV_FILE_PATH.format("*"))
 
     # Iterate over the list of filepaths & remove each file.
@@ -29,16 +28,17 @@ def remove_csv_files():
 @pytest.mark.functions
 def test_read_csvs_one_csv_path():
     # Setup
-    # When a CSV file with 3 cols and 3 rows is on disk
-    create_csv_file(1)
+    # When a CSV file with 3 cols and 4 rows is on disk
+    number_of_files = 1
+    create_csv_file(number_of_files)
 
     # If the csv file is read into DataFrame
     dfs = io.read_csvs(CSV_FILE_PATH.format("*"))
 
-    # Then the dataframe has 3 cols and 3 rows
+    # Then the dataframe has 3 cols and 4 rows
     try:
         assert len(dfs.columns) == 3
-        assert len(dfs) == 3
+        assert len(dfs) == 4
     finally:
         # Cleanup
         remove_csv_files()
@@ -62,16 +62,17 @@ def test_read_csvs_zero_csv_path():
 @pytest.mark.functions
 def test_read_csvs_three_csv_path():
     # Setup
-    # When a CSV file with 3 cols and 3 rows is on disk
-    create_csv_file(3)
+    # When a CSV file with 3 cols and 4 rows is on disk
+    number_of_files = 3
+    create_csv_file(number_of_files)
 
     # If the csv file is read into DataFrame
     dfs = io.read_csvs(CSV_FILE_PATH.format("*"))
 
-    # Then the dataframe has 3 cols and 9 rows
+    # Then the dataframe has 3 cols and 12 rows
     try:
         assert len(dfs.columns) == 3
-        assert len(dfs) == 9
+        assert len(dfs) == 4 * number_of_files
     finally:
         # Cleanup
         remove_csv_files()
@@ -80,17 +81,18 @@ def test_read_csvs_three_csv_path():
 @pytest.mark.functions
 def test_read_csvs_three_separated_csv_path():
     # Setup
-    # When a CSV file with 3 cols and 3 rows is on disk
-    create_csv_file(3)
+    # When a CSV file with 3 cols and 4 rows is on disk
+    number_of_files = 3
+    create_csv_file(number_of_files)
 
     # If the csv file is read into DataFrame
-    dfs = io.read_csvs(CSV_FILE_PATH.format("*"), seperate_df=True)
+    dfs = io.read_csvs(CSV_FILE_PATH.format("*"), separate_df=True)
 
     # Then the dataframe list has 3 dataframes
     try:
-        assert len(dfs) == 3
+        assert len(dfs) == number_of_files
         for df in dfs.values():
-            assert len(df) == 3
+            assert len(df) == 4
             assert len(df.columns) == 3
     finally:
         # Cleanup
@@ -99,7 +101,7 @@ def test_read_csvs_three_separated_csv_path():
 
 @pytest.mark.functions
 def test_read_csvs_two_unmatching_csv_files():
-    # Setupmy
+    # Setup
     # When two csv files do not have same column names
     df = pd.DataFrame(
         [[1, 2, 3], [1, 2, 3], [1, 2, 3]], columns=["a", "b", "c"]
@@ -119,4 +121,26 @@ def test_read_csvs_two_unmatching_csv_files():
         # If the read raises an exception it is ok
         pass
     finally:
+        remove_csv_files()
+
+
+@pytest.mark.functions
+def test_read_csvs_lists():
+    # Setup
+    # When a CSV file with 3 cols and 4 rows is on disk
+    number_of_files = 3
+    create_csv_file(number_of_files)
+    csvs_list = [CSV_FILE_PATH.format(i) for i in range(number_of_files)]
+
+    # If the list of csv files is read into DataFrame
+    dfs = io.read_csvs(files_path=csvs_list, separate_df=True)
+
+    # Then the dataframe list has 3 dataframes
+    try:
+        assert len(dfs) == number_of_files
+        for df in dfs.values():
+            assert len(df) == 4
+            assert len(df.columns) == 3
+    finally:
+        # Cleanup
         remove_csv_files()

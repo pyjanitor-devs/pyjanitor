548854f9362e532b0c458e043a06f96d2854448d
Eric Ma <ericmajinglong@gmail.com>
2019-05-27 09:48:46 -0400

    Bump version: 0.17.1 → 0.17.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def requirements():
 
 setup(
     name="pyjanitor",
-    version="0.17.1",
+    version="0.17.2",
     description="Tools for cleaning pandas DataFrames",
     author="Eric J. Ma",
     author_email="ericmajinglong@gmail.com",

20b6952299d98fba2366a27162ce962bda16317b
Eric Ma <ericmajinglong@gmail.com>
2019-05-27 00:01:52 -0400

    [ENH] Add join_apply function

--- janitor/functions.py
+++ janitor/functions.py
@@ -2980,3 +2980,54 @@ def shuffle(df: pd.DataFrame, random_state=None) -> pd.DataFrame:
     :param random_state: (optional) A seed for the random number generator.
     """
     return df.sample(frac=1, random_state=random_state)
+
+
+@pf.register_dataframe_method
+def join_apply(df, func, new_column_name):
+    """
+    Join the result of applying a function across dataframe rows.
+
+    This is a convenience function that allows us to apply arbitrary functions
+    that take any combination of information from any of the columns. The only
+    requirement is that the function signature takes in a row from the
+    DataFrame.
+
+    The example below shows us how to sum the result of two columns into a new
+    column.
+
+    .. code-block:: python
+
+        df = (
+            pd.DataFrame({'a':[1, 2, 3], 'b': [2, 3, 4]})
+            .join_apply(lambda x: 2 * x['a'] + x['b'], new_column_name="2a+b")
+        )
+
+    This following example shows us how to use conditionals in the same
+    function.
+
+    .. code-block:: python
+
+        def take_a_if_even(x):
+            if x['a'] % 2:
+                return x['a']
+            else:
+                return x['b']
+
+        df = (
+            pd.DataFrame({'a': [1, 2, 3], 'b': [2, 3, 4]})
+            .join_apply(take_a_if_even, 'a_if_even')
+        )
+
+    :param df: A pandas DataFrame
+    :param func: A function that is applied elementwise across all rows of the
+        DataFrame.
+    :param new_name: New column name.
+    """
+    df = (
+        df.copy()
+        .join(
+            df.apply(func, axis=1)
+            .rename(new_name)
+        )
+    )
+    return df

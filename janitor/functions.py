9a22987e4e0c8ab0a65bad6b489a76ea9b2bd84f
asearfos <asearfos@gmail.com>
2019-07-13 21:17:20 -0500

    add function to move row or column

--- janitor/functions.py
+++ janitor/functions.py
@@ -26,6 +26,64 @@ from .utils import (
 
 
 @pf.register_dataframe_method
+def move(df: pd.DataFrame, source: Union[int, str], target: Union[int, str], position: str = 'before',
+         axis: int = 0) -> pd.DataFrame:
+    """
+     Move column or row to a position adjacent to another column or row in dataframe.
+
+     :param df: The pandas Dataframe object.
+     :param int or str source: column or row to move
+     :param str target: column or row to move adjacent to
+     :param str position: Specifies whether the series is moved to before or after the adjacent series. Values can be either 'before' or 'after'; defaults to 'before'.
+     :param int axis: Axis along which the function is applied. 0 to move a row, 1 to move a column.
+     :returns: The dataframe with the series moved.
+
+    """
+    if axis not in [0, 1]:
+        raise ValueError(f"Invalid axis '{axis}'. Can only be 0 or 1.")
+
+    if position not in ['before', 'after']:
+        raise ValueError(f"Invalid position '{position}'. Can only be 'before' or 'after'.")
+
+    if axis == 0:
+        names = list(df.index)
+
+        if source not in names:
+            raise ValueError(f"Source row '{source}' not in dataframe.")
+
+        if target not in names:
+            raise ValueError(f"Target row '{target}' not in dataframe.")
+
+        names.remove(source)
+        pos = names.index(target)
+
+        if position == 'after':
+            pos += 1
+        names.insert(pos, source)
+
+        df = df.loc[names, :]
+    else:
+        names = list(df.columns)
+
+        if source not in names:
+            raise ValueError(f"Source column '{source}' not in dataframe.")
+
+        if target not in names:
+            raise ValueError(f"Target column '{target}' not in dataframe.")
+
+        names.remove(source)
+        pos = names.index(target)
+
+        if position == 'after':
+            pos += 1
+        names.insert(pos, source)
+
+        df = df.loc[:, names]
+
+    return df
+
+
+@pf.register_dataframe_method
 def clean_names(
     df: pd.DataFrame,
     strip_underscores: str = None,

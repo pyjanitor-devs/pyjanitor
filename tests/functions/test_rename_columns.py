15f0b571d90fa10916bf4231446dbd0b8f484ba2
Dmitry Nortsev <dsnorcev@yahoo.com>
2019-07-14 18:41:42 +0100

    Add unit tests for check_column

--- tests/functions/test_rename_columns.py
+++ tests/functions/test_rename_columns.py
@@ -1,6 +1,8 @@
 import pytest
 from hypothesis import given
 
+from janitor.utils import check_column
+
 
 @pytest.mark.functions
 def test_rename_columns(dataframe):
@@ -14,7 +16,7 @@ def test_rename_columns(dataframe):
 
 
 @pytest.mark.functions
-def test_rename_column_absent_column(dataframe):
+def test_rename_columns_absent_column(dataframe):
     """
     rename_column should raise an error if the column is absent.
     """
@@ -23,3 +25,20 @@ def test_rename_column_absent_column(dataframe):
         df.clean_names().rename_columns({"a": "index", "bb": "chart"})
 
     assert set(df.columns) == set(dataframe.columns)
+
+
+@pytest.mark.functions
+def test_check_column(dataframe):
+    """
+    rename_column should return true if column exist
+    """
+    assert check_column(dataframe, ["a"]) is None
+
+
+@pytest.mark.functions
+def test_check_column_absent_column(dataframe):
+    """
+    rename_column should raise an error if the column is absent.
+    """
+    with pytest.raises(ValueError):
+        check_column(dataframe, ["b"])

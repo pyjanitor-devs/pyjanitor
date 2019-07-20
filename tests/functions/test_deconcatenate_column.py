2858d163d791caa31c56d870dbbcafd9e0f71641
Shan Dou <shandou.seismo@gmail.com>
2019-07-20 15:24:02 -0700

    revert to original test for `test_deconcatenate_column`

--- tests/functions/test_deconcatenate_column.py
+++ tests/functions/test_deconcatenate_column.py
@@ -8,27 +8,10 @@ def test_deconcatenate_column(dataframe):
         sep="-",
         new_column_name="index",
     )
-    index_original = list(df_orig.columns).index("index")
-    index_next = index_original + 1
     df = df_orig.deconcatenate_column(
         column_name="index",
-        new_column_names=["col1", "col2"],
+        new_column_names=["A", "B"],
         sep="-",
-        preserve_position=False,
     )
-    assert "col1" in df.columns
-    assert "col2" in df.columns
-    # Test for `preserve_position` kwarg
-    df = df_orig.deconcatenate_column(
-        column_name="index",
-        new_column_names=["col1", "col2"],
-        sep="-",
-        preserve_position=True,
-    )
-    assert "index" not in df.columns, "column_name not dropped"
-    assert (
-        list(df.columns).index("col1") == index_original
-    ), "Position not preserved"
-    assert (
-        list(df.columns).index("col2") == index_next
-    ), "Position not preserved"
+    assert "A" in df.columns
+    assert "B" in df.columns

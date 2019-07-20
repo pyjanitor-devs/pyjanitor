3720d605bd202c68452122efe5574d61fdbf836e
Robert Hosbach <rahosbach@lbl.gov>
2019-07-20 12:11:58 -0700

    Add inflate_currency() method to finance.py

--- janitor/finance.py
+++ janitor/finance.py
@@ -487,3 +487,295 @@ def convert_currency(
         df[column_name] = df[column_name] * rate
 
     return df
+
+@lru_cache(maxsize=32)
+def _inflate_currency(
+    country: str = None, currency_year: int = None, to_year: int = None
+):
+    """
+    Currency inflation for Pandas DataFrame column.
+    Helper function for `inflate_currency` method.
+    The API used is the World Bank Indicator API:
+    https://datahelpdesk.worldbank.org/knowledgebase/articles/889392-about-the-indicators-api-documentation
+    """
+
+    # Check all inputs are correct data type
+    check("country", country, [str])
+    check("currency_year", currency_year, [int])
+    check("to_year", to_year, [int])
+
+    # Get WB country abbreviation
+    _check_wb_country(country)
+    if country in wb_country_dict.keys():
+        country = wb_country_dict[country]
+    else:
+        # `country` is already a correct abbreviation; do nothing
+        pass
+
+    _check_wb_years(currency_year)
+    _check_wb_years(to_year)
+
+    url = (
+        "https://api.worldbank.org/v2/country/"
+        + country
+        + "/indicator/FP.CPI.TOTL.ZG?date="
+        + str(min(currency_year, to_year))
+        + ":"
+        + str(max(currency_year, to_year))
+        + "&format=json"
+    )
+
+    result = requests.get(url)
+
+    if result.status_code != 200:
+        raise ConnectionError(
+            "WB Indicator API failed to receive a 200 "
+            "response from the server. "
+            "Please try again later."
+        )
+
+    inflation_dict = json.loads(result.text)
+
+    # Check that the inflation_dict is currently a list with two elements
+    # It's the second element we want
+    if len(inflation_dict) != 2:
+        raise ValueError(
+            "WB Indicator API did not return " "the expected result."
+        )
+    inflation_dict = inflation_dict[1]
+
+    # Error checking
+    if inflation_dict is None:
+        raise ValueError(
+            "The WB Indicator API returned nothing. "
+            "This likely means the currency_year and "
+            "to_year are outside of the year range for "
+            "which the WB has inflation data for the "
+            "specified country."
+        )
+
+    # Create new dict with only the year and inflation values
+    inflation_dict_ready = {
+        int(inflation_dict[i]["date"]): float(inflation_dict[i]["value"])
+        for i in range(len(inflation_dict))
+    }
+
+    # Error catching
+    if currency_year not in inflation_dict_ready.keys():
+        raise ValueError(
+            f"The WB Indicator API does not have inflation "
+            f"data for {currency_year} for {country}."
+        )
+    if to_year not in inflation_dict_ready.keys():
+        raise ValueError(
+            f"The WB Indicator API does not have inflation "
+            f"data for {to_year} for {country}."
+        )
+
+    inflator = (
+        inflation_dict_ready[to_year] / inflation_dict_ready[currency_year]
+    )
+    return inflator
+
+
+@pf.register_dataframe_method
+@deprecated_alias(colname="column_name")
+def inflate_currency(
+    df: pd.DataFrame,
+    column_name: str = None,
+    country: str = None,
+    currency_year: int = None,
+    to_year: int = None,
+    make_new_column: bool = False,
+) -> pd.DataFrame:
+    """
+    Inflates a column of monetary values from one year to another, based on
+    the currency's country.
+
+    This method mutates the original DataFrame.
+
+    :param df: A pandas dataframe.
+    :param column_name: Name of the new column. Should be a string, in order
+        for the column name to be compatible with the Feather binary
+        format (this is a useful thing to have).
+    :param country: The country associated with the currency being inflated.
+        May be any keys or values of: wb_country_dict = {'Aruba': 'ABW',
+		'Afghanistan': 'AFG', 'Angola': 'AGO', 'Albania': 'ALB',
+		'Andorra': 'AND', 'Arab World': 'ARB', 'United Arab Emirates': 'ARE',
+		'Argentina': 'ARG', 'Armenia': 'ARM', 'American Samoa': 'ASM',
+		'Antigua and Barbuda': 'ATG', 'Australia': 'AUS', 'Austria': 'AUT',
+		'Azerbaijan': 'AZE', 'Burundi': 'BDI', 'Belgium': 'BEL',
+		'Benin': 'BEN', 'Burkina Faso': 'BFA', 'Bangladesh': 'BGD',
+		'Bulgaria': 'BGR', 'Bahrain': 'BHR', 'Bahamas, The': 'BHS',
+		'Bosnia and Herzegovina': 'BIH', 'Belarus': 'BLR', 'Belize': 'BLZ',
+		'Bermuda': 'BMU', 'Bolivia': 'BOL', 'Brazil': 'BRA',
+		'Barbados': 'BRB', 'Brunei Darussalam': 'BRN', 'Bhutan': 'BTN',
+		'Botswana': 'BWA', 'Central African Republic': 'CAF',
+		'Canada': 'CAN', 'Central Europe and the Baltics': 'CEB',
+		'Switzerland': 'CHE', 'Channel Islands': 'CHI', 'Chile': 'CHL',
+		'China': 'CHN', "Cote d'Ivoire": 'CIV', 'Cameroon': 'CMR',
+		'Congo, Dem. Rep.': 'COD', 'Congo, Rep.': 'COG', 'Colombia': 'COL',
+		'Comoros': 'COM', 'Cabo Verde': 'CPV', 'Costa Rica': 'CRI',
+		'Caribbean small states': 'CSS', 'Cuba': 'CUB', 'Curacao': 'CUW',
+		'Cayman Islands': 'CYM', 'Cyprus': 'CYP', 'Czech Republic': 'CZE',
+		'Germany': 'DEU', 'Djibouti': 'DJI', 'Dominica': 'DMA',
+		'Denmark': 'DNK', 'Dominican Republic': 'DOM', 'Algeria': 'DZA',
+		'East Asia & Pacific (excluding high income)': 'EAP',
+		'Early-demographic dividend': 'EAR', 'East Asia & Pacific': 'EAS',
+		'Europe & Central Asia (excluding high income)': 'ECA',
+		'Europe & Central Asia': 'ECS', 'Ecuador': 'ECU',
+		'Egypt, Arab Rep.': 'EGY', 'Euro area': 'EMU', 'Eritrea': 'ERI',
+		'Spain': 'ESP', 'Estonia': 'EST', 'Ethiopia': 'ETH',
+		'European Union': 'EUU',
+		'Fragile and conflict affected situations': 'FCS',
+		'Finland': 'FIN', 'Fiji': 'FJI', 'France': 'FRA',
+		'Faroe Islands': 'FRO', 'Micronesia, Fed. Sts.': 'FSM',
+		'Gabon': 'GAB', 'United Kingdom': 'GBR', 'Georgia': 'GEO',
+		'Ghana': 'GHA', 'Gibraltar': 'GIB', 'Guinea': 'GIN',
+		'Gambia, The': 'GMB', 'Guinea-Bissau': 'GNB',
+		'Equatorial Guinea': 'GNQ', 'Greece': 'GRC', 'Grenada': 'GRD',
+		'Greenland': 'GRL', 'Guatemala': 'GTM', 'Guam': 'GUM',
+		'Guyana': 'GUY', 'High income': 'HIC',
+		'Hong Kong SAR, China': 'HKG', 'Honduras': 'HND',
+		'Heavily indebted poor countries (HIPC)': 'HPC',
+		'Croatia': 'HRV', 'Haiti': 'HTI', 'Hungary': 'HUN',
+		'IBRD only': 'IBD', 'IDA & IBRD total': 'IBT',
+		'IDA total': 'IDA', 'IDA blend': 'IDB', 'Indonesia': 'IDN',
+		'IDA only': 'IDX', 'Isle of Man': 'IMN', 'India': 'IND',
+		'Not classified': 'INX', 'Ireland': 'IRL',
+		'Iran, Islamic Rep.': 'IRN', 'Iraq': 'IRQ', 'Iceland': 'ISL',
+		'Israel': 'ISR', 'Italy': 'ITA', 'Jamaica': 'JAM',
+		'Jordan': 'JOR', 'Japan': 'JPN', 'Kazakhstan': 'KAZ',
+		'Kenya': 'KEN', 'Kyrgyz Republic': 'KGZ',
+		'Cambodia': 'KHM', 'Kiribati': 'KIR',
+		'St. Kitts and Nevis': 'KNA', 'Korea, Rep.': 'KOR',
+		'Kuwait': 'KWT',
+		'Latin America & Caribbean (excluding high income)': 'LAC',
+		'Lao PDR': 'LAO', 'Lebanon': 'LBN', 'Liberia': 'LBR',
+		'Libya': 'LBY', 'St. Lucia': 'LCA',
+		'Latin America & Caribbean': 'LCN',
+		'Least developed countries: UN classification': 'LDC',
+		'Low income': 'LIC', 'Liechtenstein': 'LIE',
+		'Sri Lanka': 'LKA', 'Lower middle income': 'LMC',
+		'Low & middle income': 'LMY', 'Lesotho': 'LSO',
+		'Late-demographic dividend': 'LTE', 'Lithuania': 'LTU',
+		'Luxembourg': 'LUX', 'Latvia': 'LVA',
+		'Macao SAR, China': 'MAC', 'St. Martin (French part)': 'MAF',
+		'Morocco': 'MAR', 'Monaco': 'MCO', 'Moldova': 'MDA',
+		'Madagascar': 'MDG', 'Maldives': 'MDV',
+		'Middle East & North Africa': 'MEA', 'Mexico': 'MEX',
+		'Marshall Islands': 'MHL', 'Middle income': 'MIC',
+		'North Macedonia': 'MKD', 'Mali': 'MLI', 'Malta': 'MLT',
+		'Myanmar': 'MMR',
+		'Middle East & North Africa (excluding high income)': 'MNA',
+		'Montenegro': 'MNE', 'Mongolia': 'MNG',
+		'Northern Mariana Islands': 'MNP', 'Mozambique': 'MOZ',
+		'Mauritania': 'MRT', 'Mauritius': 'MUS', 'Malawi': 'MWI',
+		'Malaysia': 'MYS', 'North America': 'NAC', 'Namibia': 'NAM',
+		'New Caledonia': 'NCL', 'Niger': 'NER', 'Nigeria': 'NGA',
+		'Nicaragua': 'NIC', 'Netherlands': 'NLD', 'Norway': 'NOR',
+		'Nepal': 'NPL', 'Nauru': 'NRU', 'New Zealand': 'NZL',
+		'OECD members': 'OED', 'Oman': 'OMN',
+		'Other small states': 'OSS', 'Pakistan': 'PAK',
+		'Panama': 'PAN', 'Peru': 'PER', 'Philippines': 'PHL',
+		'Palau': 'PLW', 'Papua New Guinea': 'PNG', 'Poland': 'POL',
+		'Pre-demographic dividend': 'PRE', 'Puerto Rico': 'PRI',
+		"Korea, Dem. People's Rep.": 'PRK', 'Portugal': 'PRT',
+		'Paraguay': 'PRY', 'West Bank and Gaza': 'PSE',
+		'Pacific island small states': 'PSS',
+		'Post-demographic dividend': 'PST',
+		'French Polynesia': 'PYF', 'Qatar': 'QAT',
+		'Romania': 'ROU', 'Russian Federation': 'RUS',
+		'Rwanda': 'RWA', 'South Asia': 'SAS',
+		'Saudi Arabia': 'SAU', 'Sudan': 'SDN', 'Senegal': 'SEN',
+		'Singapore': 'SGP', 'Solomon Islands': 'SLB',
+		'Sierra Leone': 'SLE', 'El Salvador': 'SLV',
+		'San Marino': 'SMR', 'Somalia': 'SOM', 'Serbia': 'SRB',
+		'Sub-Saharan Africa (excluding high income)': 'SSA',
+		'South Sudan': 'SSD', 'Sub-Saharan Africa': 'SSF',
+		'Small states': 'SST', 'Sao Tome and Principe': 'STP',
+		'Suriname': 'SUR', 'Slovak Republic': 'SVK',
+		'Slovenia': 'SVN', 'Sweden': 'SWE', 'Eswatini': 'SWZ',
+		'Sint Maarten (Dutch part)': 'SXM', 'Seychelles': 'SYC',
+		'Syrian Arab Republic': 'SYR',
+		'Turks and Caicos Islands': 'TCA', 'Chad': 'TCD',
+		'East Asia & Pacific (IDA & IBRD countries)': 'TEA',
+		'Europe & Central Asia (IDA & IBRD countries)': 'TEC',
+		'Togo': 'TGO', 'Thailand': 'THA', 'Tajikistan': 'TJK',
+		'Turkmenistan': 'TKM',
+		'Latin America & the Caribbean (IDA & IBRD countries)': 'TLA',
+		'Timor-Leste': 'TLS',
+		'Middle East & North Africa (IDA & IBRD countries)': 'TMN',
+		'Tonga': 'TON', 'South Asia (IDA & IBRD)': 'TSA',
+		'Sub-Saharan Africa (IDA & IBRD countries)': 'TSS',
+		'Trinidad and Tobago': 'TTO', 'Tunisia': 'TUN',
+		'Turkey': 'TUR', 'Tuvalu': 'TUV', 'Tanzania': 'TZA',
+		'Uganda': 'UGA', 'Ukraine': 'UKR',
+		'Upper middle income': 'UMC',
+		'Uruguay': 'URY', 'United States': 'USA',
+		'Uzbekistan': 'UZB', 'St. Vincent and the Grenadines': 'VCT',
+		'Venezuela, RB': 'VEN', 'British Virgin Islands': 'VGB',
+		'Virgin Islands (U.S.)': 'VIR', 'Vietnam': 'VNM',
+		'Vanuatu': 'VUT', 'World': 'WLD', 'Samoa': 'WSM',
+		'Kosovo': 'XKX', 'Yemen, Rep.': 'YEM', 'South Africa': 'ZAF',
+		'Zambia': 'ZMB', 'Zimbabwe': 'ZWE'}
+    :param currency_year: The currency year to inflate from.
+        The year should be 1960 or later.
+    :param to_year: The currency year to inflate to.
+    	The year should be 1960 or later.
+    :param make_new_column: Generates new column for inflated currency if
+        True, otherwise, inflates currency in place.
+
+    :Setup:
+
+    .. code-block:: python
+
+        import pandas as pd
+        import janitor
+
+        data_dict = {
+            "a": [1.23452345, 2.456234, 3.2346125] * 3,
+            "Bell__Chart": [1/3, 2/7, 3/2] * 3,
+            "decorated-elephant": [1/234, 2/13, 3/167] * 3,
+            "animals": ["rabbit", "leopard", "lion"] * 3,
+            "cities": ["Cambridge", "Shanghai", "Basel"] * 3,
+        }
+
+        example_dataframe = pd.DataFrame(data_dict)
+
+    :Example: Converting a column from 1990 USD to 2018 USD
+
+     (the conversion factor is approx. 2.44/5.40 = 0.45):
+
+    .. code-block:: python
+
+        example_dataframe.inflate_currency('a', country='USA',
+        currency_year=1990, to_year=2018)
+
+    :Output:
+
+    .. code-block:: python
+
+                   a  Bell__Chart  decorated-elephant  animals     cities
+        0  0.558624     0.333333            0.004274   rabbit  Cambridge
+        1  1.111450     0.285714            0.153846  leopard   Shanghai
+        2  1.463667     1.500000            0.017964     lion      Basel
+        3  0.558624     0.333333            0.004274   rabbit  Cambridge
+        4  1.111450     0.285714            0.153846  leopard   Shanghai
+        5  1.463667     1.500000            0.017964     lion      Basel
+        6  0.558624     0.333333            0.004274   rabbit  Cambridge
+        7  1.111450     0.285714            0.153846  leopard   Shanghai
+        8  1.463667     1.500000            0.017964     lion      Basel
+
+    """
+
+    inflator = _inflate_currency(country, currency_year, to_year)
+
+    if make_new_column:
+        new_column_name = column_name + "_" + str(to_year)
+        df[new_column_name] = df[column_name] * inflator
+
+    else:
+        df[column_name] = df[column_name] * inflator
+
+    return df

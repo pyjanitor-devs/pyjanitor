8fe662e2667a5b272841cf7346163c831e45356b
Robert Hosbach <rahosbach@lbl.gov>
2019-07-29 08:26:18 -0700

    Updated engineering.py to remove eval() statement

--- janitor/engineering.py
+++ janitor/engineering.py
@@ -94,7 +94,7 @@ def convert_units(
     if not np.issubdtype(df[column_name].dtype, np.number):
         raise TypeError(f"{column_name} must be a numeric column.")
 
-    original_vals = df[column_name].values * eval("unyt." + existing_units)
+    original_vals = df[column_name].values * unyt.unit(existing_units)
     converted_vals = original_vals.to(to_units)
     df[dest_column_name] = np.array(converted_vals)
 

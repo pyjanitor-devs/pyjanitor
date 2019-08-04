0c429e5ba8b5588dba88954bdf7a1c96df5baf39
Paul Anzel <anzel.paul@heb.com>
2019-08-04 13:12:56 -0500

    Add functional test

--- tests/functions/test_flag_nulls.py
+++ tests/functions/test_flag_nulls.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pytest
+from janitor.functions import flag_nulls
 from pandas.testing import assert_frame_equal
 
 TEST_DF = pd.DataFrame({"a": [1, 2, None, 4], "b": [5.0, None, 7.0, 8.0]})
@@ -21,6 +22,21 @@ def test_functional_on_all_columns():
 
     # Should also be the same for explicit columns
     df = TEST_DF.flag_nulls(columns=["a", "b"])
+
+    assert_frame_equal(df, expected)
+
+
+@pytest.mark.functions
+def test_non_method_functional():
+    expected = pd.DataFrame(
+        {
+            "a": [1, 2, None, 4],
+            "b": [5.0, None, 7.0, 8.0],
+            "null_flag": [0, 1, 1, 0],
+        }
+    )
+
+    df = flag_nulls(TEST_DF)
 
     assert_frame_equal(df, expected)
 

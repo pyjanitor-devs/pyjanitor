5ce1ecb84ec33e93602eb28ebdfb40601e647075
Eric Ma <ericmajinglong@gmail.com>
2019-08-10 11:08:32 -0400

    corrected pytest marks on utils (change from functions to utils)

--- tests/utils/test_currency_column_to_numeric.py
+++ tests/utils/test_currency_column_to_numeric.py
@@ -3,21 +3,21 @@ import pytest
 from janitor.utils import _currency_column_to_numeric
 
 
-@pytest.mark.functions
+@pytest.mark.utils
 def test_empty_input():
     assert _currency_column_to_numeric("") == "ORIGINAL_NA"
 
 
-@pytest.mark.functions
+@pytest.mark.utils
 def test_cast_non_numeric_true():
     assert _currency_column_to_numeric("foo", {"foo": 42}) == 42
 
 
-@pytest.mark.functions
+@pytest.mark.utils
 def test_cast_non_numeric_false():
     assert _currency_column_to_numeric("10 dollars", {"foo": 42}) == "10"
 
 
-@pytest.mark.functions
+@pytest.mark.utils
 def test_non_cast_input():
     assert _currency_column_to_numeric("-1,000,000 yen") == "-1000000"

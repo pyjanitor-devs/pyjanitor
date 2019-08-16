465aa6b5a87a8f322a298a49ab41cc6e5ea4080d
Ram-N <ramnarasimha@gmail.com>
2019-08-16 18:14:05 -0400

    Added the check_dtype=False flag to assert_frame_equal #533

--- tests/functions/test_flag_nulls.py
+++ tests/functions/test_flag_nulls.py
@@ -8,54 +8,50 @@ from janitor.functions import flag_nulls
 def test_functional_on_all_columns(missingdata_df):
     expected = missingdata_df.copy()
     expected["null_flag"] = [0, 1, 1] * 3
-    expected = expected.astype({"null_flag": "int32"})
 
     df = missingdata_df.flag_nulls()
 
-    assert_frame_equal(df, expected)
+    assert_frame_equal(df, expected, check_dtype=False)
 
     # Should also be the same for explicit columns
     df = missingdata_df.flag_nulls(columns=["a", "Bell__Chart"])
 
-    assert_frame_equal(df, expected)
+    assert_frame_equal(df, expected, check_dtype=False)
 
 
 @pytest.mark.functions
 def test_non_method_functional(missingdata_df):
     expected = missingdata_df.copy()
     expected["null_flag"] = [0, 1, 1] * 3
-    expected = expected.astype({"null_flag": "int32"})
 
     df = flag_nulls(missingdata_df)
 
-    assert_frame_equal(df, expected)
+    assert_frame_equal(df, expected, check_dtype=False)
 
 
 @pytest.mark.functions
 def test_functional_on_some_columns(missingdata_df):
     expected = missingdata_df.copy()
     expected["null_flag"] = [0, 0, 1] * 3
-    expected = expected.astype({"null_flag": "int32"})
 
     df = missingdata_df.flag_nulls(columns=["a"])
 
-    assert_frame_equal(df, expected)
+    assert_frame_equal(df, expected, check_dtype=False)
 
     # Testing when we provide the direct name
     df = missingdata_df.flag_nulls(columns="a")
 
-    assert_frame_equal(df, expected)
+    assert_frame_equal(df, expected, check_dtype=False)
 
 
 @pytest.mark.functions
 def test_rename_output_column(missingdata_df):
     expected = missingdata_df.copy()
     expected["flag"] = [0, 1, 1] * 3
-    expected = expected.astype({"flag": "int32"})
 
     df = missingdata_df.flag_nulls(column_name="flag")
 
-    assert_frame_equal(df, expected)
+    assert_frame_equal(df, expected, check_dtype=False)
 
 
 @pytest.mark.functions

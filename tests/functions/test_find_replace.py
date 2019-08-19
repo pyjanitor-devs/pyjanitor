ff270e4e0e95e6ee8aaabb14aa895a36be660ca4
Shan Dou <shandou.seismo@gmail.com>
2019-08-18 19:02:54 -0700

    add and pass tests for find_replace match='regex'

--- tests/functions/test_find_replace.py
+++ tests/functions/test_find_replace.py
@@ -3,14 +3,13 @@ import pandas as pd
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture
 def df():
     return pd.DataFrame(
         {"a": [1, np.nan, 3], "b": [2, 3, 1], "c": [2, np.nan, 2]}
     )
 
 
-@pytest.mark.test
 @pytest.mark.functions
 def test_find_replace_single(df):
     assert df["a"].iloc[2] == 3
@@ -24,8 +23,31 @@ def test_find_replace_single(df):
     assert sum(df["c"] == 5) == 2
 
 
-@pytest.mark.test
 @pytest.mark.functions
 def test_find_replace_null_raises_error(df):
     with pytest.raises(ValueError):
         df.find_replace("a", {np.nan: 5})
+
+
+@pytest.fixture
+def df_orders():
+    return pd.DataFrame({
+        'customer': ['Mary', 'Tom', 'Lila'],
+        'order': ['ice coffee', 'lemonade', 'regular coffee']
+    })
+
+
+@pytest.mark.functions
+def test_find_replace_regex(df_orders):
+    df_orders.find_replace('order', {'coffee$': 'latte'}, match='regex')
+    assert df_orders['order'].iloc[0] == 'latte'
+    assert df_orders['order'].iloc[1] == 'lemonade'
+    assert df_orders['order'].iloc[-1] == 'latte'
+
+
+@pytest.mark.functions
+def test_find_replace_regex_match_rases_error(df_orders):
+    with pytest.raises(ValueError):
+        df_orders.find_replace(
+            'order', {'lemonade': 'orange juice'}, match='bla'
+        )

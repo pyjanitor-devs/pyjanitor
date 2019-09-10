708ea510b6a018e3bdfc918fd28c12c52f5e9486
Robert Hosbach <rahosbach@lbl.gov>
2019-09-09 19:39:43 -0700

    Add test_count_cumulative_unique.py pytest module

---
+++ tests/functions/test_count_cumulative_unique.py
@@ -0,0 +1,24 @@
+import pytest
+
+from janitor.functions import count_cumulative_unique
+
+
+@pytest.mark.functions
+def test_case_sensitive(dataframe):
+    from pdb import set_trace as bp
+    bp()
+    dataframe['ccu'] = ['a', 'b', 'c', 'A', 'B', 'C', 'a', 'b', 'c']
+    df = dataframe.count_cumulative_unique(
+        "ccu", dest_column_name="ccu_count", case_sensitive=True
+    )
+    # column ccu_count should contain [1,2,3,4,5,6,6,6,6]
+    assert all(df['ccu'] == [1, 2, 3, 4, 5, 6, 6, 6, 6])
+
+@pytest.mark.functions
+def test_not_case_sensitive(dataframe):
+    dataframe['ccu'] = ['a', 'b', 'c', 'A', 'B', 'C', 'a', 'b', 'c']
+    df = dataframe.count_cumulative_unique(
+        "ccu", dest_column_name="ccu_count", case_sensitive=False
+    )
+    # column ccu_count should contain [1,2,3,3,3,3,3,3,3]
+    assert all(df['ccu'] == [1, 2, 3, 3, 3, 3, 3, 3, 3])

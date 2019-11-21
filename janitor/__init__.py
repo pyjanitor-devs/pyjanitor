296ca00c626eb562d624dc153e097f0fd74f64c6
Dal Williams <jwilli18@go.olemiss.edu>
2019-11-21 14:11:39 -0600

    Added currency column refactor to __init__.py

--- janitor/__init__.py
+++ janitor/__init__.py
@@ -2,7 +2,7 @@ from .functions import *  # noqa: F403, F401
 from .math import *
 from .ml import get_features_targets as _get_features_targets
 from .utils import refactored_function
-
+from .finance import currency_column_to_numeric as _currency_column_to_numeric
 # from .dataframe import JanitorDataFrame as DataFrame  # noqa: F401
 # from .dataframe import JanitorSeries as Series  # noqa: F401
 
@@ -12,6 +12,13 @@ from .utils import refactored_function
 )
 def get_features_targets(*args, **kwargs):
     return _get_features_targets(*args, **kwargs)
+
+
+@refactored_function(
+    "currency_column_to_numeric() has moved. Please use finance.currency_column_to_numeric()."
+)
+def currency_column_to_numeric(*args, **kwargs):
+    return _currency_column_to_numeric(*args, **kwargs)
 
 
 __version__ = "0.18.2"

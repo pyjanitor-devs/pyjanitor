ac044607ec665fed9f9eacb577cceb0738a6d408
Robert Hosbach <rahosbach@lbl.gov>
2020-01-14 14:54:08 -0800

    Add comments to datatypes check test in test_jitter.py

--- tests/functions/test_jitter.py
+++ tests/functions/test_jitter.py
@@ -6,17 +6,25 @@ from janitor.functions import jitter
 @pytest.mark.functions
 def test_datatypes_check(dataframe):
     with pytest.raises(TypeError):
+        # `scale` should be a numeric value > 0
         assert dataframe.jitter(
             column_name="a", dest_column_name="a_jitter", scale="x"
         )
+        # `random_state` should be an integer or 1-d array (see documentation for np.random.seed)
         assert dataframe.jitter(
             column_name="a", dest_column_name="a_jitter", scale=1, random_state="x"
         )
+        # `clip` should only contain numeric values
         assert dataframe.jitter(
             column_name="a", dest_column_name="a_jitter", scale=1,
             clip=["x", 2]
         )
     with pytest.raises(ValueError):
+        # `scale` should be greater than 0
+        assert dataframe.jitter(
+            column_name="a", dest_column_name="a_jitter", scale=-5
+        )
+        # `clip` should be a size-2 tuple of numeric values
         assert dataframe.jitter(
             column_name="a", dest_column_name="a_jitter", scale=1,
             clip=[-10, 10, 5]

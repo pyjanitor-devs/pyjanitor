fcc565d135d05ceb64c0cc14958cf02a76e00cd3
Eric Ma <ericmajinglong@gmail.com>
2020-02-21 22:17:30 +0100

    bumpversion 0.20.3

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ extra_all = extra_biology + extra_engineering + extra_spark
 
 setup(
     name="pyjanitor",
-    version="0.20.2",
+    version="0.20.3",
     description="Tools for cleaning pandas DataFrames",
     author="Eric J. Ma",
     author_email="ericmajinglong@gmail.com",

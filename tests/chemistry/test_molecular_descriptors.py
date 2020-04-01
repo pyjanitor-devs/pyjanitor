c1fd682e6781ccb339983592356416c7edbf9cce
Hector <23343812+hectormz@users.noreply.github.com>
2020-03-31 19:17:38 -0700

    [TST] Add test docstrings to ml, biology, chemistry modules (#649)
    * Add test docstrings to `ml`
    
    * Add test docstring to `biology`
    
    * Add test docstring to `chemistry`
    
    * Update CHANGELOG once for all test docstring additions
    
    * Update `test_maccs_keys_fingerprint` docstring
    
    Co-Authored-By: Eric Ma <ericmjl@users.noreply.github.com>
    
    * Fix docstrings imperative
    
    Co-authored-by: Eric Ma <ericmjl@users.noreply.github.com>

--- tests/chemistry/test_molecular_descriptors.py
+++ tests/chemistry/test_molecular_descriptors.py
@@ -11,5 +11,6 @@ import janitor.chemistry  # noqa: disable=unused-import
 )
 @pytest.mark.chemistry
 def test_molecular_descriptors(chemdf):
+    """Test conversion of Mol objects to 39 column molecular descriptors."""
     mol_desc = chemdf.smiles2mol("smiles", "mol").molecular_descriptors("mol")
     assert mol_desc.shape == (10, 39)

9b00b1fd4a4338967ea3bec76c57c9e06a75f832
samukweku <samueloranyeli@gmail.com>
2020-06-15 00:40:30 +0000

    format and lint update_where and find_replace

--- tests/functions/test_update_where.py
+++ tests/functions/test_update_where.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import pytest
 from pandas._testing import assert_frame_equal
 
@@ -31,19 +31,15 @@ def test_update_where():
     """
     Test that function works with expression
     """
-    df = pd.DataFrame({
-        "a": [1, 2, 3, 4],
-        "b": [5, 6, 7, 8],
-        "c": [0, 0, 0, 0]
-    })
-    expected = pd.DataFrame({
-        'a': [1, 2, 3, 4],
-        'b': [5, 6, 7, 8],
-        'c': [0, 0, 10, 0]
-    })
-    result = (df.update_where(conditions="a > 2 and b < 8",
-                              target_column_name='c',
-                              target_val=10))
+    df = pd.DataFrame(
+        {"a": [1, 2, 3, 4], "b": [5, 6, 7, 8], "c": [0, 0, 0, 0]}
+    )
+    expected = pd.DataFrame(
+        {"a": [1, 2, 3, 4], "b": [5, 6, 7, 8], "c": [0, 0, 10, 0]}
+    )
+    result = df.update_where(
+        conditions="a > 2 and b < 8", target_column_name="c", target_val=10
+    )
 
     assert_frame_equal(result, expected)
 
@@ -54,17 +50,17 @@ def test_update_where_1():
     is not a string
     """
 
-    df = pd.DataFrame({
-        "a": [1, 2, 3, 4],
-        "b": [5, 6, 7, 8],
-        "c": [0, 0, 0, 0]
-    })
+    df = pd.DataFrame(
+        {"a": [1, 2, 3, 4], "b": [5, 6, 7, 8], "c": [0, 0, 0, 0]}
+    )
 
     with pytest.raises(TypeError):
-        update_where(df=df,
-                     conditions=(df['a'] > 2) & (df['b'] < 8),
-                     target_column_name='c',
-                     target_val=10)
+        update_where(
+            df=df,
+            conditions=(df["a"] > 2) & (df["b"] < 8),
+            target_column_name="c",
+            target_val=10,
+        )
 
 
 def test_update_where_2():
@@ -72,22 +68,26 @@ def test_update_where_2():
     Test with null entries
     """
 
-    df = pd.DataFrame({
-        'A': [1, 2, 3, 4, np.nan],
-        'B': range(10, 0, -2),
-        'C': range(10, 5, -1)
-    })
+    df = pd.DataFrame(
+        {
+            "A": [1, 2, 3, 4, np.nan],
+            "B": range(10, 0, -2),
+            "C": range(10, 5, -1),
+        }
+    )
 
-    expected = pd.DataFrame({
-        'A': [1.0, 2.0, 3.0, 4.0, np.nan],
-        'B': [10, 8, 6, 4, 2],
-        'C': [10, 9, 8, 7, 10]
-    })
+    expected = pd.DataFrame(
+        {
+            "A": [1.0, 2.0, 3.0, 4.0, np.nan],
+            "B": [10, 8, 6, 4, 2],
+            "C": [10, 9, 8, 7, 10],
+        }
+    )
 
-    #set A not equal to A, since NaN != NaN
-    #this excludes the null rows
-    result = (df.update_where(conditions='A!=A and B==2',
-                              target_column_name='C',
-                              target_val=10))
+    # set A not equal to A, since NaN != NaN
+    # this excludes the null rows
+    result = df.update_where(
+        conditions="A!=A and B==2", target_column_name="C", target_val=10
+    )
 
     assert_frame_equal(result, expected)

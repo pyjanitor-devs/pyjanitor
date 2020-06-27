2a17528805f611006e729d044011dc1529c76108
Eric Ma <ericmajinglong@gmail.com>
2020-06-27 09:49:37 -0400

    Fix escape sequence

--- scripts/check-autodoc.py
+++ scripts/check-autodoc.py
@@ -6,32 +6,38 @@ that have not been added to the "General Functions" API docs.
 import re
 from pathlib import Path
 
-test_folder = Path("./tests/functions")
 
 # Extract the names from the test_functions folder
 # function_names = [
 #     name.stem.split("_", 1)[-1].strip() for name in test_folder.iterdir()
 # ]
-function_names = []
-for name in test_folder.iterdir():
-    if not name.is_dir():
-        print(name)
-        print(name.stem.split("_", 1)[-1].strip())
-        function_names.append(name.stem.split("_", 1)[-1].strip())
 
+
+def extract_function_names(test_folder):
+    function_names = []
+    for name in test_folder.iterdir():
+        if not name.is_dir():
+            function_names.append(name.stem.split("_", 1)[-1].strip())
+    return function_names
+
+
+def extract_documented_functions(docs):
+    pattern = re.compile(r"\s{4}[a-zA-Z_]+")
+
+    # get the names in the general_functions page
+    with docs.open() as doc:
+        doc_functions = [
+            pattern.search(line).group().strip()
+            for line in doc
+            if pattern.search(line)
+        ]
+    return doc_functions
+
+
+test_folder = Path("./tests/functions")
+function_names = extract_function_names(test_folder)
 docs = Path("./docs/reference/general_functions.rst")
-
-pattern = re.compile("\s{4}[a-zA-Z_]+")
-
-# get the names in the general_functions page
-with docs.open() as doc:
-    doc_functions = [
-        pattern.search(line).group().strip()
-        for line in doc
-        if pattern.search(line)
-    ]
-
-# get the functions that have not yet been added
+doc_functions = extract_documented_functions(docs)
 missing_funcs = set(function_names).difference(doc_functions)
 
 if len(missing_funcs) > 0:

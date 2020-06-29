9aa5620fd52242a73391f270eff8fd7a0885e1bc
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2020-06-29 02:27:19 +0200

    [DOC] Fix signatures #661 (#685)
    * Modify various functions signatures.
    
    - Complete signature of functions.find_replace
    - Complete signature of functions.join_apply
    - Complete signature of finance._convert_currency
    - Complete signature of finance._inflate_currency
    - Complete signature of ml.get_features_targets
    
    * Update CHANGELOG.rst
    
    * Update CHANGELOG.rst

--- janitor/finance.py
+++ janitor/finance.py
@@ -347,7 +347,7 @@ def _convert_currency(
     from_currency: str = None,
     to_currency: str = None,
     historical_date: date = None,
-):
+) -> float:
     """
     Currency conversion for Pandas DataFrame column.
 
@@ -492,7 +492,7 @@ def convert_currency(
 @lru_cache(maxsize=32)
 def _inflate_currency(
     country: str = None, currency_year: int = None, to_year: int = None
-):
+) -> float:
     """
     Currency inflation for Pandas DataFrame column.
     Helper function for `inflate_currency` method.

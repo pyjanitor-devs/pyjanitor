da34595c903043ba42749ad27abb5714e0584ec2
Uday Guntupalli <uguntupalli@nexant.com>
2020-07-27 21:29:05 -0700

    incorporating changes recommended via pull request

--- janitor/timeseries.py
+++ janitor/timeseries.py
@@ -1,5 +1,5 @@
 """
-Time series-specific data cleaning functions.
+Time series-specific data testing and cleaning functions.
 """
 
 import pandas as pd
@@ -17,22 +17,24 @@ def _flag_missing_timestamps(
     last_time_stamp: pd.Timestamp,
 ) -> namedtuple:
     """
-    Utility function to test if input data frame is missing any timestamps relative to expected timestamps
-    generated based on the first_time_stamp, last_time_stamp and frequency.
+    Test if timestamps are missing
 
-    :param pd.DataFrame df: data frame which needs to be tested for missing timestamps
-    :param str frequency: frequency i.e. sampling frequency of the data, expressed in seconds. A list of acceptable
-    frequency strings are available here
-    (https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases)
-    :param str column_name: name of the column which has time series if not the index.
-    :param pd.Timestamp first_time_stamp: timestamp at which the time_series is expected to start from.
-    :param pd.Timestamp last_time_stamp: timestamp at which the time_series is expected to end with.
+    Utility function to test if input data frame is missing any timestamps relative to expected timestamps.
+    They are generated based on the first_time_stamp, last_time_stamp and frequency.
+
+    :param df: data frame which needs to be tested for missing timestamps
+    :param frequency: frequency i.e. sampling frequency of the data.
+        A list of acceptable frequency strings are available here:
+        (https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases)
+    :param column_name: name of the column which has time series if not the index.
+    :param first_time_stamp: timestamp at which the time_series is expected to start from.
+    :param last_time_stamp: timestamp at which the time_series is expected to end with.
     :return: namedtuple with 3 attributes namely flag, raw_data and new_index
         1. flag - boolean set to True if there are missing timestamps, else set to False
         2. raw_data - input data frame as is without any modifications
-        3. new_index - pd.DateTimeIndex that can be used to set the new index, defaults to None, assigned a value only
-        when flag is set to True
-    :rtype: namedtuple
+        3. new_index - pd.DateTimeIndex that can be used to set the new index.
+            Defaults to None.
+            Assigned a value only when flag is set to True
     """
     # Declare a named tuple to hold results
     MissingTimeStampFlag = namedtuple('MissingTimeStampFlag', ['flag', 'raw_data', 'new_index'])
@@ -47,9 +49,9 @@ def _flag_missing_timestamps(
 
     # Get actual timestamps
     if column_name:
-        df.set_index(column_name, inplace=True)
+        df = df.set_index(column_name, inplace=True)
 
-    df.sort_index(inplace=True)
+    df = df.sort_index(inplace=True)
     actual_timestamps = df.index.values
 
     # Check if they are the same
@@ -73,21 +75,28 @@ def fill_missing_timestamps(
     last_time_stamp: pd.Timestamp = None,
 ) -> pd.DataFrame:
     """
-    Fills data frame with missing timestamps if found to be missing. Re-index the data frame if there are missing
-    timestamps else no changes wil be made and the original data frame will be returned.
+    Fills data frame with missing timestamps if found to be missing.
 
-    :param pd.DataFrame df: data frame which needs to be tested for missing timestamps
-    :param str frequency: frequency i.e. sampling frequency of the data, expressed in seconds. A list of acceptable
-    frequency strings are available here
-    (https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases)
-    :param str column_name: name of the column which has time series if not the index. Defaults to None. By default, the
-    index is used for checking for the timestamps, unless a value other than None is assigned to column_name
-    :param pd.Timestamp first_time_stamp: timestamp at which the time_series is expected to start from. Defaults to None
-    If no input is provided, assumes the minimum value in time_series
-    :param pd.Timestamp last_time_stamp: timestamp at which the time_series is expected to end with. Defaults to None
-    If no input is provided, assumes the maximum value in time_series
-    :return: reindexed data frame if it turns out to have missing timestamps, else returns the input data frame as is
-    :rtype: pd.DataFrame
+    Test the data frame for missing timestamps.
+    If timestamps are missing, then Re-indexes the data frame.
+    If timestamps are not missing, original data frame will be returned.
+
+    :param df: data frame which needs to be tested for missing timestamps
+    :param frequency: frequency i.e. sampling frequency of the data.
+        A list of acceptable frequency strings are available here:
+        (https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases)
+    :param column_name: name of the column which has time series if not the index.
+        Defaults to None.
+        By default the index is used for checking for the timestamps,
+        unless a value other than None is assigned to column_name
+    :param first_time_stamp: timestamp at which the time_series is expected to start from.
+        Defaults to None.
+        If no input is provided assumes the minimum value in time_series
+    :param last_time_stamp: timestamp at which the time_series is expected to end with.
+        Defaults to None.
+        If no input is provided, assumes the maximum value in time_series
+    :return: reindexed data frame if it turns out to have missing timestamps,
+        else returns the input data frame as is
     """
     # Check all the inputs are the correct data type
     check("df", df, [pd.DataFrame])
@@ -117,7 +126,7 @@ def fill_missing_timestamps(
 
     # Return result based on whether timestamps are missing or not
     if timestamps_missing_flag['flag']:
-        df.set_index(timestamps_missing_flag['new_index'], inplace=True)
+        df = df.set_index(timestamps_missing_flag['new_index'], inplace=True)
         return df
     else:
         return timestamps_missing_flag['raw_data']
--- tests/timeseries/test_fill_missing_timestamps.py
+++ tests/timeseries/test_fill_missing_timestamps.py
@@ -3,22 +3,23 @@ import pandas as pd
 from janitor.timeseries import fill_missing_timestamps
 
 
-@pytest.mark.timeseries
-def test_datatypes_check():
+@pytest.mark.parametrize(
+    "df,frequency,column_name,first_time_stamp,last_time_stamp",
+    [
+        (4, '1T', None, None, None),
+        (pd.DataFrame([]), 2, None, None, None),
+        (pd.DataFrame([]), '1B', 1, None, None),
+        (pd.DataFrame([]), '1B', 'DateTime', '2020-01-01', None),
+        (pd.DataFrame([]), '1B', 'DateTime', pd.Timestamp(2020, 1, 1), '2020-02-01'),
+    ]
+)
+def test_datatypes_check(df, frequency, column_name, first_time_stamp, last_time_stamp):
     with pytest.raises(TypeError):
-        assert fill_missing_timestamps(
-            4, '1T', None, None, None
-        )
-        assert fill_missing_timestamps(
-            pd.DataFrame([]), 2, None, None, None
-        )
-        assert fill_missing_timestamps(
-            pd.DataFrame([]), '1B', 1, None, None
-        )
-        assert fill_missing_timestamps(
-            pd.DataFrame([]), '1B', 'DateTime', '2020-01-01', None
-        )
-        assert fill_missing_timestamps(
-            pd.DataFrame([]), '1B', 'DateTime', pd.Timestamp(2020, 1, 1), '2020-02-01'
-        )
+        fill_missing_timestamps(df, frequency, column_name, first_time_stamp, last_time_stamp)
 
+
+@pytest.mark.timeseries
+def test_fill_missing_timestamps():
+    assert fill_missing_timestamps(
+        pd.DataFrame([]), '1B', 'DateTime', pd.Timestamp(2020, 1, 1), pd.Timestamp(2020, 2, 1)
+    )

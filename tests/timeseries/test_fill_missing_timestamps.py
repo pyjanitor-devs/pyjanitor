8654321f0b53d845af84d8666ceb690f45f52127
Uday Guntupalli <uguntupalli@nexant.com>
2020-07-28 09:26:48 -0700

    adding a test for test_fill_missing_timestamps

--- tests/timeseries/test_fill_missing_timestamps.py
+++ tests/timeseries/test_fill_missing_timestamps.py
@@ -1,6 +1,12 @@
 import pytest
 import pandas as pd
+from random import randint
 from janitor.timeseries import fill_missing_timestamps
+
+# Random data for testing 
+ts_index = pd.date_range('1/1/2019', periods=1000, freq='1H')
+v1 = [randint(1, 2000) for i in range(1000)]
+test_df = pd.DataFrame({'v1': v1}, index=ts_index)
 
 
 @pytest.mark.parametrize(
@@ -20,6 +26,16 @@ def test_datatypes_check(df, frequency, column_name, first_time_stamp, last_time
 
 @pytest.mark.timeseries
 def test_fill_missing_timestamps():
-    assert fill_missing_timestamps(
-        pd.DataFrame([]), '1B', 'DateTime', pd.Timestamp(2020, 1, 1), pd.Timestamp(2020, 2, 1)
+    # Remove random row from the data frame
+    df1 = test_df.drop(test_df.sample())
+
+    result = fill_missing_timestamps(
+        df1,
+        frequency='1H'
     )
+
+    assert result == test_df
+
+
+
+

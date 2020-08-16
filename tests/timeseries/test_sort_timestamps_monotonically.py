ff1bb8b0ccb1cfd9964265c032e55570bd980cc7
Uday Guntupalli <uguntupalli@nexant.com>
2020-08-16 14:31:20 -0700

    1. Added ```sort_timestamps_monotonically``` function 2. Added a unit test for ```sort_timestamps_monotonically``` 3. Ran ```make docs``` 4. Ran ```make style``` -> fails 5. Ran ```make lint``` 6. Ran ```make test``` -> fails

---
+++ tests/timeseries/test_sort_timestamps_monotonically.py
@@ -0,0 +1,32 @@
+import pytest
+import pandas as pd
+from random import randint
+from janitor.timeseries import sort_timestamps_monotonically
+
+
+# Random data for testing
+@pytest.fixture
+def timeseries_dataframe() -> pd.DataFrame:
+    """
+    Returns a time series dataframe
+    """
+    ts_index = pd.date_range("1/1/2019", periods=1000, freq="1H")
+    v1 = [randint(1, 2000) for i in range(1000)]
+    test_df = pd.DataFrame({"v1": v1}, index=ts_index)
+    return test_df
+
+
+@pytest.mark.timeseries
+def test_sort_timestamps_monotonically(timeseries_dataframe):
+    """Test that sort_timestamps_monotonically works as expected."""
+    from sklearn.utils import shuffle
+
+    # Increasing direction
+    df = shuffle(timeseries_dataframe)
+    df1 = sort_timestamps_monotonically(df)
+    assert df1.equals(timeseries_dataframe)
+
+    # Decreasing direction
+    df2 = timeseries_dataframe.sort_index(ascending=False)
+    df3 = sort_timestamps_monotonically(df2, "decreasing")
+    assert df3.equals(df2)

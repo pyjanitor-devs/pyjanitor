cfcc32ec60653b9f545ac040b87b272e510cd009
loganthomas <logan.thomas005@gmail.com>
2020-09-22 22:24:39 -0500

    [DOC] update tests/xarray/contest.py da() docstring to ignore darglint

--- tests/xarray/conftest.py
+++ tests/xarray/conftest.py
@@ -12,6 +12,7 @@ def da():
     that are either linearly or log-spaced increments.
 
     Included is a simple metadata dictionary passed as ``attrs``.
+    # noqa: DAR201
     """
     da = xr.DataArray(
         np.random.randint(0, 100, size=(512, 1024)),

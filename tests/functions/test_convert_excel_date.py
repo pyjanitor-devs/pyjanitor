01e23d6b8279b76761217cc0089aff9322eda1be
samukweku <samueloranyeli@gmail.com>
2020-11-09 08:52:06 +0000

    updates

--- tests/functions/test_convert_excel_date.py
+++ tests/functions/test_convert_excel_date.py
@@ -8,7 +8,8 @@ import pytest
 def test_convert_excel_date():
     df = (
         pd.read_excel(
-            Path(pytest.EXAMPLES_DIR) / "notebooks" / "dirty_data.xlsx", engine='openpyxl'
+            Path(pytest.EXAMPLES_DIR) / "notebooks" / "dirty_data.xlsx",
+            engine="openpyxl",
         )
         .clean_names()
         .convert_excel_date("hire_date")

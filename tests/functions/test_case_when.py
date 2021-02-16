b529ab9150f72837e55c134a00d64a096ad05274
robertmitchellv <robert.beaty.mitchell@gmail.com>
2021-02-16 08:38:17 +0000

    Added a basic test for case_when

---
+++ tests/functions/test_case_when.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import pytest
+from pandas.testing import assert_frame_equal
+
+from janitor.functions import case_when
+
+def test_update_where_query():
+    """Test that function works with pandas query-style string expression."""
+    df = pd.DataFrame({
+        "a": [0, 0, 1, 2, "hi"], 
+        "b": [0, 3, 4, 5, "bye"], 
+        "c": [6, 7, 8, 9, "wait"]}
+    )
+    expected = pd.DataFrame({
+        "a": [0, 0, 1, 2, "hi"], 
+        "b": [0, 3, 4, 5, "bye"], 
+        "c": [6, 7, 8, 9, "wait"],
+        "value": ["x", 0, 8, 9, "hi"]}
+    )
+    result = case_when(
+        df, 
+        "value", 
+        [((df.a == 0) & (df.b != 0)) | (df.c == "wait"), df.a],
+        [(df.b == 0) & (df.a == 0), "x"],
+        [True, df.c]
+    ) 
+
+    assert_frame_equal(result, expected)

71f14c7fe689d43ca5c30b349f6153085525f9fb
samuel oranyeli <samueloranyeli@gmail.com>
2021-04-09 00:52:08 +0000

    xfail for finance/test_convert_currency

--- tests/finance/test_convert_currency.py
+++ tests/finance/test_convert_currency.py
@@ -25,6 +25,7 @@ def test_make_currency_api_request():
     assert r.status_code == 200
 
 
+@pytest.mark.xfail(reason="changes madeto API prevent this from running")
 @pytest.mark.finance
 def test_make_new_currency_col(dataframe):
     """Test converting to same currency equals original currency column."""

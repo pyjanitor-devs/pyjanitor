446751dce5b11b7fcb5906e27bcc2d37a6174cac
BaritoneBeard <NickAlmeder@Yahoo.com>
2021-05-07 15:41:04 -0400

    Attempt to comply with Black

--- tests/functions/test_trunc_datetime.py
+++ tests/functions/test_trunc_datetime.py
@@ -9,14 +9,14 @@ def test_trunc_datetime():
     print(x)
     x = trunc_datetime("month", x)
     time = {
-        'Year': [x.year],
-        'Month': [x.month],
-        'Day': [x.day],
-        'Hour': [x.hour],
-        'Minute': [x.minute],
-        'Second': [x.second],
+        "Year": [x.year],
+        "Month": [x.month],
+        "Day": [x.day],
+        "Hour": [x.hour],
+        "Minute": [x.minute],
+        "Second": [x.second],
     }
 
     # time[] returns datetime object, needs indexing.
-    assert time['Day'][0] == 1
-    assert time['Month'][0] == datetime.now().month
+    assert time["Day"][0] == 1
+    assert time["Month"][0] == datetime.now().month

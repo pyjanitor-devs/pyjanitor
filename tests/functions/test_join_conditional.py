2818f5e22255f60bb90355a9c29deab7db9ab649
BaritoneBeard <NickAlmeder@Yahoo.com>
2021-05-10 21:53:38 -0400

    linter compliance

--- tests/functions/test_join_conditional.py
+++ tests/functions/test_join_conditional.py
@@ -7,19 +7,19 @@ import pytest
 @pytest.mark.functions
 def test_join_conditional():
     company_sales = {
-        'SalesMonth': ['Jan', 'Feb', 'Mar', 'April'],
-        'Company1': [150.0, 200.0, 300.0, 400.0],
-        'Company2': [180.0, 250.0, np.nan, 500.0],
-        'Company3': [400.0, 500.0, 600.0, 675.0]
+        "SalesMonth": ["Jan", "Feb", "Mar", "April"],
+        "Company1": [150.0, 200.0, 300.0, 400.0],
+        "Company2": [180.0, 250.0, np.nan, 500.0],
+        "Company3": [400.0, 500.0, 600.0, 675.0],
     }
 
     df = pd.DataFrame.from_dict(company_sales)
 
     company_sales2 = {
-        'SalesMonth': ['Jan', 'Feb', 'April'],
-        'Company1': [150.0, 200.0, 400.0],
-        'Company2': [180.0, 250.0, 500.0],
-        'Company3': [400.0, 500.0, 675.0]
+        "SalesMonth": ["Jan", "Feb", "April"],
+        "Company1": [150.0, 200.0, 400.0],
+        "Company2": [180.0, 250.0, 500.0],
+        "Company3": [400.0, 500.0, 675.0],
     }
     df_2 = pd.DataFrame.from_dict(company_sales2)
 

06d78be1743a9466919148b946e97593c6917825
BaritoneBeard <NickAlmeder@Yahoo.com>
2021-05-11 13:46:17 -0400

    test improvements

--- tests/functions/test_convert_stock.py
+++ tests/functions/test_convert_stock.py
@@ -4,4 +4,4 @@ from janitor import get_symbol
 def test_convert_stock():
     assert get_symbol("GME") == "GameStop Corp."
     assert get_symbol("AAPL") != "Aramark"
-    assert get_symbol("ASNF") == "Not found."
+    assert get_symbol("ASNF") is None

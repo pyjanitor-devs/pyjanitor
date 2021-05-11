4a37c921a75154d9d744b89d4598eb081c5f96e0
BaritoneBeard <NickAlmeder@Yahoo.com>
2021-05-11 12:59:34 -0400

    linter compliance

--- tests/functions/test_get_symbol.py
+++ tests/functions/test_get_symbol.py
@@ -5,4 +5,4 @@ def test_get_symbol():
     assert get_symbol("GME") == "GameStop Corp."
     assert get_symbol("GME") != "Globus Medical Inc."
     assert get_symbol("F") == "Ford Motor Company"
-    assert get_symbol("ZZZZ") == "Not found."
+    assert get_symbol("ZZZZ") is None

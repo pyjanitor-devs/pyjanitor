7c94cf586ea9f1330b2ea8a1759f4437970c0ca8
ericlessantostv <54956337+ericlessantostv@users.noreply.github.com>
2021-05-10 23:00:11 -0400

    Create test_get_symbol.py

---
+++ tests/functions/test_get_symbol.py
@@ -0,0 +1,9 @@
+from janitor.functions import get_symbol
+
+def test_get_symbol():
+  assert (get_symbol("GME") == "GameStop Corp.")
+  assert (get_symbol("GME") != "Globus Medical Inc.")
+  assert (get_symbol("F") == "Ford Motor Company")
+  assert (get_symbol("ZZZZ") == "Not found.")
+  
+  

a9214333a52f6e7b7ce4a50afab03b00f29cd494
BaritoneBeard <NickAlmeder@Yahoo.com>
2021-05-10 20:42:30 -0400

    Linter compliance

--- tests/functions/test_nonstandard_charac.py
+++ tests/functions/test_nonstandard_charac.py
@@ -3,23 +3,24 @@ import pandas as pd
 
 def test_charac():
     table_GDP = pd.read_html(
-        'https://en.wikipedia.org/wiki/Economy_of_the_United_States',
-        match='Nominal GDP')
+        "https://en.wikipedia.org/wiki/Economy_of_the_United_States",
+        match="Nominal GDP",
+    )
     df = table_GDP[0]
 
-    df = df.clean_names(strip_underscores=True, case_type='lower')
+    df = df.clean_names(strip_underscores=True, case_type="lower")
 
     print(df.columns)
 
-    assert 'current_accountbalance_in_%_of_gdp' in df.columns.values
+    assert "current_accountbalance_in_%_of_gdp" in df.columns.values
 
 
 def test_space():
     table_GDP = pd.read_html(
-        'https://en.wikipedia.org/wiki/Economy_of_Russia',
-        match='Year')
+        "https://en.wikipedia.org/wiki/Economy_of_Russia", match="Year"
+    )
     df = table_GDP[0]
 
-    df = df.clean_names(strip_underscores=True, case_type='lower')
+    df = df.clean_names(strip_underscores=True, case_type="lower")
 
     assert ("in %" in df.columns.values) is False

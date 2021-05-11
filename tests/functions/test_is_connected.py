ea6896ec0b795cb5d5acbca58157e76905f68bba
ericlessantostv <54956337+ericlessantostv@users.noreply.github.com>
2021-05-10 23:50:26 -0400

    Update test_is_connected.py

--- tests/functions/test_is_connected.py
+++ tests/functions/test_is_connected.py
@@ -1,9 +1,8 @@
 from janitor.functions import is_connected
+import pytest
 
 def test_is_connected():
-    print("Starting tests...")
     assert (is_connected("www.google.com"))
     assert (is_connected("www.google.comzzz") == False)
     assert (is_connected("www.facebook.com"))
     assert (is_connected("aadsfff.com") == False)
-    print("Passed all tests")

2747b0eff10e341e6ad4ee0a389dcd11461a3a5a
Nicholas Almeder <32600512+BaritoneBeard@users.noreply.github.com>
2021-05-19 19:53:22 -0400

    Update test_get_symbol.py

--- tests/finance/test_get_symbol.py
+++ tests/finance/test_get_symbol.py
@@ -1,11 +1,7 @@
 from janitor import get_symbol
 
 '''
-Tests get_symbol function,
-get_symbol should return appropriate string corresponding to abbreviation
-
-If the abbreviation does not have a corresponding string,
-an exception should be raised to alert the user.
+tests the convert_symbol helper function.
 
 Test 1: GME is Gamestop Corp. Test should run fine.
 Test 2: GME is not Globius Medical Inc. 
@@ -14,6 +10,7 @@ Test 3: A little redundant, but it's another
 abbreviations than just the one tested so far.
 Test 4: ZZZZ does not belong to any company,
 it should therefore it should be None
+
 '''
 
 def test_get_symbol():

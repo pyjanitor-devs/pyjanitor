bc15392da2ba040e5ae01780fc8ef8d0bd6e144c
Nicholas Almeder <32600512+BaritoneBeard@users.noreply.github.com>
2021-05-19 20:11:03 -0400

    Updated and moved test_is_connected.py to utils

--- tests/functions/test_is_connected.py
+++
@@ -1,8 +0,0 @@
-from janitor.functions import is_connected
-
-
-def test_is_connected():
-    assert is_connected("www.google.com")
-    assert is_connected("www.google.comzzz") is False
-    assert is_connected("www.facebook.com")
-    assert is_connected("aadsfff.com") is False

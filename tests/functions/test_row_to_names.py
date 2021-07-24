3257eff6cb17296f700d75b33b06e63ff1e41f86
fireddd <mailtoshubhransh@gmail.com>
2021-07-25 02:28:59 +0530

    Update the tests for row_to_names function

--- tests/functions/test_row_to_names.py
+++ tests/functions/test_row_to_names.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import pytest
 
 
@@ -24,13 +25,11 @@ def test_row_to_names_delete_this_row(dataframe):
 
 @pytest.mark.functions
 def test_row_to_names_delete_the_row_without_resetting_index(dataframe):
+    """Test that executes row_to_names while deleting the given row
+    index while not resetting the index"""
     df = dataframe.row_to_names(2, remove_row=True, reset_index=False)
-    assert df.iloc[2, 0] == 1
-    assert df.iloc[2, 1] == 1.234_523_45
-    assert df.iloc[2, 2] == 1
-    assert df.iloc[2, 3] == "rabbit"
-    assert df.iloc[2, 4] == "Cambridge"
-    assert df.iloc[2].name == 3
+    expected_index = pd.Index([0, 1, 3, 4, 5, 6, 7, 9])
+    pd.testing.assert_index_equal(df.index, expected_index)
 
 
 @pytest.mark.functions
@@ -46,10 +45,8 @@ def test_row_to_names_delete_above(dataframe):
 
 @pytest.mark.functions
 def test_row_to_names_delete_above_without_resetting_index(dataframe):
+    """Test that executes row_to_names while deleting the all rows
+    above the given row index while not resetting the index"""
     df = dataframe.row_to_names(2, remove_rows_above=True, reset_index=False)
-    assert df.iloc[0, 0] == 3
-    assert df.iloc[0, 1] == 3.234_612_5
-    assert df.iloc[0, 2] == 3
-    assert df.iloc[0, 3] == "lion"
-    assert df.iloc[0, 4] == "Basel"
-    assert df.iloc[0].name == 2
+    expected_index = pd.Index([2, 3, 4, 5, 6, 7, 8])
+    pd.testing.assert_index_equal(df.index, expected_index)

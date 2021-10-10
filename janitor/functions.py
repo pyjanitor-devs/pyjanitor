6d616a50de0784218523ade05e334d4ed81f6a65
Farhan Reynaldo <xynxnx@gmail.com>
2021-10-10 09:56:48 +0700

    [ENH] Handle null values in concatenate_columns function (#935)
    * handle null values when concatenate columns
    
    * fix the ignore empty logic
    
    * add ignore_empty to docstrings
    
    * change from lambda to def
    
    * avoid imputing the original columns
    
    * create unittest for handling null values
    
    Co-authored-by: Farhan Reynaldo <farhanreynaldo@gmail.com>

--- janitor/functions.py
+++ janitor/functions.py
@@ -1372,6 +1372,7 @@ def concatenate_columns(
     column_names: List[Hashable],
     new_column_name,
     sep: str = "-",
+    ignore_empty: bool = True,
 ) -> pd.DataFrame:
     """Concatenates the set of columns into a single column.
 
@@ -1401,19 +1402,26 @@ def concatenate_columns(
     :param column_names: A list of columns to concatenate together.
     :param new_column_name: The name of the new column.
     :param sep: The separator between each column's data.
+    :param ignore_empty: Ignore null values if exists.
     :returns: A pandas DataFrame with concatenated columns.
     :raises JanitorError: if at least two columns are not provided
         within ``column_names``.
     """
     if len(column_names) < 2:
         raise JanitorError("At least two columns must be specified")
-    for i, col in enumerate(column_names):
-        if i == 0:
-            df[new_column_name] = df[col].astype(str)
-        else:
-            df[new_column_name] = (
-                df[new_column_name] + sep + df[col].astype(str)
-            )
+
+    df[new_column_name] = (
+        df[column_names].fillna("").astype(str).agg(sep.join, axis=1)
+    )
+
+    if ignore_empty:
+
+        def remove_empty_string(x):
+            return sep.join(x for x in x.split(sep) if x)
+
+        df[new_column_name] = df[new_column_name].transform(
+            remove_empty_string
+        )
 
     return df
 

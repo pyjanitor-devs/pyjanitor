303e228aae6841057abf48591976974f187c94f9
Eric Ma <ericmjl@users.noreply.github.com>
2021-10-23 23:18:07 -0400

    Update tests/functions/test_process_text.py
    Co-authored-by: Logan Thomas <logan.thomas005@gmail.com>

--- tests/functions/test_process_text.py
+++ tests/functions/test_process_text.py
@@ -193,7 +193,7 @@ def test_str_get():
 
 
 def test_str_lower():
-    """Test string conversion to lowercase using `.str.lower()``."""
+    """Test string conversion to lowercase using `.str.lower()`."""
 
     df = pd.DataFrame(
         {

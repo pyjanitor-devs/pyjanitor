816edec14fc5f64986ea87c6bd15c25741da55ae
Jeremy Goh <jeremy_gsk@outlook.com>
2022-01-02 16:44:19 +0800

    Address code review

--- janitor/functions/bin_numeric.py
+++ janitor/functions/bin_numeric.py
@@ -1,8 +1,8 @@
-from typing import Hashable, Optional, Union, Sequence
+from typing import Optional, Union, Sequence
 import pandas_flavor as pf
 import pandas as pd
 
-from janitor.utils import deprecated_alias
+from janitor.utils import check, check_column, deprecated_alias
 
 
 ScalarSequence = Sequence[float]
@@ -16,34 +16,36 @@ ScalarSequence = Sequence[float]
 )
 def bin_numeric(
     df: pd.DataFrame,
-    from_column_name: Hashable,
-    to_column_name: Hashable,
+    from_column_name: str,
+    to_column_name: str,
     bins: Optional[Union[int, ScalarSequence, pd.IntervalIndex]] = 5,
     **kwargs,
 ) -> pd.DataFrame:
     """
     Generate a new column that labels bins for a specified numeric column.
 
-    This method mutates the original DataFrame.
+    This method does not mutate the original DataFrame.
 
     A wrapper around the pandas [`cut()`][pd_cut_docs] function to bin data of
     one column, generating a new column with the results.
 
     [pd_cut_docs]: https://pandas.pydata.org/docs/reference/api/pandas.cut.html
 
-    ```python
-    import pandas as pd
-    import janitor
-    df = (
-        pd.DataFrame(...)
-        .bin_numeric(
-            from_column_name='col1',
-            to_column_name='col1_binned',
-            bins=3,
-            labels=['1-2', '3-4', '5-6'],
-        )
-    )
-    ```
+    Example: Binning a numeric column with specific bin edges.
+
+        >>> import pandas as pd
+        >>> import janitor
+        >>> df = pd.DataFrame({"a": [3, 6, 9, 12, 15]})
+        >>> df.bin_numeric(
+        ...     from_column_name="a", to_column_name="a_binned",
+        ...     bins=[0, 5, 11, 15],
+        ... )
+            a  a_binned
+        0   3    (0, 5]
+        1   6   (5, 11]
+        2   9   (5, 11]
+        3  12  (11, 15]
+        4  15  (11, 15]
 
     :param df: A pandas DataFrame.
     :param from_column_name: The column whose data you want binned.
@@ -52,15 +54,22 @@ def bin_numeric(
         documentation for more details.
     :param **kwargs: Additional kwargs to pass to `pd.cut`, except `retbins`.
     :return: A pandas DataFrame.
+    :raises ValueError: If `from_column_name` is not a column of `df`.
     :raises ValueError: If `retbins` is passed in as a kwarg.
+    :raises TypeError: If `from_column_name` or `to_column_name` are not string
+        types.
     """
     if "retbins" in kwargs:
         raise ValueError("`retbins` is not an acceptable keyword argument.")
 
-    df[str(to_column_name)] = pd.cut(
-        df[str(from_column_name)],
-        bins=bins,
-        **kwargs,
+    check("from_column_name", from_column_name, [str])
+    check("to_column_name", to_column_name, [str])
+    check_column(df, from_column_name)
+
+    df = df.assign(
+        **{
+            to_column_name: pd.cut(df[from_column_name], bins=bins, **kwargs),
+        }
     )
 
     return df

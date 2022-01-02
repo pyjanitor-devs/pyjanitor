ef60a27e7b3878c33daa6dc37a7f4e97c52dc37c
Jeremy Goh <jeremy_gsk@outlook.com>
2022-01-02 19:56:40 +0800

    Update reorder_columns docstring with MWE

--- janitor/functions/reorder_columns.py
+++ janitor/functions/reorder_columns.py
@@ -11,29 +11,30 @@ def reorder_columns(
 ) -> pd.DataFrame:
     """Reorder DataFrame columns by specifying desired order as list of col names.
 
-    Columns not specified retain their order and follow after specified cols.
+    Columns not specified retain their order and follow after the columns specified
+    in `column_order`.
 
-    Validates column_order to ensure columns are all present in DataFrame.
+    All columns specified within the `column_order` list must be present within `df`.
 
     This method does not mutate the original DataFrame.
 
-    Functional usage syntax:
+    Example:
 
-    Given `DataFrame` with column names `col1`, `col2`, `col3`:
+        >>> import pandas as pd
+        >>> import janitor
+        >>> df = pd.DataFrame({"col1": [1, 1, 1], "col2": [2, 2, 2], "col3": [3, 3, 3]})
+        >>> df
+           col1  col2  col3
+        0     1     2     3
+        1     1     2     3
+        2     1     2     3
+        >>> df.reorder_columns(['col3', 'col1'])
+           col3  col1  col2
+        0     3     1     2
+        1     3     1     2
+        2     3     1     2
 
-    ```python
-    df = reorder_columns(df, ['col2', 'col3'])
-    ```
-
-    Method chaining syntax:
-
-    ```python
-    import pandas as pd
-    import janitor
-    df = pd.DataFrame(...).reorder_columns(['col2', 'col3'])
-    ```
-
-    The column order of `df` is now `col2`, `col3`, `col1`.
+    Notice that the column order of `df` is now `col3`, `col1`, `col2`.
 
     Internally, this function uses `DataFrame.reindex` with `copy=False`
     to avoid unnecessary data duplication.
@@ -44,12 +45,12 @@ def reorder_columns(
     :returns: A pandas DataFrame with reordered columns.
     :raises IndexError: if a column within `column_order` is not found
         within the DataFrame.
-    """
+    """  # noqa: E501
     check("column_order", column_order, [list, tuple, pd.Index])
 
     if any(col not in df.columns for col in column_order):
         raise IndexError(
-            "A column in `column_order` was not found in the DataFrame."
+            "One or more columns in `column_order` were not found in the DataFrame."
         )
 
     # if column_order is a Pandas index, needs conversion to list:

9bb30b88662e4523e93e0c67a13adad8953366be
Eric Ma <ericmjl@users.noreply.github.com>
2022-01-22 13:52:02 +0000

    Simplify column names in example.

--- janitor/functions/drop_constant_columns.py
+++ janitor/functions/drop_constant_columns.py
@@ -14,22 +14,22 @@ def drop_constant_columns(df: pd.DataFrame) -> pd.DataFrame:
         >>> import janitor
         >>> data_dict = {
         ...     "a": [1, 1, 1] ,
-        ...     "Bell__Chart": [1, 2, 3] ,
-        ...     "decorated-elephant": [1, 1, 1] ,
-        ...     "animals": ["rabbit", "leopard", "lion"] ,
-        ...     "cities": ["Cambridge", "Shanghai", "Basel"]
+        ...     "b": [1, 2, 3] ,
+        ...     "c": [1, 1, 1] ,
+        ...     "d": ["rabbit", "leopard", "lion"] ,
+        ...     "e": ["Cambridge", "Shanghai", "Basel"]
         ... }
         >>> df = pd.DataFrame(data_dict)
         >>> df
-           a  Bell__Chart  decorated-elephant  animals     cities
-        0  1            1                   1   rabbit  Cambridge
-        1  1            2                   1  leopard   Shanghai
-        2  1            3                   1     lion      Basel
+           a  b  c        d          e
+        0  1  1  1   rabbit  Cambridge
+        1  1  2  1  leopard   Shanghai
+        2  1  3  1     lion      Basel
         >>> df.drop_constant_columns()
-           Bell__Chart  animals     cities
-        0            1   rabbit  Cambridge
-        1            2  leopard   Shanghai
-        2            3     lion      Basel
+           b        d          e
+        0  1   rabbit  Cambridge
+        1  2  leopard   Shanghai
+        2  3     lion      Basel
 
     :param df: Input Pandas DataFrame
     :returns: The Pandas DataFrame with the constant columns dropped.

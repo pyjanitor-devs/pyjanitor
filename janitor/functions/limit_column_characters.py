5f6f77eec58acbcafbdf30251487259a220e8e45
Jeremy Goh <jeremy_gsk@outlook.com>
2022-01-25 21:15:48 +0800

    Add MWE to docstring of limit_column_characters

--- janitor/functions/limit_column_characters.py
+++ janitor/functions/limit_column_characters.py
@@ -1,3 +1,4 @@
+"""Implementation of limit_column_characters."""
 import pandas_flavor as pf
 import pandas as pd
 
@@ -6,7 +7,9 @@ from janitor.utils import check
 
 @pf.register_dataframe_method
 def limit_column_characters(
-    df: pd.DataFrame, column_length: int, col_separator: str = "_"
+    df: pd.DataFrame,
+    column_length: int,
+    col_separator: str = "_",
 ) -> pd.DataFrame:
     """Truncate column sizes to a specific length.
 
@@ -16,7 +19,29 @@ def limit_column_characters(
     a given separator character with the index of duplicate columns, except
     for the first distinct column name.
 
-    :param df: A pandas dataframe.
+    Example:
+
+        >>> import pandas as pd
+        >>> import janitor
+        >>> data_dict = {
+        ...     "really_long_name": [9, 8, 7],
+        ...     "another_really_long_name": [2, 4, 6],
+        ...     "another_really_longer_name": list("xyz"),
+        ...     "this_is_getting_out_of_hand": list("pqr"),
+        ... }
+        >>> df = pd.DataFrame(data_dict)
+        >>> df
+           really_long_name  another_really_long_name another_really_longer_name this_is_getting_out_of_hand
+        0                 9                         2                          x                           p
+        1                 8                         4                          y                           q
+        2                 7                         6                          z                           r
+        >>> df.limit_column_characters(7)
+           really_  another another_1 this_is
+        0        0        2         x       p
+        1        1        4         y       q
+        2        2        6         z       r
+
+    :param df: A pandas DataFrame.
     :param column_length: Character length for which to truncate all columns.
         The column separator value and number for duplicate column name does
         not contribute. Therefore, if all columns are truncated to 10
@@ -24,68 +49,11 @@ def limit_column_characters(
         remaining will be 12 characters (assuming a column separator of one
         character).
     :param col_separator: The separator to use for counting distinct column
-        values. I think an underscore looks nicest, however a period is a
-        common option as well. Supply an empty string (i.e. '') to remove the
-        separator.
+        values, for example, `'_'` or `'.'`.
+        Supply an empty string (i.e. `''`) to remove the separator.
     :returns: A pandas DataFrame with truncated column lengths.
-    """
-    # :Example Setup:
+    """  # noqa: E501
 
-    # ```python
-
-    #     import pandas as pd
-    #     import janitor
-    #     data_dict = {
-    #         "really_long_name_for_a_column": range(10),
-    #         "another_really_long_name_for_a_column": \
-    #         [2 * item for item in range(10)],
-    #         "another_really_longer_name_for_a_column": list("lllongname"),
-    #         "this_is_getting_out_of_hand": list("longername"),
-    #     }
-
-    # :Example: Standard truncation:
-
-    # ```python
-
-    #     example_dataframe = pd.DataFrame(data_dict)
-    #     example_dataframe.limit_column_characters(7)
-
-    # :Output:
-
-    # ```python
-
-    #            really_  another another_1 this_is
-    #     0        0        0         l       l
-    #     1        1        2         l       o
-    #     2        2        4         l       n
-    #     3        3        6         o       g
-    #     4        4        8         n       e
-    #     5        5       10         g       r
-    #     6        6       12         n       n
-    #     7        7       14         a       a
-    #     8        8       16         m       m
-    #     9        9       18         e       e
-
-    # :Example: Standard truncation with different separator character:
-
-    # ```python
-
-    #     example_dataframe2 = pd.DataFrame(data_dict)
-    #     example_dataframe2.limit_column_characters(7, ".")
-
-    # ```python
-
-    #            really_  another another.1 this_is
-    #     0        0        0         l       l
-    #     1        1        2         l       o
-    #     2        2        4         l       n
-    #     3        3        6         o       g
-    #     4        4        8         n       e
-    #     5        5       10         g       r
-    #     6        6       12         n       n
-    #     7        7       14         a       a
-    #     8        8       16         m       m
-    #     9        9       18         e       e
     check("column_length", column_length, [int])
     check("col_separator", col_separator, [str])
 

fd63b112e818c526f063a3a3ed9e89309a327bf0
Jeremy Goh <jeremy_gsk@outlook.com>
2022-02-04 14:15:52 +0800

    Add MWE to shuffle docstring

--- janitor/functions/shuffle.py
+++ janitor/functions/shuffle.py
@@ -1,3 +1,4 @@
+"""Implementation of `shuffle` functions."""
 import pandas_flavor as pf
 import pandas as pd
 
@@ -13,14 +14,34 @@ def shuffle(
     Super-sugary syntax! Underneath the hood, we use `df.sample(frac=1)`,
     with the option to set the random state.
 
-    Example usage:
-    ```python
-        df = pd.DataFrame(...).shuffle()
-    ```
+    Example:
 
-    :param df: A pandas DataFrame
-    :param random_state: (optional) A seed for the random number generator.
-    :param reset_index: (optional) Resets index to default integers
+        >>> import pandas as pd
+        >>> import janitor
+        >>> df = pd.DataFrame({
+        ...     "col1": range(5),
+        ...     "col2": list("abcde"),
+        ... })
+        >>> df
+           col1 col2
+        0     0    a
+        1     1    b
+        2     2    c
+        3     3    d
+        4     4    e
+        >>> df.shuffle(random_state=42)
+           col1 col2
+        0     1    b
+        1     4    e
+        2     2    c
+        3     0    a
+        4     3    d
+
+    :param df: A pandas DataFrame.
+    :param random_state: If provided, set a seed for the random number
+        generator.
+    :param reset_index: If True, reset the dataframe index to the default
+        RangeIndex.
     :returns: A shuffled pandas DataFrame.
     """
     result = df.sample(frac=1, random_state=random_state)

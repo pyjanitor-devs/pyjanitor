fd79404bba2fd7ea92ed8a41eb1383eb85557483
Jeremy Goh <30731072+thatlittleboy@users.noreply.github.com>
2022-02-05 11:45:47 +0800

    [DOC] Add MWE to 3 more functions (#1006)
    * Add MWE for row_to_names docstring
    
    * Change default arg of row_number to 0
    
    Related to #1004.
    
    * Remove empty `fill_empty.py` file
    
    * Add MWE to shuffle docstring
    
    * Add MWE for `round_to_fraction` docstring
    
    * Fix doctest bug

--- janitor/functions/row_to_names.py
+++ janitor/functions/row_to_names.py
@@ -1,3 +1,4 @@
+"""Implementation of the `row_to_names` function."""
 import warnings
 import pandas_flavor as pf
 import pandas as pd
@@ -8,7 +9,7 @@ from janitor.utils import check
 @pf.register_dataframe_method
 def row_to_names(
     df: pd.DataFrame,
-    row_number: int = None,
+    row_number: int = 0,
     remove_row: bool = False,
     remove_rows_above: bool = False,
     reset_index: bool = False,
@@ -20,22 +21,46 @@ def row_to_names(
     Contains options to remove the elevated row from the DataFrame along with
     removing the rows above the selected row.
 
-    Method chaining usage:
+    Example: Replace column names with the first row and reset the index.
 
+        >>> import pandas as pd
+        >>> import janitor
+        >>> df = pd.DataFrame({
+        ...     "a": ["nums", 6, 9],
+        ...     "b": ["chars", "x", "y"],
+        ... })
+        >>> df
+              a      b
+        0  nums  chars
+        1     6      x
+        2     9      y
+        >>> df.row_to_names(0, remove_row=True, reset_index=True)
+          nums chars
+        0    6     x
+        1    9     y
 
+    Example: Remove rows above the elevated row and the elevated row itself.
 
-        df = (
-            pd.DataFrame(...)
-            .row_to_names(
-                row_number=0,
-                remove_row=False,
-                remove_rows_above=False,
-                reset_index=False,
-            )
-        )
+        >>> import pandas as pd
+        >>> import janitor
+        >>> df = pd.DataFrame({
+        ...     "a": ["bla1", "nums", 6, 9],
+        ...     "b": ["bla2", "chars", "x", "y"],
+        ... })
+        >>> df
+              a      b
+        0  bla1   bla2
+        1  nums  chars
+        2     6      x
+        3     9      y
+        >>> df.row_to_names(1, remove_row=True, remove_rows_above=True, reset_index=True)
+          nums chars
+        0    6     x
+        1    9     y
 
     :param df: A pandas DataFrame.
-    :param row_number: The row containing the variable names
+    :param row_number: Position of the row containing the variable names.
+        Note that indexing starts from 0. Defaults to 0 (first row).
     :param remove_row: Whether the row should be removed from the DataFrame.
         Defaults to False.
     :param remove_rows_above: Whether the rows above the selected row should
@@ -43,130 +68,7 @@ def row_to_names(
     :param reset_index: Whether the index should be reset on the returning
         DataFrame. Defaults to False.
     :returns: A pandas DataFrame with set column names.
-    """
-    # :Setup:
-
-    # ```python
-
-    #     import pandas as pd
-    #     import janitor
-    #     data_dict = {
-    #         "a": [1, 2, 3] * 3,
-    #         "Bell__Chart": [1, 2, 3] * 3,
-    #         "decorated-elephant": [1, 2, 3] * 3,
-    #         "animals": ["rabbit", "leopard", "lion"] * 3,
-    #         "cities": ["Cambridge", "Shanghai", "Basel"] * 3
-    #     }
-
-    # :Example: Move first row to column names:
-
-    # ```python
-
-    #     example_dataframe = pd.DataFrame(data_dict)
-    #     example_dataframe.row_to_names(0)
-
-    # :Output:
-
-    # ```python
-
-    #        1  1  1   rabbit  Cambridge
-    #     0  1  1  1   rabbit  Cambridge
-    #     1  2  2  2  leopard   Shanghai
-    #     2  3  3  3     lion      Basel
-    #     3  1  1  1   rabbit  Cambridge
-    #     4  2  2  2  leopard   Shanghai
-    #     5  3  3  3     lion      Basel
-    #     6  1  1  1   rabbit  Cambridge
-    #     7  2  2  2  leopard   Shanghai
-
-    # :Example: Move first row to column names and
-    #  remove row while resetting the index:
-
-    # ```python
-
-    #     example_dataframe = pd.DataFrame(data_dict)
-    #     example_dataframe.row_to_names(0, remove_row=True,\
-    #       reset_index=True)
-
-    # :Output:
-
-    # ```python
-
-    #       1   1   1   rabbit  Cambridge
-    #   0   2   2   2   leopard Shanghai
-    #   1   3   3   3   lion    Basel
-    #   2   1   1   1   rabbit  Cambridge
-    #   3   2   2   2   leopard Shanghai
-    #   4   3   3   3   lion    Basel
-    #   5   1   1   1   rabbit  Cambridge
-    #   6   2   2   2   leopard Shanghai
-    #   7   3   3   3   lion    Basel
-
-    # :Example: Move first row to column names and remove
-    #   row without resetting the index:
-
-    # ```python
-
-    #     example_dataframe = pd.DataFrame(data_dict)
-    #     example_dataframe.row_to_names(0, remove_row=True)
-
-    # :Output:
-
-    # ```python
-
-    #        1  1  1   rabbit  Cambridge
-    #     1  2  2  2  leopard   Shanghai
-    #     2  3  3  3     lion      Basel
-    #     3  1  1  1   rabbit  Cambridge
-    #     4  2  2  2  leopard   Shanghai
-    #     5  3  3  3     lion      Basel
-    #     6  1  1  1   rabbit  Cambridge
-    #     7  2  2  2  leopard   Shanghai
-    #     8  3  3  3     lion      Basel
-
-    # :Example: Move first row to column names, remove row
-    #   and remove rows above selected row without resetting
-    #   index:
-
-    # ```python
-
-    #     example_dataframe = pd.DataFrame(data_dict)
-    #     example_dataframe.row_to_names(2, remove_row=True, \
-    #       remove_rows_above=True, reset_index= True)
-
-    # :Output:
-
-    # ```python
-
-    #       3   3   3   lion    Basel
-    #   0   1   1   1   rabbit  Cambridge
-    #   1   2   2   2   leopard Shanghai
-    #   2   3   3   3   lion    Basel
-    #   3   1   1   1   rabbit  Cambridge
-    #   4   2   2   2   leopard Shanghai
-    #   5   3   3   3   lion    Basel
-
-    # :Example: Move first row to column names, remove row,
-    # and remove rows above selected row without resetting
-    # index:
-
-    # ```python
-
-    #     example_dataframe = pd.DataFrame(data_dict)
-    #     example_dataframe.row_to_names(2, remove_row=True, \
-    #       remove_rows_above=True)
-
-    # :Output:
-
-    # ```python
-
-    #        3  3  3     lion      Basel
-    #     3  1  1  1   rabbit  Cambridge
-    #     4  2  2  2  leopard   Shanghai
-    #     5  3  3  3     lion      Basel
-    #     6  1  1  1   rabbit  Cambridge
-    #     7  2  2  2  leopard   Shanghai
-    #     8  3  3  3     lion      Basel
+    """  # noqa: E501
 
     check("row_number", row_number, [int])
 

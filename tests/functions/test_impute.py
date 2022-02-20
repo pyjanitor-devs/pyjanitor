142a044d2d9f98d35018da40a555cef8f1cca5a1
Jeremy Goh <jeremy_gsk@outlook.com>
2022-02-20 23:48:20 +0800

    Bring impute to 100% coverage

--- tests/functions/test_impute.py
+++ tests/functions/test_impute.py
@@ -1,8 +1,10 @@
+"""Tests for the `impute` functions"""
 import pytest
 
 
 @pytest.mark.functions
 def test_impute_single_value(missingdata_df):
+    """Check if constant value is imputed correctly."""
     df = missingdata_df.impute("a", 5)
     assert set(df["a"]) == set([1, 2, 5])
 
@@ -22,5 +24,20 @@ def test_impute_single_value(missingdata_df):
     ],
 )
 def test_impute_statistical(missingdata_df, statistic, expected):
+    """Check if imputing via statistic_column_name works correctly."""
     df = missingdata_df.impute("a", statistic_column_name=statistic)
     assert set(df["a"]) == expected
+
+
+@pytest.mark.functions
+def test_impute_error_with_invalid_inputs(missingdata_df):
+    """Check errors are properly raised with invalid inputs."""
+    with pytest.raises(ValueError):
+        _ = missingdata_df.impute(
+            "a",
+            value=0,
+            statistic_column_name="mean",
+        )
+
+    with pytest.raises(KeyError):
+        _ = missingdata_df.impute("a", statistic_column_name="foobar")

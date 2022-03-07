75a9160e5b7417c3c10e7c45624a1d83242d87c6
Samuel Oranyeli <samueloranyeli@gmail.com>
2022-03-08 00:30:01 +1100

    [ENH] Peformance improvement for `expand_grid` (#1033)
    * expand_grid improvements
    
    * expand_grid improvements
    
    * expand_grid improvements
    
    * dirty speed tests
    
    * dirty speed tests
    
    * cleanup utils, notebook, docs, tests
    
    * changelog
    
    * docs update
    
    * add more tests
    
    * notebook fix
    
    * fix comments
    
    * fix comments
    
    * Update janitor/utils.py
    
    Co-authored-by: Jeremy Goh <30731072+thatlittleboy@users.noreply.github.com>
    
    * updates based on feedback
    
    * notebook update - spelling fix
    
    * minor update
    
    * minor update
    
    * Update janitor/functions/complete.py
    
    Co-authored-by: Jeremy Goh <30731072+thatlittleboy@users.noreply.github.com>
    
    * Update janitor/functions/utils.py
    
    Co-authored-by: Jeremy Goh <30731072+thatlittleboy@users.noreply.github.com>
    
    * Update janitor/functions/complete.py
    
    Co-authored-by: Jeremy Goh <30731072+thatlittleboy@users.noreply.github.com>
    
    * Update janitor/functions/complete.py
    
    Co-authored-by: Jeremy Goh <30731072+thatlittleboy@users.noreply.github.com>
    
    * add description for sort parameter
    
    * add comments
    
    * version adjustment for mkdocs
    
    * version adjustment for mkdocstrings
    
    * minor
    
    Co-authored-by: samukweku <samukweku@gmail.com>
    Co-authored-by: sammychoco <samuel.oranyeli@slalom.com>
    Co-authored-by: Jeremy Goh <30731072+thatlittleboy@users.noreply.github.com>

--- tests/functions/test_expand_grid.py
+++ tests/functions/test_expand_grid.py
@@ -18,8 +18,8 @@ def test_others_not_dict(df):
 
 
 @given(df=df_strategy())
-def test_others_None(df):
-    """Return DataFrame if no `others`."""
+def test_others_none(df):
+    """Return DataFrame if no `others`, and df exists."""
     assert_frame_equal(df.expand_grid("df"), df)
 
 
@@ -30,14 +30,14 @@ def test_others_empty():
 
 @given(df=df_strategy())
 def test_df_key(df):
-    """Raise error if dataframe key is not supplied."""
+    """Raise error if df exists and df_key is not supplied."""
     with pytest.raises(KeyError):
         expand_grid(df, others={"y": [5, 4, 3, 2, 1]})
 
 
 @given(df=df_strategy())
 def test_df_key_hashable(df):
-    """Raise error if dataframe key is not Hashable."""
+    """Raise error if df exists and df_key is not Hashable."""
     with pytest.raises(TypeError):
         expand_grid(df, df_key=["a"], others={"y": [5, 4, 3, 2, 1]})
 
@@ -54,19 +54,19 @@ def test_numpy_gt_2d():
         expand_grid(others={"x": np.array([[[2, 3]]])})
 
 
-def test_Series_empty():
+def test_series_empty():
     """Raise ValueError if Series is empty."""
     with pytest.raises(ValueError):
         expand_grid(others={"x": pd.Series([], dtype=int)})
 
 
-def test_DataFrame_empty():
-    """Raise ValueError if Series is empty."""
+def test_dataframe_empty():
+    """Raise ValueError if DataFrame is empty."""
     with pytest.raises(ValueError):
         expand_grid(others={"x": pd.DataFrame([])})
 
 
-def test_Index_empty():
+def test_index_empty():
     """Raise ValueError if Index is empty."""
     with pytest.raises(ValueError):
         expand_grid(others={"x": pd.Index([], dtype=int)})
@@ -74,8 +74,8 @@ def test_Index_empty():
 
 @settings(deadline=None)
 @given(df=df_strategy())
-def test_Series(df):
-    """Test expand_grid output"""
+def test_series(df):
+    """Test expand_grid output for Series input."""
     A = df["a"]
     B = df["cities"]
     others = {"A": A, "B": B}
@@ -92,8 +92,8 @@ def test_Series(df):
 
 @settings(deadline=None)
 @given(df=df_strategy())
-def test_Series_DataFrame(df):
-    """Test expand_grid output"""
+def test_series_dataframe(df):
+    """Test expand_grid output for Series and DataFrame inputs."""
     A = df["a"]
     B = df.iloc[:, [1, 2]]
     others = {"A": A, "B": B}
@@ -109,8 +109,11 @@ def test_Series_DataFrame(df):
 
 @settings(deadline=None)
 @given(df=df_strategy())
-def test_Series_MultiIndex_DataFrame(df):
-    """Test expand_grid output"""
+def test_series_multiindex_dataframe(df):
+    """
+    Test expand_grid output
+    if the DataFrame's columns is a MultiIndex.
+    """
     A = df["a"]
     B = df.iloc[:, [1, 2]]
     B.columns = pd.MultiIndex.from_arrays([["C", "D"], B.columns])
@@ -129,7 +132,7 @@ def test_Series_MultiIndex_DataFrame(df):
 @settings(deadline=None)
 @given(df=df_strategy())
 def test_numpy_1d(df):
-    """Test expand_grid output"""
+    """Test expand_grid output for a 1D numpy array."""
     A = df["a"].to_numpy()
     B = df["cities"]
     others = {"A": A, "B": B}
@@ -147,7 +150,7 @@ def test_numpy_1d(df):
 @settings(deadline=None)
 @given(df=categoricaldf_strategy())
 def test_numpy_2d(df):
-    """Test expand_grid output"""
+    """Test expand_grid output for a 2D numpy array"""
     A = df["names"]
     base = df.loc[:, ["numbers"]].assign(num=df.numbers * 4)
     B = base.to_numpy(dtype=int)
@@ -165,8 +168,8 @@ def test_numpy_2d(df):
 
 @settings(deadline=None)
 @given(df=df_strategy())
-def test_Index(df):
-    """Test expand_grid output"""
+def test_index(df):
+    """Test expand_grid output for a pandas Index that has a name."""
     A = pd.Index(df["a"])
     B = df["cities"]
     others = {"A": A, "B": B}
@@ -182,9 +185,25 @@ def test_Index(df):
 
 
 @settings(deadline=None)
+@given(df=df_strategy())
+def test_index_name_none(df):
+    """Test expand_grid output for a pandas Index without a name."""
+    A = pd.Index(df["a"].array, name=None)
+    B = df["cities"]
+    others = {"A": A, "B": B}
+    result = expand_grid(others=others)
+    A = df.loc[:, ["a"]]
+    B = df.loc[:, ["cities"]]
+    expected = A.assign(key=1).merge(B.assign(key=1), on="key")
+    expected = expected.drop(columns="key")
+    expected.columns = pd.MultiIndex.from_arrays([["A", "B"], [0, "cities"]])
+    assert_frame_equal(result, expected)
+
+
+@settings(deadline=None)
 @given(df=categoricaldf_strategy())
-def test_MultiIndex(df):
-    """Test expand_grid output"""
+def test_multiindex(df):
+    """Test expand_grid output for a pandas MultiIndex with a name."""
     A = df["names"]
     base = df.loc[:, ["numbers"]].assign(num=df.numbers * 4)
     B = pd.MultiIndex.from_frame(base)
@@ -201,9 +220,28 @@ def test_MultiIndex(df):
 
 
 @settings(deadline=None)
+@given(df=categoricaldf_strategy())
+def test_multiindex_names_none(df):
+    """Test expand_grid output for a pandas MultiIndex without a name."""
+    A = df["names"]
+    base = df.loc[:, ["numbers"]].assign(num=df.numbers * 4)
+    B = pd.MultiIndex.from_frame(base, names=[None, None])
+    others = {"A": A, "B": B}
+    result = expand_grid(others=others)
+    A = df.loc[:, ["names"]]
+    B = base.copy()
+    expected = A.assign(key=1).merge(B.assign(key=1), on="key")
+    expected = expected.drop(columns="key")
+    expected.columns = pd.MultiIndex.from_arrays(
+        [["A", "B", "B"], ["names", 0, 1]]
+    )
+    assert_frame_equal(result, expected)
+
+
+@settings(deadline=None)
 @given(df=df_strategy())
-def test_Pandas_extension_array(df):
-    """Test expand_grid output"""
+def test_pandas_extension_array(df):
+    """Test expand_grid output for a pandas array."""
     A = df["a"]
     B = df["cities"].astype("string").array
     others = {"A": A, "B": B}
@@ -221,7 +259,7 @@ def test_Pandas_extension_array(df):
 @settings(deadline=None)
 @given(df=df_strategy())
 def test_sequence(df):
-    """Test expand_grid output"""
+    """Test expand_grid output for list."""
     A = df["a"].to_list()
     B = df["cities"]
     others = {"A": A, "B": B}
@@ -239,7 +277,7 @@ def test_sequence(df):
 @settings(deadline=None)
 @given(df=df_strategy())
 def test_scalar(df):
-    """Test expand_grid output"""
+    """Test expand_grid output for a scalar value."""
     A = df["a"]
     B = 2
     others = {"A": A, "B": B}
@@ -250,5 +288,39 @@ def test_scalar(df):
     expected = expected.drop(columns="key")
     expected.columns = pd.MultiIndex.from_arrays(
         [["A", "B"], expected.columns]
+    )
+    assert_frame_equal(result, expected)
+
+
+@settings(deadline=None)
+@given(df=df_strategy())
+def test_chain_df(df):
+    """Test expand_grid in a method-chain operation."""
+    A = df["a"]
+    B = df[["cities"]]
+    others = {"A": A}
+    result = B.expand_grid(df_key="city", others=others)
+    A = df.loc[:, ["a"]]
+    expected = B.assign(key=1).merge(A.assign(key=1), on="key")
+    expected = expected.drop(columns="key")
+    expected.columns = pd.MultiIndex.from_arrays(
+        [["city", "A"], expected.columns]
+    )
+    assert_frame_equal(result, expected)
+
+
+@settings(deadline=None)
+@given(df=df_strategy())
+def test_series_name(df):
+    """Test expand_grid where the Series has no name."""
+    A = df["a"].rename(None)
+    B = df[["cities"]]
+    others = {"A": A}
+    result = B.expand_grid(df_key="city", others=others)
+    A = df.loc[:, ["a"]]
+    expected = B.assign(key=1).merge(A.assign(key=1), on="key")
+    expected = expected.drop(columns="key")
+    expected.columns = pd.MultiIndex.from_arrays(
+        [["city", "A"], ["cities", 0]]
     )
     assert_frame_equal(result, expected)

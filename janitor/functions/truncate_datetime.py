45873a729d31125f2b96a8e45ac47db48e25be14
Jeremy Goh <jeremy_gsk@outlook.com>
2022-03-13 00:31:54 +0800

    Add MWE and optimizations,bugfixes

--- janitor/functions/truncate_datetime.py
+++ janitor/functions/truncate_datetime.py
@@ -1,6 +1,7 @@
+"""Implementation of the `truncate_datetime` family of functions."""
+import datetime as dt
 import pandas_flavor as pf
 import pandas as pd
-import datetime as dt
 
 
 @pf.register_dataframe_method
@@ -16,25 +17,52 @@ def truncate_datetime_dataframe(
     Calling on existing df will not alter the contents
     of said df.
 
-    Note: Truncating down to a Month or Day will yield 0s,
-    as there is no 0 month or 0 day in most datetime systems.
+    This method does not mutate the original DataFrame.
+
+    Examples:
+
+        >>> import pandas as pd
+        >>> import janitor
+        >>> df = pd.DataFrame({
+        ...     "foo": ["xxxx", "yyyy", "zzzz"],
+        ...     "dt": pd.date_range("2020-03-11", periods=3),
+        ... })
+        >>> df
+            foo         dt
+        0  xxxx 2020-03-11
+        1  yyyy 2020-03-12
+        2  zzzz 2020-03-13
+        >>> df.truncate_datetime_dataframe("month")
+            foo         dt
+        0  xxxx 2020-03-01
+        1  yyyy 2020-03-01
+        2  zzzz 2020-03-01
 
     :param df: The pandas DataFrame on which to truncate datetime.
     :param datepart: Truncation precision, YEAR, MONTH, DAY,
         HOUR, MINUTE, SECOND. (String is automagically
         capitalized)
 
+    :raises ValueError: If an invalid `datepart` precision is passed in.
     :returns: A pandas DataFrame with all valid datetimes truncated down
         to the specified precision.
     """
-    for i in df.columns:
-        for j in df.index:
+    ACCEPTABLE_DATEPARTS = ("YEAR", "MONTH", "DAY", "HOUR", "MINUTE", "SECOND")
+    datepart = datepart.upper()
+    if datepart not in ACCEPTABLE_DATEPARTS:
+        raise ValueError(
+            "Received an invalid `datepart` precision. "
+            f"Please enter any one of {ACCEPTABLE_DATEPARTS}."
+        )
+
+    df = df.copy()
+
+    for col in df.columns:
+        for row in df.index:
             try:
-                df[i][j] = _truncate_datetime(datepart, df[i][j])
-            except KeyError:
-                pass
-            except TypeError:
-                pass
+                df.loc[row, col] = _truncate_datetime(
+                    datepart, df.loc[row, col]
+                )
             except AttributeError:
                 pass
 
@@ -42,23 +70,26 @@ def truncate_datetime_dataframe(
 
 
 def _truncate_datetime(datepart: str, timestamp: dt.datetime) -> dt.datetime:
-    """
+    """Truncate a given timestamp to the given datepart.
+
+    Data checks are assumed to have already been done; No further checks will
+    be performed within this internal function.
 
     :param datepart: Truncation precision, YEAR, MONTH, DAY,
-        HOUR, MINUTE, SECOND. (String is automagically
-        capitalized)
-    :param timestamp: Expecting a datetime from python datetime class (dt)
+        HOUR, MINUTE, SECOND.
+    :param timestamp: Expecting a datetime from python `datetime` class (dt).
+    :raises AttributeError: If the input timestamp is not a datetime-like
+        object.
     :returns: A truncated datetime object to the precision specified by
         datepart.
     """
     recurrence = [0, 1, 1, 0, 0, 0]  # [YEAR, MONTH, DAY, HOUR, MINUTE, SECOND]
-    datepart = datepart.upper()
     ENUM = {
         "YEAR": 0,
         "MONTH": 1,
         "DAY": 2,
         "HOUR": 3,
-        "MINUTE:": 4,
+        "MINUTE": 4,
         "SECOND": 5,
         0: timestamp.year,
         1: timestamp.month,
@@ -67,24 +98,8 @@ def _truncate_datetime(datepart: str, timestamp: dt.datetime) -> dt.datetime:
         4: timestamp.minute,
         5: timestamp.second,
     }
-    try:
-        ENUM[datepart]
-    # Capture the error but replace it with explicit instructions.
-    except KeyError:
-        msg = (
-            "Invalid truncation. Please enter any one of 'year', "
-            "'month', 'day', 'hour', 'minute' or 'second'."
-        )
-        raise KeyError(msg)
 
     for i in range(ENUM[datepart] + 1):
-        recurrence[i] = ENUM.get(i)
+        recurrence[i] = ENUM[i]
 
-    return dt.datetime(
-        recurrence[0],
-        recurrence[1],
-        recurrence[2],
-        recurrence[3],
-        recurrence[4],
-        recurrence[5],
-    )
+    return dt.datetime(*recurrence)

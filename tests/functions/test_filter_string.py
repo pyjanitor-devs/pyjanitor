fbf8d10790ac804beaf1ea98f35e2a51addf7fe3
Zero <Zeroto521@gmail.com>
2022-03-19 11:01:07 +0800

    Finished `filter_string` test cases

--- tests/functions/test_filter_string.py
+++ tests/functions/test_filter_string.py
@@ -4,13 +4,28 @@ import pytest
 @pytest.mark.functions
 def test_filter_string(dataframe):
     df = dataframe.filter_string(
-        column_name="animals@#$%^", search_string="bbit"
+        column_name="animals@#$%^",
+        search_string="bbit",
     )
+
     assert len(df) == 3
 
 
 def test_filter_string_complement(dataframe):
     df = dataframe.filter_string(
-        column_name="cities", search_string="hang", complement=True
+        column_name="cities",
+        search_string="hang",
+        complement=True,
     )
+
     assert len(df) == 6
+
+
+def test_filter_string_case(dataframe):
+    df = dataframe.filter_string(
+        column_name="cities",
+        search_string="B",
+        case=False,
+    )
+
+    assert len(df) == 1

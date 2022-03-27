dd50a15dbe6118c47b2a79fb3132fa48e8618a95
Jeremy Goh <jeremy_gsk@outlook.com>
2022-03-27 17:00:01 +0800

    Update count_cumulative_unique MWE and tests

--- janitor/functions/count_cumulative_unique.py
+++ janitor/functions/count_cumulative_unique.py
@@ -1,6 +1,9 @@
+"""Implementation of count_cumulative_unique."""
 from typing import Hashable
 import pandas_flavor as pf
 import pandas as pd
+
+from janitor.utils import check_column
 
 
 @pf.register_dataframe_method
@@ -12,73 +15,70 @@ def count_cumulative_unique(
 ) -> pd.DataFrame:
     """Generates a running total of cumulative unique values in a given column.
 
-    Functional usage syntax:
-
-    ```python
-        import pandas as pd
-        import janitor as jn
-
-        df = pd.DataFrame(...)
-
-        df = jn.functions.count_cumulative_unique(
-            df=df,
-            column_name='animals',
-            dest_column_name='animals_unique_count',
-            case_sensitive=True
-        )
-    ```
-
-    Method chaining usage example:
-
-    ```python
-        import pandas as pd
-        import janitor
-
-        df = pd.DataFrame(...)
-
-        df = df.count_cumulative_unique(
-            column_name='animals',
-            dest_column_name='animals_unique_count',
-            case_sensitive=True
-        )
-    ```
-
     A new column will be created containing a running
     count of unique values in the specified column.
     If `case_sensitive` is `True`, then the case of
     any letters will matter (i.e., `a != A`);
     otherwise, the case of any letters will not matter.
 
-    This method mutates the original DataFrame.
+    This method does not mutate the original DataFrame.
 
-    :param df: A pandas dataframe.
-    :param column_name: Name of the column containing
-        values from which a running count of unique values
-        will be created.
+    Examples:
+
+        >>> import pandas as pd
+        >>> import janitor
+        >>> df = pd.DataFrame({
+        ...     "letters": list("aabABb"),
+        ...     "numbers": range(4, 10),
+        ... })
+        >>> df
+          letters  numbers
+        0       a        4
+        1       a        5
+        2       b        6
+        3       A        7
+        4       B        8
+        5       b        9
+        >>> df.count_cumulative_unique(
+        ...     column_name="letters",
+        ...     dest_column_name="letters_unique_count",
+        ... )
+          letters  numbers  letters_unique_count
+        0       a        4                     1
+        1       a        5                     1
+        2       b        6                     2
+        3       A        7                     3
+        4       B        8                     4
+        5       b        9                     4
+
+    :param df: A pandas DataFrame.
+    :param column_name: Name of the column containing values from which a
+        running count of unique values will be created.
     :param dest_column_name: The name of the new column containing the
         cumulative count of unique values that will be created.
     :param case_sensitive: Whether or not uppercase and lowercase letters
-        will be considered equal (e.g., 'A' != 'a' if `True`).
-
+        will be considered equal.
     :returns: A pandas DataFrame with a new column containing a cumulative
         count of unique values from another column.
     """
+    check_column(df, column_name)
 
     if not case_sensitive:
         # Make it so that the the same uppercase and lowercase
         # letter are treated as one unique value
-        df[column_name] = df[column_name].astype(str).map(str.lower)
+        series = df[column_name].astype("string").str.lower()
+    else:
+        series = df[column_name]
 
-    df[dest_column_name] = (
-        (
-            df[[column_name]]
-            .drop_duplicates()
-            .assign(dummyabcxyz=1)
-            .dummyabcxyz.cumsum()
-        )
-        .reindex(df.index)
+    dummy_name = "_pyjanitor_dummy_col_"
+    count_column = (
+        series.drop_duplicates()
+        .to_frame()
+        .assign(**{dummy_name: 1})[dummy_name]
+        .cumsum()
+        .reindex(df.index, copy=False)
         .ffill()
         .astype(int)
     )
 
-    return df
+    return df.assign(**{dest_column_name: count_column})
--- tests/functions/test_count_cumulative_unique.py
+++ tests/functions/test_count_cumulative_unique.py
@@ -1,23 +1,62 @@
 import pytest
+import numpy as np
 
-from janitor.functions import count_cumulative_unique  # noqa: F401
+
+@pytest.mark.functions
+def test_column_name_not_exists(dataframe):
+    """Checks that an error is raised if the column to be counted does not exist
+    in df.
+    """
+    with pytest.raises(
+        ValueError,
+        match="_foo_ not present in dataframe columns",
+    ):
+        dataframe.count_cumulative_unique(
+            "_foo_",
+            dest_column_name="foo_count",
+        )
+
+
+@pytest.mark.functions
+@pytest.mark.parametrize("case_sensitive", [True, False])
+def test_original_column_values_not_altered(dataframe, case_sensitive):
+    """Checks that the column to be counted is not altered by the case-switching
+    logic implemented by `count_cumulative_unique`.
+    """
+    before = np.array(["a", "b", "c", "A", "B", "C", "a", "b", "c"])
+    dataframe["ccu"] = before
+
+    df = dataframe.count_cumulative_unique(
+        "ccu",
+        dest_column_name="ccu_count",
+        case_sensitive=case_sensitive,
+    )
+    after = df["ccu"].to_numpy()
+
+    assert (before == after).all()
 
 
 @pytest.mark.functions
 def test_case_sensitive(dataframe):
+    """Checks a new column is added containing the correct distinct count,
+    when case sensitive is True.
+    """
     dataframe["ccu"] = ["a", "b", "c", "A", "B", "C", "a", "b", "c"]
     df = dataframe.count_cumulative_unique(
         "ccu", dest_column_name="ccu_count", case_sensitive=True
     )
-    # column ccu_count should contain [1,2,3,4,5,6,6,6,6]
+
     assert all(df["ccu_count"] == [1, 2, 3, 4, 5, 6, 6, 6, 6])
 
 
 @pytest.mark.functions
 def test_not_case_sensitive(dataframe):
+    """Checks a new column is added containing the correct distinct count,
+    when case sensitive is False.
+    """
     dataframe["ccu"] = ["a", "b", "c", "A", "B", "C", "a", "b", "c"]
     df = dataframe.count_cumulative_unique(
         "ccu", dest_column_name="ccu_count", case_sensitive=False
     )
-    # column ccu_count should contain [1,2,3,3,3,3,3,3,3]
+
     assert all(df["ccu_count"] == [1, 2, 3, 3, 3, 3, 3, 3, 3])

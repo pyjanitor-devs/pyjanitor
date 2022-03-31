2d69e49cd7baee407e772c04e18c32a112b2a3aa
Samuel Oranyeli <samueloranyeli@gmail.com>
2022-03-31 11:38:42 +1100

    [ENH] xlsx_cells (#1046)
    * add check argument to subprocess.run in readcommandline, and capture CalledProcessError in tests
    
    * updates to error reporting for read_commandline; more explicit
    
    * default values
    
    * default values
    
    * Add files via upload
    
    * more tests
    
    * minor edits to read_csvs
    
    * minor edits to read_csvs
    
    * minor edits to read_csvs
    
    * edit to test_xlsx_table
    
    * edit to test_xlsx_table
    
    * sheetnames typing change
    
    * modify docs, add better logic, and tests
    
    * Add recursive function to extract cell properties
    
    * Add tests
    
    * Docs cleanup
    
    * tests cleanup
    
    * doctests added, xlsx_table cleaned up
    
    * Update io.py
    
    * Update test_xlsx_table.py
    
    * add changelog
    
    * fix docs
    
    * make object_to_dict private
    
    * fix docs
    
    * updates based on code review
    
    * updates based on code review
    
    * updates based on code review
    
    * docs fix
    
    * some cleanup
    
    * clean up notebook
    
    * Add inspiration to docs
    
    Co-authored-by: samukweku <samukweku@gmail.com>
    Co-authored-by: Eric Ma <ericmjl@users.noreply.github.com>

--- tests/io/test_xlsx_table.py
+++ tests/io/test_xlsx_table.py
@@ -4,6 +4,7 @@ import pytest
 from janitor import io
 from pandas.testing import assert_frame_equal
 from pathlib import Path
+from openpyxl import load_workbook
 
 
 TEST_DATA_DIR = "tests/test_data"
@@ -18,8 +19,8 @@ no_headers = (
 
 
 def test_check_sheetname():
-    """Test sheetname is a string."""
-    with pytest.raises(TypeError):
+    """Raise KeyError if sheetname does not exist."""
+    with pytest.raises(KeyError):
         io.xlsx_table(filename, 1, None)
 
 
@@ -31,7 +32,9 @@ def test_check_filename():
 
 def test_table_exists():
     """Raise error if there is no table in the sheet."""
-    with pytest.raises(ValueError):
+    with pytest.raises(
+        ValueError, match="There is no table in 'Cover' sheet."
+    ):
         io.xlsx_table(filename, "Cover")
 
 
@@ -40,8 +43,23 @@ def test_table_name():
     Raise error if `table` is not None,
     and the table name cannot be found.
     """
-    with pytest.raises(ValueError):
+    with pytest.raises(
+        KeyError, match="Table 'fake' is not in the 'Tables' sheet."
+    ):
         io.xlsx_table(filename, "Tables", table="fake")
+
+
+def test_wb_read_only():
+    """
+    Raise error if Workbook is provided, and read_only is True.
+    """
+    wb = load_workbook(filename, read_only=True)
+    with pytest.raises(
+        AttributeError,
+        match="Accessing the tables.+",
+    ):
+        io.xlsx_table(wb, "Tables")
+    wb.close()
 
 
 def test_table_str():
@@ -72,7 +90,8 @@ def test_table_no_header():
 
 def test_tables():
     """Test output for multiple tables."""
-    expected = io.xlsx_table(filename, "Tables", ("dSalesReps", "dSupplier"))
+    wb = load_workbook(filename, read_only=False)
+    expected = io.xlsx_table(wb, "Tables", ("dSalesReps", "dSupplier"))
     actual = {
         "dSalesReps": pd.read_excel(
             filename, engine="openpyxl", sheet_name="Tables", usecols="A:C"
@@ -87,7 +106,7 @@ def test_tables():
         assert_frame_equal(value, actual[key])
 
 
-def test_tables_None():
+def test_tables_none():
     """Test output for multiple tables."""
     expected = io.xlsx_table(filename, "Tables")
     actual = {

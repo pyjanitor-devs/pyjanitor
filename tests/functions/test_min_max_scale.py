214c722904215546f7e0f22d6ab34345c339651a
Zero <Zeroto521@gmail.com>
2022-05-04 15:01:56 +0800

    test it

--- tests/functions/test_min_max_scale.py
+++ tests/functions/test_min_max_scale.py
@@ -10,18 +10,23 @@ def test_min_max_scale(dataframe):
 
 @pytest.mark.functions
 def test_min_max_scale_custom_new_min_max(dataframe):
-    df = dataframe.min_max_scale(column_name="a", new_min=1, new_max=2)
+    df = dataframe.min_max_scale(column_name="a", feature_range=(1, 2))
     assert df["a"].min() == 1
     assert df["a"].max() == 2
 
 
 @pytest.mark.functions
-def test_min_max_old_min_max_errors(dataframe):
+@pytest.mark.parametrize(
+    "feature_range",
+    [
+        range(2),
+        (1, 2, 3),
+        ("1", 2),
+        [1, "2"],
+        ["1", "2"],
+        [2, 1],
+    ],
+)
+def test_min_max_new_min_max_errors(dataframe, feature_range):
     with pytest.raises(ValueError):
-        dataframe.min_max_scale(column_name="a", old_min=10, old_max=0)
-
-
-@pytest.mark.functions
-def test_min_max_new_min_max_errors(dataframe):
-    with pytest.raises(ValueError):
-        dataframe.min_max_scale(column_name="a", new_min=10, new_max=0)
+        dataframe.min_max_scale(feature_range=feature_range)

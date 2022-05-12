9ca2b501b953430c56dc26dec08126003b742b83
Zero <Zeroto521@gmail.com>
2022-05-12 19:28:13 +0800

    Test normal case

--- tests/utils/test_deprecated_kwargs.py
+++ tests/utils/test_deprecated_kwargs.py
@@ -13,10 +13,28 @@ from janitor.utils import deprecated_kwargs
         (["b", "a"], dict(a=1, b=2)),
     ],
 )
-def test_work(arguments, kwargs):
+def test_arguments(arguments, kwargs):
     @deprecated_kwargs(*arguments)
     def simple_sum(alpha, beta, a=0, b=0):
         return alpha + beta
 
     with pytest.raises(ValueError):
         simple_sum(1, 2, **kwargs)
+
+
+@pytest.mark.utils
+@pytest.mark.parametrize(
+    "arguments, args, excepted",
+    [
+        (["a"], [0, 0], 0),
+        (["b"], [1, 1], 2),
+        (["a", "b"], [0, 1], 1),
+        (["b", "a"], [0, 1], 1),
+    ]
+)
+def test_work(arguments, args, excepted):
+    @deprecated_kwargs(*arguments)
+    def simple_sum(alpha, beta, a=0, b=0):
+        return alpha + beta
+
+    assert simple_sum(*args) == excepted

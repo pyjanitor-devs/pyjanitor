bdfb9c773c08d51fd68b12b87c094f35ff62a0e9
Zero <Zeroto521@gmail.com>
2022-05-16 16:57:40 +0800

    Title more clearer names

--- tests/utils/test_deprecated_kwargs.py
+++ tests/utils/test_deprecated_kwargs.py
@@ -5,7 +5,7 @@ from janitor.utils import deprecated_kwargs
 
 @pytest.mark.utils
 @pytest.mark.parametrize(
-    "arguments, kwargs",
+    "arguments, func_kwargs",
     [
         (["a"], dict(a=1)),
         (["b"], dict(b=2)),
@@ -13,18 +13,18 @@ from janitor.utils import deprecated_kwargs
         (["b", "a"], dict(a=1, b=2)),
     ],
 )
-def test_arguments(arguments, kwargs):
+def test_arguments(arguments, func_kwargs):
     @deprecated_kwargs(*arguments)
     def simple_sum(alpha, beta, a=0, b=0):
         return alpha + beta
 
     with pytest.raises(ValueError):
-        simple_sum(1, 2, **kwargs)
+        simple_sum(1, 2, **func_kwargs)
 
 
 @pytest.mark.utils
 @pytest.mark.parametrize(
-    "arguments, args, expected",
+    "arguments, func_args, expected",
     [
         (["a"], [0, 0], 0),
         (["b"], [1, 1], 2),
@@ -32,9 +32,9 @@ def test_arguments(arguments, kwargs):
         (["b", "a"], [0, 1], 1),
     ],
 )
-def test_work(arguments, args, expected):
+def test_work(arguments, func_args, expected):
     @deprecated_kwargs(*arguments)
     def simple_sum(alpha, beta, a=0, b=0):
         return alpha + beta
 
-    assert simple_sum(*args) == expected
+    assert simple_sum(*func_args) == expected

89f83cc51cb5f358300e34c314d93cd3ab1c7eae
samukweku <samueloranyeli@gmail.com>
2022-06-07 23:30:35 +0000

    fix error message

--- janitor/functions/utils.py
+++ janitor/functions/utils.py
@@ -228,10 +228,10 @@ def _column_sel_dispatch(columns_to_select, df):  # noqa: F811
     if pd.api.types.is_string_dtype(df_columns):
         if columns_to_select in df_columns:
             return [columns_to_select]
-        columns_to_select = fnmatch.filter(df_columns, columns_to_select)
-        if not columns_to_select:
+        outcome = fnmatch.filter(df_columns, columns_to_select)
+        if not outcome:
             raise KeyError(f"No match was returned for '{columns_to_select}'.")
-        return columns_to_select
+        return outcome
 
     if df_columns.is_all_dates:
         if not df_columns.is_monotonic_increasing:

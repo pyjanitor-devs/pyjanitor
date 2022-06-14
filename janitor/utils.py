99e9e011ef01d7e2f9712bb17e2145066e2253ef
samukweku <samueloranyeli@gmail.com>
2022-06-14 09:48:33 +0000

    expand docstring

--- janitor/utils.py
+++ janitor/utils.py
@@ -87,7 +87,7 @@ def _sub_expand_grid(value, grid_index, key):  # noqa: F811
 @_expand_grid.register(pd.api.extensions.ExtensionArray)
 def _sub_expand_grid(value, grid_index, key):  # noqa: F811
     """
-    Expands the pandas array based on `grid_index`.
+    Expands the pandas extension array based on `grid_index`.
     Returns a dictionary.
     """
 

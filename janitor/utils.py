4113f74fd80fc03bdffb38cae557b902c724c275
samukweku <samueloranyeli@gmail.com>
2022-06-14 08:59:27 +0000

    add support for extension arrays

--- janitor/utils.py
+++ janitor/utils.py
@@ -67,7 +67,7 @@ def _sub_expand_grid(value, grid_index, key):  # noqa: F811
             f"has a dimension of {value.ndim}."
         )
 
-    value = value.take(grid_index, axis=0)
+    value = value[grid_index]
 
     if value.ndim == 1:
         return {(key, 0): value}
@@ -82,7 +82,7 @@ def _sub_expand_grid(value, grid_index, key):  # noqa: F811
     Returns a dictionary.
     """
 
-    return {(key, 0): value.take(grid_index, axis=0)}
+    return {(key, 0): value[grid_index]}
 
 
 @_expand_grid.register(pd.api.extensions.ExtensionArray)
@@ -92,7 +92,7 @@ def _sub_expand_grid(value, grid_index, key):  # noqa: F811
     Returns a dictionary.
     """
 
-    return {(key, 0): value.take(grid_index, axis=0)}
+    return {(key, 0): value[grid_index]}
 
 
 @_expand_grid.register(pd.Series)
@@ -105,7 +105,7 @@ def _sub_expand_grid(value, grid_index, key):  # noqa: F811
     name = value.name
     if not name:
         name = 0
-    value = extract_array(value, extract_numpy=True).take(grid_index, axis=0)
+    value = extract_array(value, extract_numpy=True)[grid_index]
 
     return {(key, name): value}
 
@@ -124,9 +124,7 @@ def _sub_expand_grid(value, grid_index, key):  # noqa: F811
         value = value.set_axis(columns, axis="columns")
 
     return {
-        (key, name): extract_array(val, extract_numpy=True).take(
-            grid_index, axis=0
-        )
+        (key, name): extract_array(val, extract_numpy=True)[grid_index]
         for name, val in value.items()
     }
 
@@ -143,7 +141,7 @@ def _sub_expand_grid(value, grid_index, key):  # noqa: F811
     for n in range(value.nlevels):
         arr = value.get_level_values(n)
         name = arr.name
-        arr = extract_array(arr, extract_numpy=True).take(grid_index, axis=0)
+        arr = extract_array(arr, extract_numpy=True)[grid_index]
         if not name:
             name = num
             num += 1
@@ -160,11 +158,7 @@ def _sub_expand_grid(value, grid_index, key):  # noqa: F811
     name = value.name
     if not name:
         name = 0
-    return {
-        (key, name): extract_array(value, extract_numpy=True).take(
-            grid_index, axis=0
-        )
-    }
+    return {(key, name): extract_array(value, extract_numpy=True)[grid_index]}
 
 
 def import_message(

fa6c28480ad320fda615604333d0a14182ae4108
Eric Ma <ericmjl@users.noreply.github.com>
2022-08-14 21:35:05 -0400

    [INF/DOC] Documentation bugfix by pinning mkdocstrings to 0.18.1 (#1147)
    * HOTFIX documentation bugfix by pinning mkdocstrings to 0.18.1
    
    * Add mkdocstrings-python-legacy into env
    
    * Apply to mkdocs/environment
    
    * Add first pass on automated test for docs build.
    
    * Commented out `mkdocs/environment.yaml` until further discussion.
    
    Co-authored-by: Zero <Zeroto521@gmail.com>

---
+++ tests/test_documentation_build.py
@@ -0,0 +1,24 @@
+"""Tests for documentation build."""
+
+import os
+
+
+def test_docs_general_functions_present():
+    """Test that all docs pages build correctly.
+
+    TODO: There has to be a better way to automatically check that
+    all of the functions are present in the docs.
+    This is an awesome thing that we could use help with in the future.
+    """
+    # Build docs using mkdocs
+    os.system("mkdocs build --clean")
+
+    # We want to check that the following keywords are all present.
+    # I put in a subsample of general functions.
+    # This can be made much more robust.
+    rendered_correctly = False
+    with open("./site/api/functions/index.html", "r+") as f:
+        for line in f.readlines():
+            if "add_columns" in line or "update_where" in line:
+                rendered_correctly = True
+    assert rendered_correctly

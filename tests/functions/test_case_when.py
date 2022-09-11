bd648d9595b031d2a7805605fa84e829018c3e32
Samuel Oranyeli <samueloranyeli@gmail.com>
2022-09-12 06:58:58 +1000

    [ENH] Numba support for multiple non-equi joins (#1158)
    * add numba for dual non equi joins  for less/greater than; also add numba for not equal only joins
    
    * add tests and comments
    
    * add comments to code for future me :)
    
    * fix errors in comments; update changelog
    
    * update test for case_when
    
    * improve logic for dual le_lt with != condition
    
    * improve logic for dual le_lt with != condition
    
    * updates to _range_indices
    
    * updates

--- tests/functions/test_case_when.py
+++ tests/functions/test_case_when.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import pytest
-from hypothesis import assume, given
+from hypothesis import assume, given, settings
 from pandas.testing import assert_frame_equal
 
 from janitor.testing_utils.strategies import (
@@ -75,6 +75,8 @@ def test_default_ndim(df):
         df.case_when(df.a < 10, "less_than_10", df, column_name="a")
 
 
+@pytest.mark.turtle
+@settings(deadline=None)
 @given(df=df_strategy())
 def test_default_length(df):
     """Raise ValueError if `default` length != len(df)."""

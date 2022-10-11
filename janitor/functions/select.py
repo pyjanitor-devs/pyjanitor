e91f1072ec350aa74437e4e900022f205a453478
samukweku <samueloranyeli@gmail.com>
2022-10-11 00:52:08 +0000

    updates to docs and tests

--- janitor/functions/select.py
+++ janitor/functions/select.py
@@ -1,13 +1,12 @@
 import pandas_flavor as pf
 import pandas as pd
-import numpy as np
 from pandas.api.types import is_list_like
 from janitor.utils import deprecated_alias
 from janitor.functions.utils import (
+    IndexLabel,
     _select_columns,
     _select_rows,
-    _level_labels,
-    level_labels,
+    _select_index_labels,
 )
 
 
@@ -23,6 +22,9 @@ def select_columns(
 
     It accepts a string, shell-like glob strings `(*string*)`,
     regex, slice, array-like object, or a list of the previous options.
+
+    Selection on a MultiIndex is possible with the `IndexLabel`
+    class.
 
     This method does not mutate the original DataFrame.
 
@@ -49,35 +51,13 @@ def select_columns(
         a callable which is applicable to each Series in the DataFrame,
         or variable arguments of all the aforementioned.
         A sequence of booleans is also acceptable.
+        For selection on a MultiIndex, `IndexLabel` can be handy.
     :param invert: Whether or not to invert the selection.
         This will result in the selection of the complement of the columns
         provided.
-    :raises ValueError: if `levels_label` is combined with other selection
-        options.
     :returns: A pandas DataFrame with the specified columns selected.
     """  # noqa: E501
 
-    all_levels = all((isinstance(arg, level_labels) for arg in args))
-    if all_levels:
-        contents = [
-            _level_labels(df.columns, arg.label, arg.level) for arg in args
-        ]
-        if len(contents) > 1:
-            contents = np.concatenate(contents)
-            # remove possible duplicates
-            contents = pd.unique(contents)
-        else:
-            contents = contents[0]
-        if invert:
-            arr = np.ones(df.columns.size, dtype=np.bool8)
-            arr[contents] = False
-            return df.iloc[arr]
-        return df.iloc[contents]
-    any_levels = any((isinstance(arg, level_labels) for arg in args))
-    if any_levels:
-        raise ValueError(
-            "`level_labels` cannot be combined with other selection options."
-        )
     # applicable to any list-like object (ndarray, Series, pd.Index, ...)
     search_result = []
     for arg in args:
@@ -85,6 +65,9 @@ def select_columns(
             search_result.extend(arg)
         else:
             search_result.append(arg)
+    index_labels = (isinstance(arg, IndexLabel) for arg in search_result)
+    if all(index_labels):
+        return _select_index_labels(df, search_result, "columns", invert)
     search_result = _select_columns(search_result, df)
     if invert:
         return df.drop(columns=search_result)
@@ -103,6 +86,9 @@ def select_rows(
     It accepts a string, shell-like glob strings `(*string*)`,
     regex, slice, array-like object, or a list of the previous options.
 
+    Selection on a MultiIndex is possible with the `IndexLabel`
+    class.
+
     This method does not mutate the original DataFrame.
 
     Optional ability to invert selection of rows available as well.
@@ -111,52 +97,32 @@ def select_rows(
 
         >>> import pandas as pd
         >>> import janitor
-        >>> df = pd.DataFrame({"col1": [1, 2], "foo": [3, 4], "col2": [5, 6]})
+        >>> df = {"col1": [1, 2], "foo": [3, 4], "col2": [5, 6]}
+        >>> df = pd.DataFrame(df, orient='index')
         >>> df
-           col1  foo  col2
-        0     1    3     5
-        1     2    4     6
-        >>> df.select_columns("col*")
-           col1  col2
-        0     1     5
-        1     2     6
+              0  1
+        col1  1  2
+        foo   3  4
+        col2  5  6
+        >>> df.select_rows("col*")
+              0  1
+        col1  1  2
+        col2  5  6
 
     :param df: A pandas DataFrame.
     :param args: Valid inputs include: an exact index name to look for,
         a shell-style glob string (e.g., `*_thing_*`),
         a regular expression,
-        a callable which is applicable to each Series in the DataFrame,
+        a callable which is applicable to the DataFrame,
         or variable arguments of all the aforementioned.
         A sequence of booleans is also acceptable.
+        For selection on a MultiIndex, `IndexLabel` can be handy.
     :param invert: Whether or not to invert the selection.
         This will result in the selection of the complement of the rows
         provided.
-    :raises ValueError: if `levels_label` is combined with other selection
-        options.
     :returns: A pandas DataFrame with the specified rows selected.
     """  # noqa: E501
 
-    all_levels = all((isinstance(arg, level_labels) for arg in args))
-    if all_levels:
-        contents = [
-            _level_labels(df.index, arg.label, arg.level) for arg in args
-        ]
-        if len(contents) > 1:
-            contents = np.concatenate(contents)
-            # remove possible duplicates
-            contents = pd.unique(contents)
-        else:
-            contents = contents[0]
-        if invert:
-            arr = np.ones(df.index.size, dtype=np.bool8)
-            arr[contents] = False
-            return df.iloc[arr]
-        return df.iloc[contents]
-    any_levels = any((isinstance(arg, level_labels) for arg in args))
-    if any_levels:
-        raise ValueError(
-            "`level_labels` cannot be combined with other selection options."
-        )
     # applicable to any list-like object (ndarray, Series, pd.Index, ...)
     search_result = []
     for arg in args:
@@ -164,6 +130,10 @@ def select_rows(
             search_result.extend(arg)
         else:
             search_result.append(arg)
+    index_labels = (isinstance(arg, IndexLabel) for arg in search_result)
+    if all(index_labels):
+        return _select_index_labels(df, search_result, "index", invert)
+
     search_result = _select_rows(search_result, df)
     if invert:
         return df.drop(index=search_result)

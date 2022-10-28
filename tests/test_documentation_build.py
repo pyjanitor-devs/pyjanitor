bb0605080c307000ca3d7544ef502f5b93fa812f
Zero <Zeroto521@gmail.com>
2022-10-28 17:25:29 +0800

    lint codes

--- tests/test_documentation_build.py
+++ tests/test_documentation_build.py
@@ -6,7 +6,8 @@ import pytest
 
 
 # If `mkdocs` wasn't installed in environment, just skip.
-# Can't use `pytest.importorskip("mkdocs")`, 'mkdocs' is also a folder to pyjanitor.
+# Can't use `pytest.importorskip("mkdocs")`, 'mkdocs' is also
+# a folder name to pyjanitor project.
 pytest.importorskip("mkdocstrings")
 
 

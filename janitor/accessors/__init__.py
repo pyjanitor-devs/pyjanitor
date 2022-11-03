829690a67d99a350c5d643a7f0608fa510487785
Zero <Zeroto521@gmail.com>
2022-11-03 15:04:17 +0800

    Simplify a bit

--- janitor/accessors/__init__.py
+++ janitor/accessors/__init__.py
@@ -1,18 +1,3 @@
-"""Miscellaneous mathematical operators.
-
-Lazy loading used here to speed up imports.
-"""
-
-import warnings
-from typing import Tuple
-
-import lazy_loader as lazy
+"""Miscellaneous mathematical operators."""
 
 from janitor.accessors.data_description import DataDescription  # noqa: F401
-
-scipy_special = lazy.load("scipy.special")
-ss = lazy.load("scipy.stats")
-pf = lazy.load("pandas_flavor")
-pd = lazy.load("pandas")
-np = lazy.load("numpy")
-pdtypes = lazy.load("pandas.api.types")

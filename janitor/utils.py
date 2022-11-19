772cf153e4c1be336d8ca485d4828c5e77d96517
joranbeasley <joranbeasley@gmail.com>
2022-11-18 22:00:48 -0800

    Update utils.py
    resolves https://github.com/pyjanitor-devs/pyjanitor/issues/1201

--- janitor/utils.py
+++ janitor/utils.py
@@ -6,7 +6,7 @@ Lazy loading used here to speed up imports.
 import os
 import sys
 import socket
-from typing import Callable, Dict, Iterable, Union
+from typing import Callable, Dict, Iterable, Union, List
 from functools import singledispatch, wraps
 from warnings import warn
 
@@ -213,7 +213,7 @@ def idempotent(func: Callable, df: pd.DataFrame, *args, **kwargs):
 
 
 def deprecated_kwargs(
-    *arguments: list[str],
+    *arguments: List[str],
     message: str = (
         "The keyword argument '{argument}' of '{func_name}' is deprecated."
     ),

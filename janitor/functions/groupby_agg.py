d6cb92679834ddbed2efd86393d5c1d9574708f1
Jeremy Goh <jeremy_gsk@outlook.com>
2023-01-30 01:19:30 +0800

    docs: migrate functions/groupby_agg

--- janitor/functions/groupby_agg.py
+++ janitor/functions/groupby_agg.py
@@ -1,3 +1,4 @@
+"""Implementation source for `groupby_agg`."""
 from typing import Callable, List, Union
 import pandas_flavor as pf
 import pandas as pd
@@ -38,7 +39,8 @@ def groupby_agg(
         [`jn.transform_column`][janitor.functions.transform_columns.transform_column]
         instead.
 
-    Example: Basic usage.
+    Examples:
+        Basic usage.
 
         >>> import pandas as pd
         >>> import janitor
@@ -59,8 +61,8 @@ def groupby_agg(
         3  shoe       200         140.0
         4   bag        25          50.0
 
-    Example: Set `dropna=False` to compute the aggregation, treating the null
-    values in the `by` column as an isolated "group".
+        Set `dropna=False` to compute the aggregation, treating the null
+        values in the `by` column as an isolated "group".
 
         >>> import pandas as pd
         >>> import janitor
@@ -80,15 +82,18 @@ def groupby_agg(
         2  None  9        1
         3     b  9        1
 
-    :param df: A pandas DataFrame.
-    :param by: Column(s) to groupby on, will be passed into `DataFrame.groupby`.
-    :param new_column_name: Name of the aggregation output column.
-    :param agg_column_name: Name of the column to aggregate over.
-    :param agg: How to aggregate.
-    :param dropna: Whether or not to include null values, if present in the
-        `by` column(s). Default is True (null values in `by` are assigned NaN in
-        the new column).
-    :returns: A pandas DataFrame.
+    Args:
+        df: A pandas DataFrame.
+        by: Column(s) to groupby on, will be passed into `DataFrame.groupby`.
+        new_column_name: Name of the aggregation output column.
+        agg_column_name: Name of the column to aggregate over.
+        agg: How to aggregate.
+        dropna: Whether or not to include null values, if present in the
+            `by` column(s). Default is True (null values in `by` are assigned NaN in
+            the new column).
+
+    Returns:
+        A pandas DataFrame.
     """  # noqa: E501
 
     return df.assign(

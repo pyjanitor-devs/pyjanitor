a5159386e0ee3cbd49a1fa418cb6a69e03022419
Jeremy Goh <jeremy_gsk@outlook.com>
2023-01-30 16:00:01 +0800

    docs: migrate functions/shuffle

--- janitor/functions/shuffle.py
+++ janitor/functions/shuffle.py
@@ -14,8 +14,7 @@ def shuffle(
     Super-sugary syntax! Underneath the hood, we use `df.sample(frac=1)`,
     with the option to set the random state.
 
-    Example:
-
+    Examples:
         >>> import pandas as pd
         >>> import janitor
         >>> df = pd.DataFrame({
@@ -37,12 +36,15 @@ def shuffle(
         3     0    a
         4     3    d
 
-    :param df: A pandas DataFrame.
-    :param random_state: If provided, set a seed for the random number
-        generator.
-    :param reset_index: If True, reset the dataframe index to the default
-        RangeIndex.
-    :returns: A shuffled pandas DataFrame.
+    Args:
+        df: A pandas DataFrame.
+        random_state: If provided, set a seed for the random number
+            generator.
+        reset_index: If True, reset the dataframe index to the default
+            RangeIndex.
+
+    Returns:
+        A shuffled pandas DataFrame.
     """
     result = df.sample(frac=1, random_state=random_state)
     if reset_index:

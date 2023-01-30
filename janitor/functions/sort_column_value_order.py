2d742b9198f726771b159b3c1a247f78103a3e7b
Jeremy Goh <jeremy_gsk@outlook.com>
2023-01-30 16:02:16 +0800

    docs: migrate functions/sort_column_value_order

--- janitor/functions/sort_column_value_order.py
+++ janitor/functions/sort_column_value_order.py
@@ -9,12 +9,10 @@ from .remove_columns import remove_columns  # noqa: F401
 def sort_column_value_order(
     df: pd.DataFrame, column: str, column_value_order: dict, columns=None
 ) -> pd.DataFrame:
-    """
-    This function adds precedence to certain values in a specified column, then
-    sorts based on that column and any other specified columns.
+    """This function adds precedence to certain values in a specified column,
+    then sorts based on that column and any other specified columns.
 
-    Example:
-
+    Examples:
         >>> import pandas as pd
         >>> import janitor
         >>> import numpy as np
@@ -43,15 +41,20 @@ def sort_column_value_order(
         2        Feb     200.0     250.0     500.0
         0        Jan     150.0     180.0     400.0
 
-    :param df: This is our DataFrame that we are manipulating
-    :param column: This is a column name as a string we are using to specify
-        which column to sort by
-    :param column_value_order: This is a dictionary of values that will
-        represent precedence of the values in the specified column
-    :param columns: This is a list of additional columns that we can sort by
-    :raises ValueError: raises error if chosen Column Name is not in
-        Dataframe, or if column_value_order dictionary is empty.
-    :return: A sorted pandas DataFrame.
+    Args:
+        df: pandas DataFrame that we are manipulating
+        column: This is a column name as a string we are using to specify
+            which column to sort by
+        column_value_order: Dictionary of values that will
+            represent precedence of the values in the specified column
+        columns: A list of additional columns that we can sort by
+
+    Raises:
+        ValueError: If chosen Column Name is not in
+            Dataframe, or if `column_value_order` dictionary is empty.
+
+    Returns:
+        A sorted pandas DataFrame.
     """
     # Validation checks
     check_column(df, column, present=True)

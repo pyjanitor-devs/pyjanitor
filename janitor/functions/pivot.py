9bdf03b2b35449c52d1905aaa754ab02c26201b0
samukweku <samueloranyeli@gmail.com>
2023-03-05 16:08:36 +1100

    make comment clearer

--- janitor/functions/pivot.py
+++ janitor/functions/pivot.py
@@ -856,13 +856,9 @@ def _computations_pivot_longer(
     if len(column_names) != len(set(column_names)):
         column_names = pd.unique(column_names)
 
-    # creates a new object - essentially a copy
-    # for large dataframes, it would be more efficient
-    # to build a new dataframe from a dictionary comprehension
-    # with copy=False
-    # but that could have adverse effects downstream
-    # if the user executes a computation that mutates the
-    # original dataframe - how to act as a copy without being a copy?
+    # this is the reason why there is no explicit copy (df.copy())
+    # at the very beginning for `pivot_longer`,
+    # because `.loc` makes a copy of the dataframe
     out = df.loc[:, column_names]
 
     if all((names_pattern is None, names_sep is None)):

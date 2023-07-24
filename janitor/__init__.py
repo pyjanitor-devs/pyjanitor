8f2415dff757999047278cb25775a0e4ad73420b
Eric Ma <ericmjl@users.noreply.github.com>
2023-07-24 16:37:32 -0400

    refactor: remove lazy_loader dependency (#1278)

--- janitor/__init__.py
+++ janitor/__init__.py
@@ -1,9 +1,5 @@
-"""Top-level janitor API lives here.
-
-Lazy loading used here to speed up imports.
-"""
-import lazy_loader as lazy
-
+"""Top-level janitor API lives here."""
+import pandas_flavor as pf  # noqa: F401
 
 from .accessors import *  # noqa: F403, F401
 from .functions import *  # noqa: F403, F401
@@ -11,14 +7,7 @@ from .io import *  # noqa: F403, F401
 from .math import *  # noqa: F403, F401
 from .ml import get_features_targets as _get_features_targets
 from .utils import refactored_function
-
-
-_ = lazy.load("pandas_flavor")
-
-try:
-    jxr = lazy.load("janitor.xarray")  # noqa: F401
-except ImportError:
-    pass
+from .xarray import *  # noqa: F403, F401
 
 
 @refactored_function(

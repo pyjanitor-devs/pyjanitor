18e6be8aad9820fef7ff6ddd317a75f361e1df65
samuel.oranyeli <samuel.oranyeli@grow.inc>
2024-05-05 20:36:22 +1000

    separate namespaces for lazyframe and eager dataframe

--- janitor/polars/__init__.py
+++ janitor/polars/__init__.py
@@ -18,9 +18,7 @@ except ImportError:
 @pl.api.register_lazyframe_namespace("janitor")
 @pl.api.register_dataframe_namespace("janitor")
 class PolarsFrame:
-    def __init__(
-        self, df: Union[pl.DataFrame, pl.LazyFrame]
-    ) -> Union[pl.DataFrame, pl.LazyFrame]:
+    def __init__(self, df: pl.DataFrame) -> pl.DataFrame:
         self._df = df
 
     def clean_names(
@@ -30,7 +28,7 @@ class PolarsFrame:
         remove_special: bool = False,
         strip_accents: bool = False,
         truncate_limit: int = None,
-    ) -> Union[pl.DataFrame, pl.LazyFrame]:
+    ) -> pl.DataFrame:
         """
         Clean the column names in a polars DataFrame.
 
@@ -87,6 +85,90 @@ class PolarsFrame:
                 the specified length. Default None does not truncate.
         Returns:
             A polars DataFrame.
+        """  # noqa: E501
+        return self._df.rename(
+            lambda col: _clean_column_names(
+                obj=col,
+                strip_accents=strip_accents,
+                strip_underscores=strip_underscores,
+                case_type=case_type,
+                remove_special=remove_special,
+                truncate_limit=truncate_limit,
+            )
+        )
+
+
+@pl.api.register_lazyframe_namespace("janitor")
+class PolarsLazyFrame:
+    def __init__(self, df: pl.LazyFrame) -> pl.LazyFrame:
+        self._df = df
+
+    def clean_names(
+        self,
+        strip_underscores: Optional[Union[str, bool]] = None,
+        case_type: str = "lower",
+        remove_special: bool = False,
+        strip_accents: bool = False,
+        truncate_limit: int = None,
+    ) -> pl.LazyFrame:
+        """
+        Clean the column names in a polars DataFrame.
+
+        Examples:
+            >>> import polars as pl
+            >>> import janitor.polars
+            >>> df = pl.DataFrame(
+            ...     {
+            ...         "Aloha": range(3),
+            ...         "Bell Chart": range(3),
+            ...         "Animals@#$%^": range(3)
+            ...     }
+            ... )
+            >>> df
+            shape: (3, 3)
+            ┌───────┬────────────┬──────────────┐
+            │ Aloha ┆ Bell Chart ┆ Animals@#$%^ │
+            │ ---   ┆ ---        ┆ ---          │
+            │ i64   ┆ i64        ┆ i64          │
+            ╞═══════╪════════════╪══════════════╡
+            │ 0     ┆ 0          ┆ 0            │
+            │ 1     ┆ 1          ┆ 1            │
+            │ 2     ┆ 2          ┆ 2            │
+            └───────┴────────────┴──────────────┘
+            >>> df.lazy().janitor.clean_names(remove_special=True)
+            <LazyFrame [3 cols, {"aloha": Int64 … "animals": Int64}] at 0x14647ED70>
+            >>> df.lazy().janitor.clean_names(remove_special=True).collect()
+            shape: (3, 3)
+            ┌───────┬────────────┬─────────┐
+            │ aloha ┆ bell_chart ┆ animals │
+            │ ---   ┆ ---        ┆ ---     │
+            │ i64   ┆ i64        ┆ i64     │
+            ╞═══════╪════════════╪═════════╡
+            │ 0     ┆ 0          ┆ 0       │
+            │ 1     ┆ 1          ┆ 1       │
+            │ 2     ┆ 2          ┆ 2       │
+            └───────┴────────────┴─────────┘
+
+        !!! info "New in version 0.28.0"
+
+        Args:
+            strip_underscores: Removes the outer underscores from all
+                column names. Default None keeps outer underscores. Values can be
+                either 'left', 'right' or 'both' or the respective shorthand 'l',
+                'r' and True.
+            case_type: Whether to make the column names lower or uppercase.
+                Current case may be preserved with 'preserve',
+                while snake case conversion (from CamelCase or camelCase only)
+                can be turned on using "snake".
+                Default 'lower' makes all characters lowercase.
+            remove_special: Remove special characters from the column names.
+                Only letters, numbers and underscores are preserved.
+            strip_accents: Whether or not to remove accents from
+                the labels.
+            truncate_limit: Truncates formatted column names to
+                the specified length. Default None does not truncate.
+        Returns:
+            A polars LazyFrame.
         """  # noqa: E501
         return self._df.rename(
             lambda col: _clean_column_names(

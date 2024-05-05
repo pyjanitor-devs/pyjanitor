9f49999bb7e0c10ff88b8937d8f36175b38c9a48
samuel.oranyeli <samuel.oranyeli@grow.inc>
2024-05-05 21:01:30 +1000

    separate namespaces for lazyframe and eager dataframe

--- janitor/polars/__init__.py
+++ janitor/polars/__init__.py
@@ -17,12 +17,9 @@ except ImportError:
     )
 
 
-@pl.api.register_lazyframe_namespace("janitor")
 @pl.api.register_dataframe_namespace("janitor")
 class PolarsFrame:
-    def __init__(
-        self, df: Union[pl.DataFrame, pl.LazyFrame]
-    ) -> Union[pl.DataFrame, pl.LazyFrame]:
+    def __init__(self, df: pl.DataFrame) -> pl.DataFrame:
         self._df = df
 
     def pivot_longer(
@@ -438,7 +435,128 @@ class PolarsExpr:
                 Applicable only if one of names_sep
                 or names_pattern is provided.
         Returns:
-            A polars DataFrame/LazyFrame that has been unpivoted from wide to long
+            A polars DataFrame that has been unpivoted from wide to long
+                format.
+        """  # noqa: E501
+        return _pivot_longer(
+            df=self._df,
+            index=index,
+            column_names=column_names,
+            names_pattern=names_pattern,
+            names_sep=names_sep,
+            names_to=names_to,
+            values_to=values_to,
+            names_transform=names_transform,
+        )
+
+
+@pl.api.register_lazyframe_namespace("janitor")
+class PolarsLazyFrame:
+    def __init__(self, df: pl.LazyFrame) -> pl.LazyFrame:
+        self._df = df
+
+    def pivot_longer(
+        self,
+        index: Union[IntoExpr, Iterable[IntoExpr], None] = None,
+        column_names: Union[IntoExpr, Iterable[IntoExpr], None] = None,
+        names_to: Optional[Union[list, tuple, str]] = "variable",
+        values_to: Optional[Union[list, tuple, str]] = "value",
+        names_sep: Optional[Union[str, None]] = None,
+        names_pattern: Optional[Union[list, tuple, str, None]] = None,
+        names_transform: Optional[Any] = pl.String,
+    ) -> pl.LazyFrame:
+        """
+        Unpivots a LazyFrame from *wide* to *long* format.
+
+        It is modeled after the `pivot_longer` function in R's tidyr package,
+        and also takes inspiration from the `melt` function in R's data.table package.
+
+        This function is useful to massage a LazyFrame into a format where
+        one or more columns are considered measured variables, and all other
+        columns are considered as identifier variables.
+
+        All measured variables are *unpivoted* (and typically duplicated) along the
+        row axis.
+
+        Examples:
+            >>> import polars as pl
+            >>> import polars.selectors as cs
+            >>> import janitor.polars
+            >>> df = pl.LazyFrame(
+            ...     {
+            ...         "Sepal.Length": [5.1, 5.9],
+            ...         "Sepal.Width": [3.5, 3.0],
+            ...         "Petal.Length": [1.4, 5.1],
+            ...         "Petal.Width": [0.2, 1.8],
+            ...         "Species": ["setosa", "virginica"],
+            ...     }
+            ... )
+            >>> df.collect()
+            shape: (2, 5)
+            ┌──────────────┬─────────────┬──────────────┬─────────────┬───────────┐
+            │ Sepal.Length ┆ Sepal.Width ┆ Petal.Length ┆ Petal.Width ┆ Species   │
+            │ ---          ┆ ---         ┆ ---          ┆ ---         ┆ ---       │
+            │ f64          ┆ f64         ┆ f64          ┆ f64         ┆ str       │
+            ╞══════════════╪═════════════╪══════════════╪═════════════╪═══════════╡
+            │ 5.1          ┆ 3.5         ┆ 1.4          ┆ 0.2         ┆ setosa    │
+            │ 5.9          ┆ 3.0         ┆ 5.1          ┆ 1.8         ┆ virginica │
+            └──────────────┴─────────────┴──────────────┴─────────────┴───────────┘
+
+            >>> df.janitor.pivot_longer(index = 'Species').collect()
+            shape: (8, 3)
+            ┌───────────┬──────────────┬───────┐
+            │ Species   ┆ variable     ┆ value │
+            │ ---       ┆ ---          ┆ ---   │
+            │ str       ┆ str          ┆ f64   │
+            ╞═══════════╪══════════════╪═══════╡
+            │ setosa    ┆ Sepal.Length ┆ 5.1   │
+            │ virginica ┆ Sepal.Length ┆ 5.9   │
+            │ setosa    ┆ Sepal.Width  ┆ 3.5   │
+            │ virginica ┆ Sepal.Width  ┆ 3.0   │
+            │ setosa    ┆ Petal.Length ┆ 1.4   │
+            │ virginica ┆ Petal.Length ┆ 5.1   │
+            │ setosa    ┆ Petal.Width  ┆ 0.2   │
+            │ virginica ┆ Petal.Width  ┆ 1.8   │
+            └───────────┴──────────────┴───────┘
+
+        !!! info "New in version 0.28.0"
+
+        Args:
+            index: Column(s) or selector(s) to use as identifier variables.
+            column_names: Column(s) or selector(s) to unpivot.
+            names_to: Name of new column as a string that will contain
+                what were previously the column names in `column_names`.
+                The default is `variable` if no value is provided. It can
+                also be a list/tuple of strings that will serve as new column
+                names, if `name_sep` or `names_pattern` is provided.
+                If `.value` is in `names_to`, new column names will be extracted
+                from part of the existing column names and overrides `values_to`.
+            values_to: Name of new column as a string that will contain what
+                were previously the values of the columns in `column_names`.
+                `values_to` can also be a list/tuple
+                and requires that `names_pattern` is also a list/tuple.
+            names_sep: Determines how the column name is broken up, if
+                `names_to` contains multiple values. It takes the same
+                specification as polars' `str.split` method.
+            names_pattern: Determines how the column name is broken up.
+                It can be a regular expression containing matching groups.
+                It takes the same
+                specification as polars' `str.extract_groups` method.
+                `names_pattern` can also be a list/tuple of regular expressions.
+                Under the hood it is processed with polars' `str.contains` function.
+                For a list/tuple of regular expressions,
+                `names_to` must also be a list/tuple and the lengths of both
+                arguments must match.
+            names_transform: Use this option to change the types of columns that
+                have been transformed to rows.
+                This does not applies to the values' columns.
+                It can be a single valid polars dtype,
+                or a dictionary pairing the new column names
+                with a valid polars dtype.
+                Applicable only if one of names_sep
+                or names_pattern is provided.
+        Returns:
+            A polars LazyFrame that has been unpivoted from wide to long
                 format.
         """  # noqa: E501
         return _pivot_longer(

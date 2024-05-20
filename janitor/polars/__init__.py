e84e62e516477ab01a7146e43e9a84f434f89f4f
samuel.oranyeli <samuel.oranyeli@grow.inc>
2024-05-20 19:14:02 +1000

    add row_to_names method for polars

--- janitor/polars/__init__.py
+++ janitor/polars/__init__.py
@@ -1,8 +1,8 @@
-from typing import Optional, Union
+from __future__ import annotations
 
 from janitor.utils import import_message
 
-from .clean_names import _clean_column_names, _clean_expr_names
+from .row_to_names import _row_to_names
 
 try:
     import polars as pl
@@ -21,80 +21,108 @@ class PolarsFrame:
     def __init__(self, df: pl.DataFrame) -> pl.DataFrame:
         self._df = df
 
-    def clean_names(
+    def row_to_names(
         self,
-        strip_underscores: Optional[Union[str, bool]] = None,
-        case_type: str = "lower",
-        remove_special: bool = False,
-        strip_accents: bool = False,
-        truncate_limit: int = None,
+        row_numbers: int | list = 0,
+        remove_rows: bool = False,
+        remove_rows_above: bool = False,
+        separator: str = "_",
     ) -> pl.DataFrame:
         """
-        Clean the column names in a polars DataFrame.
+        Elevates a row, or rows, to be the column names of a DataFrame.
 
         Examples:
-            >>> import polars as pl
-            >>> import janitor.polars
-            >>> df = pl.DataFrame(
-            ...     {
-            ...         "Aloha": range(3),
-            ...         "Bell Chart": range(3),
-            ...         "Animals@#$%^": range(3)
-            ...     }
-            ... )
+            Replace column names with the first row.
+
+            >>> import pandas as pd
+            >>> import janitor
+            >>> df = pd.DataFrame({
+            ...     "a": ["nums", 6, 9],
+            ...     "b": ["chars", "x", "y"],
+            ... })
             >>> df
-            shape: (3, 3)
-            ┌───────┬────────────┬──────────────┐
-            │ Aloha ┆ Bell Chart ┆ Animals@#$%^ │
-            │ ---   ┆ ---        ┆ ---          │
-            │ i64   ┆ i64        ┆ i64          │
-            ╞═══════╪════════════╪══════════════╡
-            │ 0     ┆ 0          ┆ 0            │
-            │ 1     ┆ 1          ┆ 1            │
-            │ 2     ┆ 2          ┆ 2            │
-            └───────┴────────────┴──────────────┘
-            >>> df.janitor.clean_names(remove_special=True)
-            shape: (3, 3)
-            ┌───────┬────────────┬─────────┐
-            │ aloha ┆ bell_chart ┆ animals │
-            │ ---   ┆ ---        ┆ ---     │
-            │ i64   ┆ i64        ┆ i64     │
-            ╞═══════╪════════════╪═════════╡
-            │ 0     ┆ 0          ┆ 0       │
-            │ 1     ┆ 1          ┆ 1       │
-            │ 2     ┆ 2          ┆ 2       │
-            └───────┴────────────┴─────────┘
+            shape: (3, 2)
+            ┌──────┬───────┐
+            │ a    ┆ b     │
+            │ ---  ┆ ---   │
+            │ str  ┆ str   │
+            ╞══════╪═══════╡
+            │ nums ┆ chars │
+            │ 6    ┆ x     │
+            │ 9    ┆ y     │
+            └──────┴───────┘
+            >>> df.janitor.row_to_names(0, remove_rows=True)
+            shape: (2, 2)
+            ┌──────┬───────┐
+            │ nums ┆ chars │
+            │ ---  ┆ ---   │
+            │ str  ┆ str   │
+            ╞══════╪═══════╡
+            │ 6    ┆ x     │
+            │ 9    ┆ y     │
+            └──────┴───────┘
+            >>> df.janitor.row_to_names(row_numbers=[0,1], remove_rows=True)
+            shape: (1, 2)
+            ┌────────┬─────────┐
+            │ nums_6 ┆ chars_x │
+            │ ---    ┆ ---     │
+            │ str    ┆ str     │
+            ╞════════╪═════════╡
+            │ 9      ┆ y       │
+            └────────┴─────────┘
+
+            Remove rows above the elevated row and the elevated row itself.
+
+            >>> import pandas as pd
+            >>> import janitor
+            >>> df = pd.DataFrame({
+            ...     "a": ["bla1", "nums", 6, 9],
+            ...     "b": ["bla2", "chars", "x", "y"],
+            ... })
+            >>> df
+            shape: (4, 2)
+            ┌──────┬───────┐
+            │ a    ┆ b     │
+            │ ---  ┆ ---   │
+            │ str  ┆ str   │
+            ╞══════╪═══════╡
+            │ bla1 ┆ bla2  │
+            │ nums ┆ chars │
+            │ 6    ┆ x     │
+            │ 9    ┆ y     │
+            └──────┴───────┘
+            >>> df.janitor.row_to_names(1, remove_rows=True, remove_rows_above=True)
+            shape: (2, 2)
+            ┌──────┬───────┐
+            │ nums ┆ chars │
+            │ ---  ┆ ---   │
+            │ str  ┆ str   │
+            ╞══════╪═══════╡
+            │ 6    ┆ x     │
+            │ 9    ┆ y     │
+            └──────┴───────┘
 
         !!! info "New in version 0.28.0"
 
         Args:
-            strip_underscores: Removes the outer underscores from all
-                column names. Default None keeps outer underscores. Values can be
-                either 'left', 'right' or 'both' or the respective shorthand 'l',
-                'r' and True.
-            case_type: Whether to make the column names lower or uppercase.
-                Current case may be preserved with 'preserve',
-                while snake case conversion (from CamelCase or camelCase only)
-                can be turned on using "snake".
-                Default 'lower' makes all characters lowercase.
-            remove_special: Remove special characters from the column names.
-                Only letters, numbers and underscores are preserved.
-            strip_accents: Whether or not to remove accents from
-                the labels.
-            truncate_limit: Truncates formatted column names to
-                the specified length. Default None does not truncate.
+            row_numbers: Position of the row(s) containing the variable names.
+                Note that indexing starts from 0. It can also be a list.
+                Defaults to 0 (first row).
+            remove_rows: Whether the row(s) should be removed from the DataFrame.
+            remove_rows_above: Whether the row(s) above the selected row should
+                be removed from the DataFrame.
+            separator: Combines the labels into a single string,
+                if row_numbers is a list of integers. Default is '_'.
+
         Returns:
             A polars DataFrame.
         """  # noqa: E501
-        return self._df.rename(
-            lambda col: _clean_column_names(
-                obj=col,
-                strip_accents=strip_accents,
-                strip_underscores=strip_underscores,
-                case_type=case_type,
-                remove_special=remove_special,
-                truncate_limit=truncate_limit,
-            )
+        return self._df.pipe(
+            _row_to_names,
+            row_numbers=row_numbers,
+            remove_rows=remove_rows,
+            remove_rows_above=remove_rows_above,
+            separator=separator,
         )
 
 
@@ -103,155 +131,106 @@ class PolarsLazyFrame:
     def __init__(self, df: pl.LazyFrame) -> pl.LazyFrame:
         self._df = df
 
-    def clean_names(
+    def row_to_names(
         self,
-        strip_underscores: Optional[Union[str, bool]] = None,
-        case_type: str = "lower",
-        remove_special: bool = False,
-        strip_accents: bool = False,
-        truncate_limit: int = None,
+        row_numbers: int | list = 0,
+        remove_rows: bool = False,
+        remove_rows_above: bool = False,
+        separator: str = "_",
     ) -> pl.LazyFrame:
         """
-        Clean the column names in a polars LazyFrame.
+        Elevates a row, or rows, to be the column names of a DataFrame.
 
         Examples:
-            >>> import polars as pl
-            >>> import janitor.polars
-            >>> df = pl.LazyFrame(
-            ...     {
-            ...         "Aloha": range(3),
-            ...         "Bell Chart": range(3),
-            ...         "Animals@#$%^": range(3)
-            ...     }
-            ... )
-            >>> df.collect()
-            shape: (3, 3)
-            ┌───────┬────────────┬──────────────┐
-            │ Aloha ┆ Bell Chart ┆ Animals@#$%^ │
-            │ ---   ┆ ---        ┆ ---          │
-            │ i64   ┆ i64        ┆ i64          │
-            ╞═══════╪════════════╪══════════════╡
-            │ 0     ┆ 0          ┆ 0            │
-            │ 1     ┆ 1          ┆ 1            │
-            │ 2     ┆ 2          ┆ 2            │
-            └───────┴────────────┴──────────────┘
-            >>> df.janitor.clean_names(remove_special=True).collect()
-            shape: (3, 3)
-            ┌───────┬────────────┬─────────┐
-            │ aloha ┆ bell_chart ┆ animals │
-            │ ---   ┆ ---        ┆ ---     │
-            │ i64   ┆ i64        ┆ i64     │
-            ╞═══════╪════════════╪═════════╡
-            │ 0     ┆ 0          ┆ 0       │
-            │ 1     ┆ 1          ┆ 1       │
-            │ 2     ┆ 2          ┆ 2       │
-            └───────┴────────────┴─────────┘
+            Replace column names with the first row.
+
+            >>> import pandas as pd
+            >>> import janitor
+            >>> df = pd.DataFrame({
+            ...     "a": ["nums", 6, 9],
+            ...     "b": ["chars", "x", "y"],
+            ... })
+            >>> df
+            shape: (3, 2)
+            ┌──────┬───────┐
+            │ a    ┆ b     │
+            │ ---  ┆ ---   │
+            │ str  ┆ str   │
+            ╞══════╪═══════╡
+            │ nums ┆ chars │
+            │ 6    ┆ x     │
+            │ 9    ┆ y     │
+            └──────┴───────┘
+            >>> df.janitor.row_to_names(0, remove_rows=True).collect()
+            shape: (2, 2)
+            ┌──────┬───────┐
+            │ nums ┆ chars │
+            │ ---  ┆ ---   │
+            │ str  ┆ str   │
+            ╞══════╪═══════╡
+            │ 6    ┆ x     │
+            │ 9    ┆ y     │
+            └──────┴───────┘
+            >>> df.janitor.row_to_names(row_numbers=[0,1], remove_rows=True).collect()
+            shape: (1, 2)
+            ┌────────┬─────────┐
+            │ nums_6 ┆ chars_x │
+            │ ---    ┆ ---     │
+            │ str    ┆ str     │
+            ╞════════╪═════════╡
+            │ 9      ┆ y       │
+            └────────┴─────────┘
+
+            Remove rows above the elevated row and the elevated row itself.
+
+            >>> import pandas as pd
+            >>> import janitor
+            >>> df = pd.DataFrame({
+            ...     "a": ["bla1", "nums", 6, 9],
+            ...     "b": ["bla2", "chars", "x", "y"],
+            ... })
+            >>> df
+            shape: (4, 2)
+            ┌──────┬───────┐
+            │ a    ┆ b     │
+            │ ---  ┆ ---   │
+            │ str  ┆ str   │
+            ╞══════╪═══════╡
+            │ bla1 ┆ bla2  │
+            │ nums ┆ chars │
+            │ 6    ┆ x     │
+            │ 9    ┆ y     │
+            └──────┴───────┘
+            >>> df.janitor.row_to_names(1, remove_rows=True, remove_rows_above=True).collect()
+            shape: (2, 2)
+            ┌──────┬───────┐
+            │ nums ┆ chars │
+            │ ---  ┆ ---   │
+            │ str  ┆ str   │
+            ╞══════╪═══════╡
+            │ 6    ┆ x     │
+            │ 9    ┆ y     │
+            └──────┴───────┘
 
         !!! info "New in version 0.28.0"
 
         Args:
-            strip_underscores: Removes the outer underscores from all
-                column names. Default None keeps outer underscores. Values can be
-                either 'left', 'right' or 'both' or the respective shorthand 'l',
-                'r' and True.
-            case_type: Whether to make the column names lower or uppercase.
-                Current case may be preserved with 'preserve',
-                while snake case conversion (from CamelCase or camelCase only)
-                can be turned on using "snake".
-                Default 'lower' makes all characters lowercase.
-            remove_special: Remove special characters from the column names.
-                Only letters, numbers and underscores are preserved.
-            strip_accents: Whether or not to remove accents from
-                the labels.
-            truncate_limit: Truncates formatted column names to
-                the specified length. Default None does not truncate.
+            row_numbers: Position of the row(s) containing the variable names.
+                Note that indexing starts from 0. It can also be a list.
+                Defaults to 0 (first row).
+            remove_rows: Whether the row(s) should be removed from the DataFrame.
+            remove_rows_above: Whether the row(s) above the selected row should
+                be removed from the DataFrame.
+            separator: If `row_numbers` is a list of numbers, this parameter
+                determines how the labels will be combined into a single string.
+
         Returns:
             A polars LazyFrame.
         """  # noqa: E501
-        return self._df.rename(
-            lambda col: _clean_column_names(
-                obj=col,
-                strip_accents=strip_accents,
-                strip_underscores=strip_underscores,
-                case_type=case_type,
-                remove_special=remove_special,
-                truncate_limit=truncate_limit,
-            )
-        )
-
-
-@pl.api.register_expr_namespace("janitor")
-class PolarsExpr:
-    def __init__(self, expr: pl.Expr) -> pl.Expr:
-        self._expr = expr
-
-    def clean_names(
-        self,
-        strip_underscores: Optional[Union[str, bool]] = None,
-        case_type: str = "lower",
-        remove_special: bool = False,
-        strip_accents: bool = False,
-        enforce_string: bool = False,
-        truncate_limit: int = None,
-    ) -> pl.Expr:
-        """
-        Clean the labels in a polars Expression.
-
-        Examples:
-            >>> import polars as pl
-            >>> import janitor.polars
-            >>> df = pl.DataFrame({"raw": ["Abçdê fgí j"]})
-            >>> df
-            shape: (1, 1)
-            ┌─────────────┐
-            │ raw         │
-            │ ---         │
-            │ str         │
-            ╞═════════════╡
-            │ Abçdê fgí j │
-            └─────────────┘
-
-            Clean the column values:
-            >>> df.with_columns(pl.col("raw").janitor.clean_names(strip_accents=True))
-            shape: (1, 1)
-            ┌─────────────┐
-            │ raw         │
-            │ ---         │
-            │ str         │
-            ╞═════════════╡
-            │ abcde_fgi_j │
-            └─────────────┘
-
-        !!! info "New in version 0.28.0"
-
-        Args:
-            strip_underscores: Removes the outer underscores
-                from all labels in the expression.
-                Default None keeps outer underscores.
-                Values can be either 'left', 'right'
-                or 'both' or the respective shorthand 'l',
-                'r' and True.
-            case_type: Whether to make the labels in the expression lower or uppercase.
-                Current case may be preserved with 'preserve',
-                while snake case conversion (from CamelCase or camelCase only)
-                can be turned on using "snake".
-                Default 'lower' makes all characters lowercase.
-            remove_special: Remove special characters from the values in the expression.
-                Only letters, numbers and underscores are preserved.
-            strip_accents: Whether or not to remove accents from
-                the expression.
-            enforce_string: Whether or not to cast the expression to a string type.
-            truncate_limit: Truncates formatted labels in the expression to
-                the specified length. Default None does not truncate.
-        Returns:
-            A polars Expression.
-        """
-        return _clean_expr_names(
-            obj=self._expr,
-            strip_accents=strip_accents,
-            strip_underscores=strip_underscores,
-            case_type=case_type,
-            remove_special=remove_special,
-            enforce_string=enforce_string,
-            truncate_limit=truncate_limit,
+        return self._df.pipe(
+            _row_to_names,
+            row_numbers=row_numbers,
+            remove_rows=remove_rows,
+            remove_rows_above=remove_rows_above,
+            separator=separator,
         )

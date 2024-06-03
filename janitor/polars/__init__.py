16bf8fd200a846d6f5b7ba64490a264b56462e58
samuel.oranyeli <samuel.oranyeli@grow.inc>
2024-06-03 19:38:47 +1000

    fix conflicts

--- janitor/polars/__init__.py
+++ janitor/polars/__init__.py
@@ -330,6 +330,89 @@ class PolarsFrame:
             names_transform=names_transform,
         )
 
+
+@pl.api.register_lazyframe_namespace("janitor")
+class PolarsLazyFrame:
+    def __init__(self, df: pl.LazyFrame) -> pl.LazyFrame:
+        self._df = df
+
+    def clean_names(
+        self,
+        strip_underscores: str | bool = None,
+        case_type: str = "lower",
+        remove_special: bool = False,
+        strip_accents: bool = False,
+        truncate_limit: int = None,
+    ) -> pl.LazyFrame:
+        """
+        Clean the column names in a polars LazyFrame.
+
+        Examples:
+            >>> import polars as pl
+            >>> import janitor.polars
+            >>> df = pl.LazyFrame(
+            ...     {
+            ...         "Aloha": range(3),
+            ...         "Bell Chart": range(3),
+            ...         "Animals@#$%^": range(3)
+            ...     }
+            ... )
+            >>> df.collect()
+            shape: (3, 3)
+            ┌───────┬────────────┬──────────────┐
+            │ Aloha ┆ Bell Chart ┆ Animals@#$%^ │
+            │ ---   ┆ ---        ┆ ---          │
+            │ i64   ┆ i64        ┆ i64          │
+            ╞═══════╪════════════╪══════════════╡
+            │ 0     ┆ 0          ┆ 0            │
+            │ 1     ┆ 1          ┆ 1            │
+            │ 2     ┆ 2          ┆ 2            │
+            └───────┴────────────┴──────────────┘
+            >>> df.janitor.clean_names(remove_special=True).collect()
+            shape: (3, 3)
+            ┌───────┬────────────┬─────────┐
+            │ aloha ┆ bell_chart ┆ animals │
+            │ ---   ┆ ---        ┆ ---     │
+            │ i64   ┆ i64        ┆ i64     │
+            ╞═══════╪════════════╪═════════╡
+            │ 0     ┆ 0          ┆ 0       │
+            │ 1     ┆ 1          ┆ 1       │
+            │ 2     ┆ 2          ┆ 2       │
+            └───────┴────────────┴─────────┘
+
+        !!! info "New in version 0.28.0"
+
+        Args:
+            strip_underscores: Removes the outer underscores from all
+                column names. Default None keeps outer underscores. Values can be
+                either 'left', 'right' or 'both' or the respective shorthand 'l',
+                'r' and True.
+            case_type: Whether to make the column names lower or uppercase.
+                Current case may be preserved with 'preserve',
+                while snake case conversion (from CamelCase or camelCase only)
+                can be turned on using "snake".
+                Default 'lower' makes all characters lowercase.
+            remove_special: Remove special characters from the column names.
+                Only letters, numbers and underscores are preserved.
+            strip_accents: Whether or not to remove accents from
+                the labels.
+            truncate_limit: Truncates formatted column names to
+                the specified length. Default None does not truncate.
+
+        Returns:
+            A polars LazyFrame.
+        """  # noqa: E501
+        return self._df.rename(
+            lambda col: _clean_column_names(
+                obj=col,
+                strip_accents=strip_accents,
+                strip_underscores=strip_underscores,
+                case_type=case_type,
+                remove_special=remove_special,
+                truncate_limit=truncate_limit,
+            )
+        )
+
     def pivot_longer(
         self,
         index: ColumnNameOrSelector = None,

18e80acba44588a53dfc050b3935a08fff134b48
samuel.oranyeli <samuel.oranyeli@grow.inc>
2024-07-20 00:38:50 +1000

    add polars.expand

--- janitor/polars/complete.py
+++ janitor/polars/complete.py
@@ -51,236 +51,195 @@ def expand(
     Examples:
         >>> import polars as pl
         >>> import janitor.polars
-        >>> df = pl.DataFrame(
-        ...     dict(
-        ...         group=(1, 2, 1, 2),
-        ...         item_id=(1, 2, 2, 3),
-        ...         item_name=("a", "a", "b", "b"),
-        ...         value1=(1, None, 3, 4),
-        ...         value2=range(4, 8),
-        ...     )
-        ... )
+        >>> data = [{'type': 'apple', 'year': 2010, 'size': 'XS'},
+        ...         {'type': 'orange', 'year': 2010, 'size': 'S'},
+        ...         {'type': 'apple', 'year': 2012, 'size': 'M'},
+        ...         {'type': 'orange', 'year': 2010, 'size': 'S'},
+        ...         {'type': 'orange', 'year': 2011, 'size': 'S'},
+        ...         {'type': 'orange', 'year': 2012, 'size': 'M'}]
+        >>> df = pd.DataFrame(data)
         >>> df
-        shape: (4, 5)
-        ┌───────┬─────────┬───────────┬────────┬────────┐
-        │ group ┆ item_id ┆ item_name ┆ value1 ┆ value2 │
-        │ ---   ┆ ---     ┆ ---       ┆ ---    ┆ ---    │
-        │ i64   ┆ i64     ┆ str       ┆ i64    ┆ i64    │
-        ╞═══════╪═════════╪═══════════╪════════╪════════╡
-        │ 1     ┆ 1       ┆ a         ┆ 1      ┆ 4      │
-        │ 2     ┆ 2       ┆ a         ┆ null   ┆ 5      │
-        │ 1     ┆ 2       ┆ b         ┆ 3      ┆ 6      │
-        │ 2     ┆ 3       ┆ b         ┆ 4      ┆ 7      │
-        └───────┴─────────┴───────────┴────────┴────────┘
+        shape: (6, 3)
+        ┌────────┬──────┬──────┐
+        │ type   ┆ year ┆ size │
+        │ ---    ┆ ---  ┆ ---  │
+        │ str    ┆ i64  ┆ str  │
+        ╞════════╪══════╪══════╡
+        │ apple  ┆ 2010 ┆ XS   │
+        │ orange ┆ 2010 ┆ S    │
+        │ apple  ┆ 2012 ┆ M    │
+        │ orange ┆ 2010 ┆ S    │
+        │ orange ┆ 2011 ┆ S    │
+        │ orange ┆ 2012 ┆ M    │
+        └────────┴──────┴──────┘
 
-        Generate all possible combinations of
-        `group`, `item_id`, and `item_name`
-        (whether or not they appear in the data)
-        >>> with pl.Config(tbl_rows=-1):
-        ...     df.complete("group", "item_id", "item_name", sort=True)
-        shape: (12, 5)
-        ┌───────┬─────────┬───────────┬────────┬────────┐
-        │ group ┆ item_id ┆ item_name ┆ value1 ┆ value2 │
-        │ ---   ┆ ---     ┆ ---       ┆ ---    ┆ ---    │
-        │ i64   ┆ i64     ┆ str       ┆ i64    ┆ i64    │
-        ╞═══════╪═════════╪═══════════╪════════╪════════╡
-        │ 1     ┆ 1       ┆ a         ┆ 1      ┆ 4      │
-        │ 1     ┆ 1       ┆ b         ┆ null   ┆ null   │
-        │ 1     ┆ 2       ┆ a         ┆ null   ┆ null   │
-        │ 1     ┆ 2       ┆ b         ┆ 3      ┆ 6      │
-        │ 1     ┆ 3       ┆ a         ┆ null   ┆ null   │
-        │ 1     ┆ 3       ┆ b         ┆ null   ┆ null   │
-        │ 2     ┆ 1       ┆ a         ┆ null   ┆ null   │
-        │ 2     ┆ 1       ┆ b         ┆ null   ┆ null   │
-        │ 2     ┆ 2       ┆ a         ┆ null   ┆ 5      │
-        │ 2     ┆ 2       ┆ b         ┆ null   ┆ null   │
-        │ 2     ┆ 3       ┆ a         ┆ null   ┆ null   │
-        │ 2     ┆ 3       ┆ b         ┆ 4      ┆ 7      │
-        └───────┴─────────┴───────────┴────────┴────────┘
+        Get unique observations:
+        >>> df.expand('type',sort=True)
+        shape: (2, 1)
+        ┌────────┐
+        │ type   │
+        │ ---    │
+        │ str    │
+        ╞════════╡
+        │ apple  │
+        │ orange │
+        └────────┘
+        >>> df.expand('size',sort=True)
+        shape: (3, 1)
+        ┌──────┐
+        │ size │
+        │ ---  │
+        │ str  │
+        ╞══════╡
+        │ M    │
+        │ S    │
+        │ XS   │
+        └──────┘
+        >>> df.expand('type', 'size',sort=True)
+        shape: (6, 2)
+        ┌────────┬──────┐
+        │ type   ┆ size │
+        │ ---    ┆ ---  │
+        │ str    ┆ str  │
+        ╞════════╪══════╡
+        │ apple  ┆ M    │
+        │ apple  ┆ S    │
+        │ apple  ┆ XS   │
+        │ orange ┆ M    │
+        │ orange ┆ S    │
+        │ orange ┆ XS   │
+        └────────┴──────┘
+        >>> df.expand('type','size','year',sort=True)
+        shape: (18, 3)
+        ┌────────┬──────┬──────┐
+        │ type   ┆ size ┆ year │
+        │ ---    ┆ ---  ┆ ---  │
+        │ str    ┆ str  ┆ i64  │
+        ╞════════╪══════╪══════╡
+        │ apple  ┆ M    ┆ 2010 │
+        │ apple  ┆ M    ┆ 2011 │
+        │ apple  ┆ M    ┆ 2012 │
+        │ apple  ┆ S    ┆ 2010 │
+        │ apple  ┆ S    ┆ 2011 │
+        │ apple  ┆ S    ┆ 2012 │
+        │ apple  ┆ XS   ┆ 2010 │
+        │ apple  ┆ XS   ┆ 2011 │
+        │ apple  ┆ XS   ┆ 2012 │
+        │ orange ┆ M    ┆ 2010 │
+        │ orange ┆ M    ┆ 2011 │
+        │ orange ┆ M    ┆ 2012 │
+        │ orange ┆ S    ┆ 2010 │
+        │ orange ┆ S    ┆ 2011 │
+        │ orange ┆ S    ┆ 2012 │
+        │ orange ┆ XS   ┆ 2010 │
+        │ orange ┆ XS   ┆ 2011 │
+        │ orange ┆ XS   ┆ 2012 │
+        └────────┴──────┴──────┘
 
-        Cross all possible `group` values with the unique pairs of
-        `(item_id, item_name)` that already exist in the data.
-        >>> with pl.Config(tbl_rows=-1):
-        ...     df.select(
-        ...         "group", pl.struct("item_id", "item_name"), "value1", "value2"
-        ...     ).complete("group", "item_id", sort=True).unnest("item_id")
-        shape: (8, 5)
-        ┌───────┬─────────┬───────────┬────────┬────────┐
-        │ group ┆ item_id ┆ item_name ┆ value1 ┆ value2 │
-        │ ---   ┆ ---     ┆ ---       ┆ ---    ┆ ---    │
-        │ i64   ┆ i64     ┆ str       ┆ i64    ┆ i64    │
-        ╞═══════╪═════════╪═══════════╪════════╪════════╡
-        │ 1     ┆ 1       ┆ a         ┆ 1      ┆ 4      │
-        │ 1     ┆ 2       ┆ a         ┆ null   ┆ null   │
-        │ 1     ┆ 2       ┆ b         ┆ 3      ┆ 6      │
-        │ 1     ┆ 3       ┆ b         ┆ null   ┆ null   │
-        │ 2     ┆ 1       ┆ a         ┆ null   ┆ null   │
-        │ 2     ┆ 2       ┆ a         ┆ null   ┆ 5      │
-        │ 2     ┆ 2       ┆ b         ┆ null   ┆ null   │
-        │ 2     ┆ 3       ┆ b         ┆ 4      ┆ 7      │
-        └───────┴─────────┴───────────┴────────┴────────┘
-
-        Fill in nulls:
-        >>> with pl.Config(tbl_rows=-1):
-        ...     df.select(
-        ...         "group", pl.struct("item_id", "item_name"), "value1", "value2"
-        ...     ).complete(
-        ...         "group",
-        ...         "item_id",
-        ...         fill_value={"value1": 0, "value2": 99},
-        ...         explicit=True,
-        ...         sort=True,
-        ...     ).unnest("item_id")
-        shape: (8, 5)
-        ┌───────┬─────────┬───────────┬────────┬────────┐
-        │ group ┆ item_id ┆ item_name ┆ value1 ┆ value2 │
-        │ ---   ┆ ---     ┆ ---       ┆ ---    ┆ ---    │
-        │ i64   ┆ i64     ┆ str       ┆ i64    ┆ i64    │
-        ╞═══════╪═════════╪═══════════╪════════╪════════╡
-        │ 1     ┆ 1       ┆ a         ┆ 1      ┆ 4      │
-        │ 1     ┆ 2       ┆ a         ┆ 0      ┆ 99     │
-        │ 1     ┆ 2       ┆ b         ┆ 3      ┆ 6      │
-        │ 1     ┆ 3       ┆ b         ┆ 0      ┆ 99     │
-        │ 2     ┆ 1       ┆ a         ┆ 0      ┆ 99     │
-        │ 2     ┆ 2       ┆ a         ┆ 0      ┆ 5      │
-        │ 2     ┆ 2       ┆ b         ┆ 0      ┆ 99     │
-        │ 2     ┆ 3       ┆ b         ┆ 4      ┆ 7      │
-        └───────┴─────────┴───────────┴────────┴────────┘
-
-        Limit the fill to only the newly created
-        missing values with `explicit = FALSE`:
-        >>> with pl.Config(tbl_rows=-1):
-        ...     df.select(
-        ...         "group", pl.struct("item_id", "item_name"), "value1", "value2"
-        ...     ).complete(
-        ...         "group",
-        ...         "item_id",
-        ...         fill_value={"value1": 0, "value2": 99},
-        ...         explicit=False,
-        ...         sort=True,
-        ...     ).unnest("item_id").sort(pl.all())
-        shape: (8, 5)
-        ┌───────┬─────────┬───────────┬────────┬────────┐
-        │ group ┆ item_id ┆ item_name ┆ value1 ┆ value2 │
-        │ ---   ┆ ---     ┆ ---       ┆ ---    ┆ ---    │
-        │ i64   ┆ i64     ┆ str       ┆ i64    ┆ i64    │
-        ╞═══════╪═════════╪═══════════╪════════╪════════╡
-        │ 1     ┆ 1       ┆ a         ┆ 1      ┆ 4      │
-        │ 1     ┆ 2       ┆ a         ┆ 0      ┆ 99     │
-        │ 1     ┆ 2       ┆ b         ┆ 3      ┆ 6      │
-        │ 1     ┆ 3       ┆ b         ┆ 0      ┆ 99     │
-        │ 2     ┆ 1       ┆ a         ┆ 0      ┆ 99     │
-        │ 2     ┆ 2       ┆ a         ┆ null   ┆ 5      │
-        │ 2     ┆ 2       ┆ b         ┆ 0      ┆ 99     │
-        │ 2     ┆ 3       ┆ b         ┆ 4      ┆ 7      │
-        └───────┴─────────┴───────────┴────────┴────────┘
-
-        >>> df = pl.DataFrame(
-        ...     {
-        ...         "Year": [1999, 2000, 2004, 1999, 2004],
-        ...         "Taxon": [
-        ...             "Saccharina",
-        ...             "Saccharina",
-        ...             "Saccharina",
-        ...             "Agarum",
-        ...             "Agarum",
-        ...         ],
-        ...         "Abundance": [4, 5, 2, 1, 8],
-        ...     }
-        ... )
-        >>> df
+        Get observations that only occur in the data:
+        >>> df.expand(pl.struct('type','size'),sort=True).unnest('type')
+        shape: (4, 2)
+        ┌────────┬──────┐
+        │ type   ┆ size │
+        │ ---    ┆ ---  │
+        │ str    ┆ str  │
+        ╞════════╪══════╡
+        │ apple  ┆ M    │
+        │ apple  ┆ XS   │
+        │ orange ┆ M    │
+        │ orange ┆ S    │
+        └────────┴──────┘
+        >>> df.expand(pl.struct('type','size','year'),sort=True).unnest('type')
         shape: (5, 3)
-        ┌──────┬────────────┬───────────┐
-        │ Year ┆ Taxon      ┆ Abundance │
-        │ ---  ┆ ---        ┆ ---       │
-        │ i64  ┆ str        ┆ i64       │
-        ╞══════╪════════════╪═══════════╡
-        │ 1999 ┆ Saccharina ┆ 4         │
-        │ 2000 ┆ Saccharina ┆ 5         │
-        │ 2004 ┆ Saccharina ┆ 2         │
-        │ 1999 ┆ Agarum     ┆ 1         │
-        │ 2004 ┆ Agarum     ┆ 8         │
-        └──────┴────────────┴───────────┘
+        ┌────────┬──────┬──────┐
+        │ type   ┆ size ┆ year │
+        │ ---    ┆ ---  ┆ ---  │
+        │ str    ┆ str  ┆ i64  │
+        ╞════════╪══════╪══════╡
+        │ apple  ┆ M    ┆ 2012 │
+        │ apple  ┆ XS   ┆ 2010 │
+        │ orange ┆ M    ┆ 2012 │
+        │ orange ┆ S    ┆ 2010 │
+        │ orange ┆ S    ┆ 2011 │
+        └────────┴──────┴──────┘
 
-        Expose missing years from 1999 to 2004 -
-        pass a polars expression with the new dates,
-        and ensure the expression's name already exists
-        in the DataFrame:
-        >>> expression = pl.int_range(1999,2005).alias('Year')
-        >>> with pl.Config(tbl_rows=-1):
-        ...     df.complete(expression,'Taxon',sort=True)
-        shape: (12, 3)
-        ┌──────┬────────────┬───────────┐
-        │ Year ┆ Taxon      ┆ Abundance │
-        │ ---  ┆ ---        ┆ ---       │
-        │ i64  ┆ str        ┆ i64       │
-        ╞══════╪════════════╪═══════════╡
-        │ 1999 ┆ Agarum     ┆ 1         │
-        │ 1999 ┆ Saccharina ┆ 4         │
-        │ 2000 ┆ Agarum     ┆ null      │
-        │ 2000 ┆ Saccharina ┆ 5         │
-        │ 2001 ┆ Agarum     ┆ null      │
-        │ 2001 ┆ Saccharina ┆ null      │
-        │ 2002 ┆ Agarum     ┆ null      │
-        │ 2002 ┆ Saccharina ┆ null      │
-        │ 2003 ┆ Agarum     ┆ null      │
-        │ 2003 ┆ Saccharina ┆ null      │
-        │ 2004 ┆ Agarum     ┆ 8         │
-        │ 2004 ┆ Saccharina ┆ 2         │
-        └──────┴────────────┴───────────┘
+        Expand the DataFrame to include new observations:
+        >>> df.expand('type','size',pl.int_range(2010,2014).alias('new_year'),sort=True)
+        shape: (24, 3)
+        ┌────────┬──────┬──────────┐
+        │ type   ┆ size ┆ new_year │
+        │ ---    ┆ ---  ┆ ---      │
+        │ str    ┆ str  ┆ i64      │
+        ╞════════╪══════╪══════════╡
+        │ apple  ┆ M    ┆ 2010     │
+        │ apple  ┆ M    ┆ 2011     │
+        │ apple  ┆ M    ┆ 2012     │
+        │ apple  ┆ M    ┆ 2013     │
+        │ apple  ┆ S    ┆ 2010     │
+        │ apple  ┆ S    ┆ 2011     │
+        │ apple  ┆ S    ┆ 2012     │
+        │ apple  ┆ S    ┆ 2013     │
+        │ apple  ┆ XS   ┆ 2010     │
+        │ apple  ┆ XS   ┆ 2011     │
+        │ apple  ┆ XS   ┆ 2012     │
+        │ apple  ┆ XS   ┆ 2013     │
+        │ orange ┆ M    ┆ 2010     │
+        │ orange ┆ M    ┆ 2011     │
+        │ orange ┆ M    ┆ 2012     │
+        │ orange ┆ M    ┆ 2013     │
+        │ orange ┆ S    ┆ 2010     │
+        │ orange ┆ S    ┆ 2011     │
+        │ orange ┆ S    ┆ 2012     │
+        │ orange ┆ S    ┆ 2013     │
+        │ orange ┆ XS   ┆ 2010     │
+        │ orange ┆ XS   ┆ 2011     │
+        │ orange ┆ XS   ┆ 2012     │
+        │ orange ┆ XS   ┆ 2013     │
+        └────────┴──────┴──────────┘
 
-        Expose missing rows per group:
-        >>> df = pl.DataFrame(
-        ...     {
-        ...         "state": ["CA", "CA", "HI", "HI", "HI", "NY", "NY"],
-        ...         "year": [2010, 2013, 2010, 2012, 2016, 2009, 2013],
-        ...         "value": [1, 3, 1, 2, 3, 2, 5],
-        ...     }
-        ... )
-        >>> df
-        shape: (7, 3)
-        ┌───────┬──────┬───────┐
-        │ state ┆ year ┆ value │
-        │ ---   ┆ ---  ┆ ---   │
-        │ str   ┆ i64  ┆ i64   │
-        ╞═══════╪══════╪═══════╡
-        │ CA    ┆ 2010 ┆ 1     │
-        │ CA    ┆ 2013 ┆ 3     │
-        │ HI    ┆ 2010 ┆ 1     │
-        │ HI    ┆ 2012 ┆ 2     │
-        │ HI    ┆ 2016 ┆ 3     │
-        │ NY    ┆ 2009 ┆ 2     │
-        │ NY    ┆ 2013 ┆ 5     │
-        └───────┴──────┴───────┘
-        >>> low = pl.col('year').min()
-        >>> high = pl.col('year').max().add(1)
-        >>> new_year_values=pl.int_range(low,high).alias('year')
-        >>> with pl.Config(tbl_rows=-1):
-        ...     df.complete(new_year_values,by='state',sort=True)
-        shape: (16, 3)
-        ┌───────┬──────┬───────┐
-        │ state ┆ year ┆ value │
-        │ ---   ┆ ---  ┆ ---   │
-        │ str   ┆ i64  ┆ i64   │
-        ╞═══════╪══════╪═══════╡
-        │ CA    ┆ 2010 ┆ 1     │
-        │ CA    ┆ 2011 ┆ null  │
-        │ CA    ┆ 2012 ┆ null  │
-        │ CA    ┆ 2013 ┆ 3     │
-        │ HI    ┆ 2010 ┆ 1     │
-        │ HI    ┆ 2011 ┆ null  │
-        │ HI    ┆ 2012 ┆ 2     │
-        │ HI    ┆ 2013 ┆ null  │
-        │ HI    ┆ 2014 ┆ null  │
-        │ HI    ┆ 2015 ┆ null  │
-        │ HI    ┆ 2016 ┆ 3     │
-        │ NY    ┆ 2009 ┆ 2     │
-        │ NY    ┆ 2010 ┆ null  │
-        │ NY    ┆ 2011 ┆ null  │
-        │ NY    ┆ 2012 ┆ null  │
-        │ NY    ┆ 2013 ┆ 5     │
-        └───────┴──────┴───────┘
+        Filter for missing observations:
+        >>> columns = ('type','size','year')
+        >>> df.expand(*columns).join(df, how='anti', on=columns).sort(by=pl.all())
+        shape: (13, 3)
+        ┌────────┬──────┬──────┐
+        │ type   ┆ size ┆ year │
+        │ ---    ┆ ---  ┆ ---  │
+        │ str    ┆ str  ┆ i64  │
+        ╞════════╪══════╪══════╡
+        │ apple  ┆ M    ┆ 2010 │
+        │ apple  ┆ M    ┆ 2011 │
+        │ apple  ┆ S    ┆ 2010 │
+        │ apple  ┆ S    ┆ 2011 │
+        │ apple  ┆ S    ┆ 2012 │
+        │ apple  ┆ XS   ┆ 2011 │
+        │ apple  ┆ XS   ┆ 2012 │
+        │ orange ┆ M    ┆ 2010 │
+        │ orange ┆ M    ┆ 2011 │
+        │ orange ┆ S    ┆ 2012 │
+        │ orange ┆ XS   ┆ 2010 │
+        │ orange ┆ XS   ┆ 2011 │
+        │ orange ┆ XS   ┆ 2012 │
+        └────────┴──────┴──────┘
 
+        Expand within each group, using `by`:
+        >>> df.expand('year','size',by='type',sort=True)
+        shape: (10, 3)
+        ┌────────┬──────┬──────┐
+        │ type   ┆ year ┆ size │
+        │ ---    ┆ ---  ┆ ---  │
+        │ str    ┆ i64  ┆ str  │
+        ╞════════╪══════╪══════╡
+        │ apple  ┆ 2010 ┆ M    │
+        │ apple  ┆ 2010 ┆ XS   │
+        │ apple  ┆ 2012 ┆ M    │
+        │ apple  ┆ 2012 ┆ XS   │
+        │ orange ┆ 2010 ┆ M    │
+        │ orange ┆ 2010 ┆ S    │
+        │ orange ┆ 2011 ┆ M    │
+        │ orange ┆ 2011 ┆ S    │
+        │ orange ┆ 2012 ┆ M    │
+        │ orange ┆ 2012 ┆ S    │
+        └────────┴──────┴──────┘
 
     !!! info "New in version 0.28.0"
 

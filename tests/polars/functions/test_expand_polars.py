dd54fb2bdca697f003d0ac560d4fd0523400628a
samuel.oranyeli <samuel.oranyeli@grow.inc>
2024-07-20 00:59:06 +1000

    add tests polars expand

---
+++ tests/polars/functions/test_expand_polars.py
@@ -0,0 +1,45 @@
+import polars as pl
+import pytest
+from polars.testing import assert_frame_equal
+
+import janitor.polars  # noqa: F401
+
+
+@pytest.fixture
+def df():
+    """pytest fixture"""
+    return pl.DataFrame(
+        dict(
+            group=(1, 2, 1, 2),
+            item_id=(1, 2, 2, 3),
+            item_name=("a", "a", "b", "b"),
+            value1=(1, None, 3, 4),
+            value2=range(4, 8),
+        )
+    )
+
+
+def test_column_None(df):
+    """Test output if *columns is empty."""
+    assert_frame_equal(df.expand(), df)
+
+
+def test_empty_groups(df):
+    """Raise TypeError if wrong column type is passed."""
+    msg = "The argument passed to the columns parameter "
+    msg += "should either be a string, a column selector, "
+    msg += "a polars expression, or a polars Series; instead got.+"
+    with pytest.raises(TypeError, match=msg):
+        df.complete("group", {})
+
+
+def test_type_sort(df):
+    """Raise TypeError if `sort` is not boolean."""
+    with pytest.raises(TypeError):
+        df.complete("group", "item_id", sort=11)
+
+
+def test_expand_1(df):
+    """
+    Test output for janitor.expand.
+    """

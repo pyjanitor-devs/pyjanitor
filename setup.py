8578b9bb9a66f438379f3f8a18bce8152a2a83f6
samuel.oranyeli <samuel.oranyeli@grow.inc>
2025-08-31 09:51:15 +1000

    use c file only for now

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ try:
     USE_CYTHON = True
 except ImportError:
     USE_CYTHON = False
+# avoid issues with cython 3 vs 0.29
+USE_CYTHON = False
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 

4464c91d5c675ae666543ed26d184f553b287346
samuel.oranyeli <samuel.oranyeli@grow.inc>
2025-10-11 22:00:50 +1100

    convert_matlab_date

--- janitor/polars/dates_to_polars.py
+++ janitor/polars/dates_to_polars.py
@@ -89,16 +89,16 @@ def convert_matlab_date(expr: pl.Expr) -> pl.Expr:
         >>> expression = pl.col('date').convert_matlab_date().alias('date_')
         >>> df.with_columns(expression)
         shape: (4, 2)
-        ┌───────────────┬─────────────────────────┐
-        │ date          ┆ date_                   │
-        │ ---           ┆ ---                     │
-        │ f64           ┆ datetime[μs]            │
-        ╞═══════════════╪═════════════════════════╡
-        │ 737125.0      ┆ 2018-03-06 00:00:00     │
-        │ 737124.815863 ┆ 2018-03-05 19:34:50.563 │
-        │ 737124.4985   ┆ 2018-03-05 11:57:50.399 │
-        │ 737124.0      ┆ 2018-03-05 00:00:00     │
-        └───────────────┴─────────────────────────┘
+        ┌───────────────┬────────────────────────────┐
+        │ date          ┆ date_                      │
+        │ ---           ┆ ---                        │
+        │ f64           ┆ datetime[μs]               │
+        ╞═══════════════╪════════════════════════════╡
+        │ 737125.0      ┆ 2018-03-06 00:00:00        │
+        │ 737124.815863 ┆ 2018-03-05 19:34:50.563199 │
+        │ 737124.4985   ┆ 2018-03-05 11:57:50.399998 │
+        │ 737124.0      ┆ 2018-03-05 00:00:00        │
+        └───────────────┴────────────────────────────┘
 
     !!! info "New in version 0.28.0"
 
@@ -108,5 +108,5 @@ def convert_matlab_date(expr: pl.Expr) -> pl.Expr:
     # https://stackoverflow.com/questions/13965740/converting-matlabs-datenum-format-to-python
     expression = expr.sub(719529).mul(86_400_000)
     expression = pl.duration(milliseconds=expression)
-    expression += pl.datetime(year=1970, month=1, day=1)
+    expression += pl.datetime(year=1970, month=1, day=1, time_unit="ms")
     return expression

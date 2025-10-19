d2c115f3d9fe6903841eeff8536ba363bd85d47a
samuel.oranyeli <samuel.oranyeli@grow.inc>
2025-10-20 00:41:14 +1100

    single equi join migration

---
+++ janitor/functions/_conditional_join/_single_join.py
@@ -0,0 +1,46 @@
+# helper function for a single join
+
+import pandas as pd
+
+from ._greater_than_indices import _greater_than_indices
+from ._helpers import (
+    greater_than_join_types,
+    less_than_join_types,
+)
+from ._less_than_indices import _less_than_indices
+from ._not_equal_indices import _not_equal_indices
+
+
+def _single_join(
+    df: pd.DataFrame,
+    right: pd.DataFrame,
+    condition: tuple,
+    keep: str,
+    return_matching_indices: bool,
+) -> dict | None:
+    """
+    Compute indices for a single join
+    """
+    left_on, right_on, op = condition
+    if op in less_than_join_types:
+        return _less_than_indices(
+            left=df[left_on],
+            right=right[right_on],
+            strict=op == "<",
+            keep=keep,
+            return_matching_indices=return_matching_indices,
+        )
+    if op in greater_than_join_types:
+        return _greater_than_indices(
+            left=df[left_on],
+            right=right[right_on],
+            strict=op == ">",
+            keep=keep,
+            return_matching_indices=return_matching_indices,
+        )
+    if op == "!=":
+        return _not_equal_indices(
+            left=df[left_on],
+            right=right[right_on],
+            keep=keep,
+        )
